8f9706afec2df79bec4e3a3a6705fb579a85d349
Matt <matt@zurb.com>
2011-11-02 17:05:47 -0700

    #72 Increase margin at bottom of Orbit when bullets are enabled

--- javascripts/jquery.orbit-1.3.0.js
+++ javascripts/jquery.orbit-1.3.0.js
@@ -364,6 +364,7 @@
       this.$bullets = $(this.bulletHTML);
     	this.$wrapper.append(this.$bullets);
     	this.$slides.each(this.addBullet);
+    	this.$element.addClass('with-bullets');
     	if (this.options.centerBullets) this.$bullets.css('margin-left', -this.$bullets.width() / 2);
     },
     
