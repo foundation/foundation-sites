eb0e869f4e879597cfeb35b4496fb96aa5db9134
Jonathan Smiley <jonathan@zurb.com>
2011-11-16 08:30:54 -0800

    Modified the ZURBjobs embed in the docs

--- marketing/docs/includes/_download.php
+++ marketing/docs/includes/_download.php
@@ -6,15 +6,13 @@
 					</p>
 				</div>
 
-<!--
-=================================================================
-==========================ZURBjobs===============================
--->
+
+				<!-- ==========================
+				ZURBjobs
+				=============================== -->
 				
 				<div class="jobs hide-on-phones">
-					<h5>Looking for a Product Engineer or Design job? Check out :</h5>
+					<h5>Product Engineering and<br />Design jobs:</h5>
 					<script type="text/javascript" src="http://www.zurb.com/jobs/widgets/jobs.js?limit=3&variation=foundation-sidebar"></script>
-				<a id="via" href="http://zurbjobs.com">via&nbsp;<span class="jobs-link">ZURBjobs</span></a>
-
-				
+					<a id="via" href="http://zurbjobs.com">via&nbsp;<span class="jobs-link">ZURBjobs</span></a>
 				</div>
