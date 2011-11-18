181b5dde046b871106020313d5c95b8ca0a6ef3d
Matt <matt@zurb.com>
2011-11-18 14:38:56 -0800

    Append version to top of files when creating a build

--- build.rb
+++ build.rb
@@ -1,4 +1,11 @@
 # Creates a zip file of the Foundation template and the compessed assets
+
+VERSION_STRING = '2.1'
+
+def prepend_text(file_name, text)
+  `exec 3<> '#{file_name}' && awk -v TEXT="#{text}" 'BEGIN {print TEXT}{print}' '#{file_name}' >&3`
+end
+
 `rm -rf public marketing/files/foundation-download.zip`
 `jammit`
 `mkdir public/src public/src/javascripts public/src/stylesheets`
@@ -22,6 +29,10 @@ text.gsub!(/<!-- Combine and Compress These JS Files -->.+<!-- End Combine and C
 
 File.open(file_name, "w") do |file|  
   file.puts text
+end
+
+%w{public/src/javascripts/app.js public/src/javascripts/foundation.js public/src/stylesheets/app.css public/src/stylesheets/foundation.css public/src/stylesheets/ie.css}.each do |file_name|
+  prepend_text(file_name, "/* Foundation v#{VERSION_STRING} http://foundation.zurb.com */")
 end
 
 `cd public/src && zip -r ../../marketing/files/foundation-download.zip *`
