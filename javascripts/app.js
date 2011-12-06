4cb977825ac6810e581832679b5bfd49827332b9
Jordan Humphreys <jordan@mailyard.net>
2011-12-06 12:47:29 -0800

    New dropdown navigation javascript, but commented out.

--- javascripts/app.js
+++ javascripts/app.js
@@ -4,20 +4,20 @@ $(document).ready(function () {
 
 	/* TABS --------------------------------- */
 	/* Remove if you don't need :) */
-	
+
 	function activateTab($tab) {
 	  var $activeTab = $tab.closest('dl').find('a.active'),
 	      contentLocation = $tab.attr("href") + 'Tab';
-	      
+
 	  //Make Tab Active
 	  $activeTab.removeClass('active');
 	  $tab.addClass('active');
-	  
+
     //Show Tab Content
 		$(contentLocation).closest('.tabs-content').find('li').hide();
 		$(contentLocation).show();
 	}
-	
+
 	$('dl.tabs').each(function () {
 		//Get all tabs
 		var tabs = $(this).children('dd').children('a');
@@ -25,20 +25,38 @@ $(document).ready(function () {
 		  activateTab($(this));
 		});
 	});
-	
+
 	if (window.location.hash) {
     activateTab($('a[href="' + window.location.hash + '"]'));
   }
-	
-	
+
+
 	/* PLACEHOLDER FOR FORMS ------------- */
 	/* Remove this and jquery.placeholder.min.js if you don't need :) */
-	
+
 	$('input, textarea').placeholder();
-	
-	
+
+	/* DROPDOWN NAV ------------- */
+	// $('.nav-bar li a').bind('touchend, click', function(e){
+	// 	e.preventDefault();
+	// 	e.stopPropagation();
+	// 	var f = $(this).siblings('.flyout');
+	// 	if (!f.is(':visible') && f.length > 0) {
+	// 		$('.nav-bar li .flyout').hide();
+	// 		f.show();
+	// 	} else {
+	// 		window.location = $(this).attr('href');
+	// 	}
+	// });
+	// $('.nav-bar, .nav-bar li .flyout').bind('touchend, touchstart, touchmove, touchscroll, hover, mousemove, mouseup, mouseover, mousedown, click', function(e) {
+	// 	return false;
+	// });
+	// $('body').bind('touchend', function(e) {
+	// 	$('.nav-bar li .flyout').hide();
+	// });
+
 	/* DISABLED BUTTONS ------------- */
 	/* Gives elements with a class of 'disabled' a return: false; */
-	
-	
+
+
 });
