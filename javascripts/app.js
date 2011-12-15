a6467b45c50183dd468d5706631b55f6555f72b0
Jonathan Smiley <jonathan@zurb.com>
2011-12-15 09:57:33 -0800

    Cleaning up some spacing

--- javascripts/app.js
+++ javascripts/app.js
@@ -43,11 +43,15 @@ $(document).ready(function () {
 
 	$('input, textarea').placeholder();
 
+
+
 	/* UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids */
 //	$('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
 //	$('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
 //	$('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
 //	$('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+	
+	
 	
 	/* DROPDOWN NAV ------------- */
 	/*
