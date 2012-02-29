5cad7ec5f2ed76bee3fbd32d5cbde477727747b1
zurbchris <chris@zurb.com>
2012-02-29 11:45:28 -0800

    added settings docs for sass gem

--- marketing/docs/gems.php
+++ marketing/docs/gems.php
@@ -55,6 +55,24 @@
             <p>We've also made it very easy to add our gem to existing project. You just need to make sure to add <strong>require "ZURB-foundation"</strong> to the top of your config file. Then navigate to your project directory and run:</p>
             <p><script type="text/javascript" src="http://snipt.net/embed/49b9cebdffc1ef28654b73c1e656602b"></script></p>
             
+            <h5>Customizable Settings</h5>
+            <p>We're in the process of making our gem even more customizable! Here are the settings we've extrapolated so far, just uncomment them and change to what you need:</p>
+            <ul>
+              <li><strong>$experimental-support-for-khtml</strong>&ndash; Set this to true to add support </li>
+              <li><strong>$experimental-support-for-microsoft</strong>&ndash; Set this to true to add support </li>
+              <li><strong>Font Stacks</strong>&ndash; Customize the default font stack we use.</li>
+              <li><strong>$body-font-size</strong>&ndash; Default font size.</li>
+              <li><strong>$body-font-family</strong>&ndash; Default font family.</li>
+              <li><strong>$body-background</strong>&ndash; Default background color.</li>
+              <li><strong>$body-line-height</strong>&ndash; Default line height.</li>
+              <li><strong>$body-color</strong>&ndash; Default font color.</li>
+              <li><strong>$grid-max-width</strong>&ndash; Change the size of the grid.</li>
+              <li><strong>$grid-min-width</strong>&ndash; How small the grid can go.</li>
+              <li><strong>$support-block-grid-nth-child</strong>&ndash; Set to true if you want support.</li>
+              <li><strong>$button-transition-duration</strong>&ndash; How long the hover effects will last.</li>
+              <li><strong>$default-color</strong>&ndash; The main color for styling your elements.</li>
+            </ul>
+            
             <h4>Using Compass to Compile Your SASS/SCSS into CSS</h4>
             <p>Navigate into your new project directory from the command line and run the following line, which will tell Compass to watch your sass files for when you save. Each time you save, Compass will compile the file into plain CSS and give you error if there are any.</p>
             <p><script type="text/javascript" src="http://snipt.net/embed/c9c8c237f2e7ad74c01ef6e272513dd6"></script></p>
