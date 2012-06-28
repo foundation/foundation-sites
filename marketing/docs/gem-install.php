7bb52fa9ae2cd1abc931926e4baf1217044a228b
zurbchris <chris@zurb.com>
2012-06-27 21:52:45 -0700

    finished docs for sass stuff

--- marketing/docs/gem-install.php
+++ marketing/docs/gem-install.php
@@ -103,7 +103,57 @@
 
               <a name="mixins"></a>
               <h3>Mixins &amp; Functions</h3>
-              <h4 class="subheader">Work with your own colors, font sizes, radii, etc by adjusting the _settings.scss file in your new project!</h4>
+              <h4 class="subheader">We wrote some awesome mixins and function that you can use for semantic grids, CSS triangles and more!</h4>
+
+              <h5>Semantic Grid</h5>
+              <p>Are you tired of adding extra divs just to include classes for building your grid? We are too! Our semantic grid mixins will help you write more semantic markup. Here's how they work:</p>
+              <ul class="disc">
+                <li><strong>@include outer-row();</strong> Create container rows to start the grid.</li>
+                <li><strong>@include inner-row();</strong> Nested rows, must be inside a column.</li>
+                <li><strong>@include inner-row(collapse);</strong> Collapse the margins of a nested row.</li>
+                <li><strong>@include column(#);</strong> Create columns inside of any row. It must contain a number.</li>
+                <li><strong>@include column(#, center);</strong> Center your columns within a row.</li>
+                <li><strong>@include column(#, collapse);</strong> Collapse the column gutter for postfix and prefix elements.</li>
+                <li><strong>@include offset-by(#);</strong> Offset your columns to the right.</li>
+                <li><strong>@include push(#);</strong> Source ordering to push columns around.</li>
+                <li><strong>@include pull(#);</strong> Source ordering to pull columns around.</li>
+                <li><strong>@include mobile-row();</strong> Create rows that appear on mobile layouts.</li>
+                <li><strong>@include mobile-column(#);</strong> Create mobile columns inside mobile rows.</li>
+                <li><strong>@include mobile-push(#);</strong> Source ordering to push columns around on mobile.</li>
+                <li><strong>@include mobile-pull(#);</strong> Source ordering to pull columns around on mobile.</li>
+              </ul>
+              <span>
+                <strong>Markup Example:</strong>
+                <script src="https://gist.github.com/3009003.js?file=example.html"></script>
+              </span>
+              <span class="btm-marg">
+                <strong>SCSS Example:</strong>
+                <script src="https://gist.github.com/3009035.js?file=semantic-grid.scss"></script>
+              </span>
+
+              <h5>Modular Scale</h5>
+              <p>The typography in Foundation is set to a specific modular scale, which you can update in your _settings.php file. In order to utilize the scale, you must follow the correct syntax.</p>
+              <ul class="disc">
+                <li><strong>ms(#);</strong> Tell your font which number from the scale to use.</li>
+              </ul>
+              <span class="btm-marg"><script src="https://gist.github.com/3009164.js?file=modular-scale.scss"></script></span>
+
+              <h5>Font Size</h5>
+              <p>If you feel like breaking out of the modular scale or not using the provided function, you can use our font-size mixin!</p>
+              <ul class="disc">
+                <li><strong>@include font-size(size);</strong> Outputs 'px' and 'rem' for the number you include!</li>
+              </ul>
+              <span class="btm-marg"><script src="https://gist.github.com/3009156.js?file=font-size.scss"></script></span>
+
+              <h5>CSS Triangles</h5>
+              <p>If you area unfamiliar with CSS triangles, learn more <a href="http://css-tricks.com/snippets/css/css-triangle/">here &rarr;</a> We use them all in foundation for the arrows in dropdowns and pips on tooltips flyouts.</p>
+
+              <ul class="disc">
+                <li><strong>@include css-triangle(size, color, direction);</strong> Easily create awesome triangles!</li>
+              </ul>
+              <span class="btm-marg"><script src="https://gist.github.com/3009123.js?file=css-triangles.scss"></script></span>
+
+
             </li>
             <li id="simple2">
               Hey
