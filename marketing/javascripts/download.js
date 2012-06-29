798b9e0cbc9a50b04771f2459f6b553de7b40923
Jordan Humphreys <jordan@mailyard.net>
2012-06-28 22:55:12 -0700

    Remove extra border color.

--- marketing/javascripts/download.js
+++ marketing/javascripts/download.js
@@ -101,17 +101,6 @@ function debug(o){
   
 })(this.jQuery);
 
-/**
- * Converts an RGB color value to HSL. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes r, g, and b are contained in the set [0, 255] and
- * returns h, s, and l in the set [0, 1].
- *
- * @param   Number  r       The red color value
- * @param   Number  g       The green color value
- * @param   Number  b       The blue color value
- * @return  Array           The HSL representation
- */
 function rgbToHsl(r, g, b){
     r /= 255, g /= 255, b /= 255;
     var max = Math.max(r, g, b), min = Math.min(r, g, b);
@@ -133,17 +122,6 @@ function rgbToHsl(r, g, b){
     return [h, s, l];
 }
 
-/**
- * Converts an HSL color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   Number  h       The hue
- * @param   Number  s       The saturation
- * @param   Number  l       The lightness
- * @return  Array           The RGB representation
- */
 function hslToRgb(h, s, l){
     var r, g, b;
 
@@ -164,71 +142,6 @@ function hslToRgb(h, s, l){
         r = hue2rgb(p, q, h + 1/3);
         g = hue2rgb(p, q, h);
         b = hue2rgb(p, q, h - 1/3);
-    }
-
-    return [r * 255, g * 255, b * 255];
-}
-
-/**
- * Converts an RGB color value to HSV. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
- * Assumes r, g, and b are contained in the set [0, 255] and
- * returns h, s, and v in the set [0, 1].
- *
- * @param   Number  r       The red color value
- * @param   Number  g       The green color value
- * @param   Number  b       The blue color value
- * @return  Array           The HSV representation
- */
-function rgbToHsv(r, g, b){
-    r = r/255, g = g/255, b = b/255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, v = max;
-
-    var d = max - min;
-    s = max == 0 ? 0 : d / max;
-
-    if(max == min){
-        h = 0; // achromatic
-    }else{
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
-    }
-
-    return [h, s, v];
-}
-
-/**
- * Converts an HSV color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
- * Assumes h, s, and v are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   Number  h       The hue
- * @param   Number  s       The saturation
- * @param   Number  v       The value
- * @return  Array           The RGB representation
- */
-function hsvToRgb(h, s, v){
-    var r, g, b;
-
-    var i = Math.floor(h * 6);
-    var f = h * 6 - i;
-    var p = v * (1 - s);
-    var q = v * (1 - f * s);
-    var t = v * (1 - (1 - f) * s);
-
-    switch(i % 6){
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
     }
 
     return [r * 255, g * 255, b * 255];
@@ -270,7 +183,6 @@ function borderColor(color) {
     pallete = $(this).closest('.row').find('.prefix');
     pallete.css('background-color', '#' + $(this).val());
     temp = pallete.colorcontrast('calculateYIQ', '#' + $(this).val());
-    $(this).css('border-color', bColor);
     if (temp === 'light') {
       pallete.css({'border-color': bColor, 'color': '#555'});
     } else {
@@ -285,7 +197,6 @@ function borderColor(color) {
     pallete = $(this).closest('.row').find('.prefix');
     pallete.css('background-color', '#' + $(this).val());
     temp = pallete.colorcontrast('calculateYIQ', '#' + $(this).val());
-    $(this).css('border-color', bColor);
     if (temp === 'dark') {
       pallete.css({'border-color': bColor, 'color': '#555'});
     } else {
