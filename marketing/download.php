b8d37b2b72b303c74eeb78781761ee572df8fd5b
Mark Hayes <mark@zurb.com>
2012-06-29 11:24:16 -0700

    point to production version of download generator

--- marketing/download.php
+++ marketing/download.php
@@ -42,7 +42,7 @@
         <hr />
 
         <h3 id="customizeFoundation">Customize Foundation</h3>
-        <form id="customBuild" action="http://comedian.local:9393/download" method="post">
+        <form id="customBuild" action="http://download.foundation.zurb.com/download" method="post">
 
           <div class="row">
             <div class="six columns">
