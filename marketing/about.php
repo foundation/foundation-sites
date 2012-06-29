5d3019958f853f9f1cb0e8fed11ce85a7e392043
zurbchris <chris@zurb.com>
2012-06-28 20:41:34 -0700

    added desktop wallpapers

--- marketing/about.php
+++ marketing/about.php
@@ -12,87 +12,193 @@
 
   <section id="mainContent">
 
-	 <div class="row">
-	 	<div class="four columns">
-	 		<h3>Why build it?</h3>
-	 		<p>We do a lot of front-end code, and Foundation helps us do it faster and better. We also want people to have the same tools we have to design, and we think Foundation helps with that.</p>
+	 	<div class="row">
+		 	<div class="four columns">
+		 		<h3>Why build it?</h3>
+		 		<p>We do a lot of front-end code, and Foundation helps us do it faster and better. We also want people to have the same tools we have to design, and we think Foundation helps with that.</p>
+		 	</div>
+		 	<div class="four columns">
+		 		<h3>What's the catch?</h3>
+		 		<p>If you read the EULA you'll see we have exclusive rights to your first-born. Don't worry, we'll only use them for good, not evil. Just kidding. No catch. This is all under the MIT license.</p>
+		 	</div>
+		 	<div class="four columns">
+		 		<h3>Something's wrong!</h3>
+		 		<p>So tell us! File it on <a href="http://www.github.com/zurb/foundation">Github</a> or simply <a href="mailto:foundation@zurb.com">email us</a>. We crave feedback, and we want to keep polishing Foundation until it's the best, most badass code framework you've ever seen.</p>
+		 	</div>
 	 	</div>
-	 	<div class="four columns">
-	 		<h3>What's the catch?</h3>
-	 		<p>If you read the EULA you'll see we have exclusive rights to your first-born. Don't worry, we'll only use them for good, not evil. Just kidding. No catch. This is all under the MIT license.</p>
-	 	</div>
-	 	<div class="four columns">
-	 		<h3>Something's wrong!</h3>
-	 		<p>So tell us! File it on <a href="http://www.github.com/zurb/foundation">Github</a> or simply <a href="mailto:foundation@zurb.com">email us</a>. We crave feedback, and we want to keep polishing Foundation until it's the best, most badass code framework you've ever seen.</p>
-	 	</div>
-	 </div>
 
-	 <div class="row">
-	 	<hr /><br /><br />
-	 </div>
+		<div class="row">
+			<div class="twelve columns">
+		 		<hr><br>
+		 	</div>
+		</div>
 
-	 <div class="row">
-	 	<div class="six columns">
-	 		<h3>Why a yeti?</h3>
-	 		<p>We just really like yetis, and his vaguely nerdy, icy blue visage seemed to fit. If you like the yeti even slightly as much as we do, we've prepared some fun goodies for you. Think how great it would be to see that furry, confident mug on your desktop or phone!</p>
-	 	</div>
-	 	<div class="six columns hide-for-small">
-	 		<div class="row">
-	 			<div class="six columns">
-	 				<img src="files/lurking-yeti-400x300.jpg" />
-	 				<ul class="link-list"><li>
-	 				  <li><a href="files/lurking-yeti-1024x768.jpg">1024x768</a></li>
-	 				  <li><a href="files/lurking-yeti-1440x900.jpg">1440x900</a></li>
-	 				  <li><a href="files/lurking-yeti-1680x1050.jpg">1680x1050</a></li>
-	 				  <li><a href="files/lurking-yeti-2880x1800.jpg">2880x1800</a></li>
-	 				</ul>
-	 			</div>
-	 			<div class="six columns">
-	 				<img src="files/tropical-yeti-400x300.jpg" />
-	 				<ul class="link-list"><li>
-	 				  <li><a href="files/tropical-yeti-1024x768.jpg">1024x768</a></li>
-	 				  <li><a href="files/tropical-yeti-1440x900.jpg">1440x900</a></li>
-	 				  <li><a href="files/tropical-yeti-1680x1050.jpg">1680x1050</a></li>
-	 				  <li><a href="files/tropical-yeti-2880x1800.jpg">2880x1800</a></li>
-	 				</ul>
-	 			</div>
-	 		</div>
-	 		<br /><br />
-	 		<div class="row">
-	 			<div class="six columns">
-	 				<img src="files/yeti-dance-400x300.jpg" />
-	 				<ul class="link-list"><li>
-	 				  <li><a href="files/yeti-dance-1024x768.jpg">1024x768</a></li>
-	 				  <li><a href="files/yeti-dance-1440x900.jpg">1440x900</a></li>
-	 				  <li><a href="files/yeti-dance-1680x1050.jpg">1680x1050</a></li>
-	 				  <li><a href="files/yeti-dance-2880x1800.jpg">2880x1800</a></li>
-	 				</ul>
-	 			</div>
-	 			<div class="six columns">
-	 				<img src="files/yeti-love-400x300.jpg" />
-	 				<ul class="link-list"><li>
-	 				  <li><a href="files/yeti-love-1024x768.jpg">1024x768</a></li>
-	 				  <li><a href="files/yeti-love-1440x900.jpg">1440x900</a></li>
-	 				  <li><a href="files/yeti-love-1680x1050.jpg">1680x1050</a></li>
-	 				  <li><a href="files/yeti-love-2880x1800.jpg">2880x1800</a></li>
-	 				</ul>
-	 			</div>
+		<div class="row">
+	 		<div class="twelve columns">
+	 			<h3>Why a yeti?</h3>
+	 			<p>We just really like yetis, and his vaguely nerdy, icy blue visage seemed to fit. If you like the yeti even slightly as much as we do, we've prepared some fun goodies for you. Think how great it would be to see that furry, confident mug on your desktop or phone!</p>
 	 		</div>
 	 	</div>
+
+	 	<div class="row">
+		 	<div class="twelve columns hide-for-small">
+		 		<div class="row">
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/f3/foundation-monster-th.png" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/f3/foundation-monster-1024x768.png">1024x768</a></li>
+		 				  <li><a href="files/f3/foundation-monster-1440x900.png">1440x900</a></li>
+		 				  <li><a href="files/f3/foundation-monster-1680x1050.png">1680x1050</a></li>
+		 				  <li><a href="files/f3/foundation-monster-2880x1800.png">2880x1800</a></li>
+		 				</ul>
+		 			</div>
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/f3/stretch-yeti-th.png" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/f3/stretch-yeti-1024x768.png">1024x768</a></li>
+		 				  <li><a href="files/f3/stretch-yeti-1440x900.png">1440x900</a></li>
+		 				  <li><a href="files/f3/stretch-yeti-1680x1050.png">1680x1050</a></li>
+		 				  <li><a href="files/f3/stretch-yeti-2880x1800.png">2880x1800</a></li>
+		 				</ul>
+		 			</div>
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/f3/tiny-yeti-th.png" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/f3/tiny-yeti-1024x768.png">1024x768</a></li>
+		 				  <li><a href="files/f3/tiny-yeti-1440x900.png">1440x900</a></li>
+		 				  <li><a href="files/f3/tiny-yeti-1680x1050.png">1680x1050</a></li>
+		 				  <li><a href="files/f3/tiny-yeti-2880x1800.png">2880x1800</a></li>
+		 				</ul>
+		 			</div>
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/f3/tools-th.png" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/f3/tools-1024x768.png">1024x768</a></li>
+		 				  <li><a href="files/f3/tools-1440x900.png">1440x900</a></li>
+		 				  <li><a href="files/f3/tools-1680x1050.png">1680x1050</a></li>
+		 				  <li><a href="files/f3/tools-2880x1800.png">2880x1800</a></li>
+		 				</ul>
+		 			</div>
+		 		</div>
+
+		 		<div class="row">
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/f3/yeti-lab-th.png" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/f3/yeti-lab-1024x768.png">1024x768</a></li>
+		 				  <li><a href="files/f3/yeti-lab-1440x900.png">1440x900</a></li>
+		 				  <li><a href="files/f3/yeti-lab-1680x1050.png">1680x1050</a></li>
+		 				  <li><a href="files/f3/yeti-lab-2880x1800.png">2880x1800</a></li>
+		 				</ul>
+		 			</div>
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/f3/yeti-moon-th.png" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/f3/yeti-moon-1024x768.png">1024x768</a></li>
+		 				  <li><a href="files/f3/yeti-moon-1440x900.png">1440x900</a></li>
+		 				  <li><a href="files/f3/yeti-moon-1680x1050.png">1680x1050</a></li>
+		 				  <li><a href="files/f3/yeti-moon-2880x1800.png">2880x1800</a></li>
+		 				</ul>
+		 			</div>
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/f3/yeti-vortex-th.png" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/f3/yeti-vortex-1024x768.png">1024x768</a></li>
+		 				  <li><a href="files/f3/yeti-vortex-1440x900.png">1440x900</a></li>
+		 				  <li><a href="files/f3/yeti-vortex-1680x1050.png">1680x1050</a></li>
+		 				  <li><a href="files/f3/yeti-vortex-2880x1800.png">2880x1800</a></li>
+		 				</ul>
+		 			</div>
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/f3/yetified-3-th.png" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/f3/yetified-3-1024x768.png">1024x768</a></li>
+		 				  <li><a href="files/f3/yetified-3-1440x900.png">1440x900</a></li>
+		 				  <li><a href="files/f3/yetified-3-1680x1050.png">1680x1050</a></li>
+		 				  <li><a href="files/f3/yetified-3-2880x1800.png">2880x1800</a></li>
+		 				</ul>
+		 			</div>
+		 		</div>
+
+		 		<div class="row">
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/lurking-yeti-400x300.jpg" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/lurking-yeti-1024x768.jpg">1024x768</a></li>
+		 				  <li><a href="files/lurking-yeti-1440x900.jpg">1440x900</a></li>
+		 				  <li><a href="files/lurking-yeti-1680x1050.jpg">1680x1050</a></li>
+		 				  <!-- <li><a href="files/lurking-yeti-2880x1800.jpg">2880x1800</a></li> -->
+		 				</ul>
+		 			</div>
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/tropical-yeti-400x300.jpg" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/tropical-yeti-1024x768.jpg">1024x768</a></li>
+		 				  <li><a href="files/tropical-yeti-1440x900.jpg">1440x900</a></li>
+		 				  <li><a href="files/tropical-yeti-1680x1050.jpg">1680x1050</a></li>
+		 				  <!-- <li><a href="files/tropical-yeti-2880x1800.jpg">2880x1800</a></li> -->
+		 				</ul>
+		 			</div>
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/yeti-dance-400x300.jpg" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/yeti-dance-1024x768.jpg">1024x768</a></li>
+		 				  <li><a href="files/yeti-dance-1440x900.jpg">1440x900</a></li>
+		 				  <li><a href="files/yeti-dance-1680x1050.jpg">1680x1050</a></li>
+		 				  <!-- <li><a href="files/yeti-dance-2880x1800.jpg">2880x1800</a></li> -->
+		 				</ul>
+		 			</div>
+		 			<div class="three columns">
+		 				<img style="border: solid 1px #eee;" src="files/yeti-love-400x300.jpg" />
+		 				<ul class="link-list"><li>
+		 				  <li><a href="files/yeti-love-1024x768.jpg">1024x768</a></li>
+		 				  <li><a href="files/yeti-love-1440x900.jpg">1440x900</a></li>
+		 				  <li><a href="files/yeti-love-1680x1050.jpg">1680x1050</a></li>
+		 				  <!-- <li><a href="files/yeti-love-2880x1800.jpg">2880x1800</a></li> -->
+		 				</ul>
+		 			</div>
+		 		</div>
+		 	</div>
 	 	<div class="six columns show-for-small">
 	 		<h5>iPhone Desktops</h5>
 	 		<ul class="block-grid four-up">
 	 			<li>
-	 				<a href="files/lurking-yeti-640x960.jpg"><img src="files/lurking-yeti-640x960.jpg" /></a>
+	 				<a href="files/f3/foundation-yeti-640x960.png"><img style="border: solid 1px #eee;" src="files/f3/foundation-monster-640x960.png" /></a>
 	 			</li>
 	 			<li>
-	 				<a href="files/tropical-yeti-640x960.jpg"><img src="files/tropical-yeti-640x960.jpg" /></a>
+	 				<a href="files/f3/stretch-yeti-640x960.png"><img style="border: solid 1px #eee;" src="files/f3/stretch-yeti-640x960.png" /></a>
 	 			</li>
 	 			<li>
-	 				<a href="files/yeti-dance-640x960.jpg"><img src="files/yeti-dance-640x960.jpg" /></a>
+	 				<a href="files/f3/tiny-yeti-640x960.png"><img style="border: solid 1px #eee;" src="files/f3/tiny-yeti-640x960.png" /></a>
 	 			</li>
 	 			<li>
-	 				<a href="files/yeti-love-640x960.jpg"><img src="files/yeti-love-640x960.jpg" /></a>
+	 				<a href="files/f3/tools-640x960.png"><img style="border: solid 1px #eee;" src="files/f3/tools-640x960.png" /></a>
+	 			</li>
+
+	 			<li>
+	 				<a href="files/f3/yeti-lab-640x960.png"><img style="border: solid 1px #eee;" src="files/f3/yeti-lab-640x960.png" /></a>
+	 			</li>
+	 			<li>
+	 				<a href="files/f3/yeti-moon-640x960.png"><img style="border: solid 1px #eee;" src="files/f3/yeti-moon-640x960.png" /></a>
+	 			</li>
+	 			<li>
+	 				<a href="files/f3/yeti-vortex-640x960.png"><img style="border: solid 1px #eee;" src="files/f3/yeti-vortex-640x960.png" /></a>
+	 			</li>
+	 			<li>
+	 				<a href="files/f3/yetified-3-640x960.png"><img style="border: solid 1px #eee;" src="files/f3/yetified-3-640x960.png" /></a>
+	 			</li>
+
+	 			<li>
+	 				<a href="files/f3/lurking-yeti-640x960.jpg"><img style="border: solid 1px #eee;" src="files/lurking-yeti-640x960.jpg" /></a>
+	 			</li>
+	 			<li>
+	 				<a href="files/tropical-yeti-640x960.jpg"><img style="border: solid 1px #eee;" src="files/tropical-yeti-640x960.jpg" /></a>
+	 			</li>
+	 			<li>
+	 				<a href="files/yeti-dance-640x960.jpg"><img style="border: solid 1px #eee;" src="files/yeti-dance-640x960.jpg" /></a>
+	 			</li>
+	 			<li>
+	 				<a href="files/yeti-love-640x960.jpg"><img style="border: solid 1px #eee;" src="files/yeti-love-640x960.jpg" /></a>
 	 			</li>
 	 		</ul>
 	 	</div>
