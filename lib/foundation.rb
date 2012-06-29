6a10a73d7da784aae84847431d915965cf7bc5a4
Mark Hayes <mark@zurb.com>
2012-06-29 14:07:39 -0700

    bumped version

--- lib/foundation.rb
+++
@@ -1,3 +0,0 @@
-# Ensure auto-import of assets works whether gem built from source or
-# from zurb-foundation gem on rubygems
-require "zurb-foundation"
--- lib/foundation/version.rb
+++ lib/foundation/version.rb
@@ -1,3 +1,3 @@
 module Foundation
-  VERSION = "3.0.0.rc1"
+  VERSION = "3.0.0.rc2"
 end
