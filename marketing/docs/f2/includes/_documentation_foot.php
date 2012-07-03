7dccbfe6c4e8eae1c57ff29746b7b06e3079f1e9
Jonathan Smiley <jonathan@zurb.com>
2012-07-03 08:48:30 -0700

    Updated the tracking code for foundation docs

--- marketing/docs/f2/includes/_documentation_foot.php
+++ marketing/docs/f2/includes/_documentation_foot.php
@@ -69,21 +69,18 @@
 	<script src="javascripts/app.js"></script>
 	<script src="presentation.js"></script>
 
-  <script> 
-
+  <script type="text/javascript">
+  
     var _gaq = _gaq || [];
-  	_gaq.push(
-  	  ['_setAccount', 'UA-2195009-2'],
-  	  ['_trackPageview'],
-  	  ['b._setAccount', 'UA-2195009-27'],
-  	  ['b._trackPageview'] 
-  	);
-
+    _gaq.push(['_setAccount', 'UA-2195009-27']);
+    _gaq.push(['_trackPageview']);
+  
     (function() {
       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
     })();
+  
   </script>
 </body>
 </html>
