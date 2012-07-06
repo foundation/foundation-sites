7773c62e3eac50b1856cc687301fda3ceec1b8d2
Mark Hayes <mark@zurb.com>
2012-07-06 14:51:51 -0700

    define convert-number-to-words within sass

--- lib/zurb-foundation.rb
+++ lib/zurb-foundation.rb
@@ -3,7 +3,6 @@ require "compass"
 extension_path = File.expand_path(File.dirname(File.dirname(__FILE__)))
 config = {:path => extension_path}
 Compass::Frameworks.register("foundation", config)
-require "foundation/sass_script_functions"
 
 module Foundation
   require "foundation/engine" if defined?(Rails)
