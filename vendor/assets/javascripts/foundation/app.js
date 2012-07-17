5b5004d8b858bc07ba6a4fbbdbb8497df569418b
Neal <me@nealke.mp>
2012-07-17 16:10:26 -0700

    Reverting change to fix button glitch.

--- vendor/assets/javascripts/foundation/app.js
+++ vendor/assets/javascripts/foundation/app.js
@@ -23,7 +23,6 @@ jQuery(document).ready(function ($) {
 
   $('dl.tabs dd a').on('click.fndtn', function (event) {
     activateTab($(this).parent('dd'));
-    return false;
   });
 
   if (window.location.hash) {
