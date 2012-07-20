a6255deaef90b2e22e1ad2fd304779c52f6879c5
Mark Hayes <mark@zurb.com>
2012-07-20 12:05:49 -0700

    update javascript sprockets manifest file

--- vendor/assets/javascripts/foundation/index.js
+++ vendor/assets/javascripts/foundation/index.js
@@ -1,12 +1,13 @@
 /*
 =require foundation/modernizr.foundation
-=require foundation/jquery.foundation.forms
+=require foundation/jquery.placeholder
 =require foundation/jquery.foundation.alerts
 =require foundation/jquery.foundation.buttons
+=require foundation/jquery.foundation.tooltips
+=require foundation/jquery.foundation.forms
 =require foundation/jquery.foundation.tabs
 =require foundation/jquery.foundation.navigation
-=require foundation/jquery.tooltips
-=require foundation/jquery.placeholder
-=require foundation/jquery.orbit-1.4.0
-=require foundation/jquery.reveal
+=require foundation/jquery.foundation.reveal
+=require foundation/jquery.foundation.orbit
+=require foundation/app
 */
