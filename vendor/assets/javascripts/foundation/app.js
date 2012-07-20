98c916494329af9cd71ec1fae9c1d9677e7f9d5b
Mark Hayes <mark@zurb.com>
2012-07-20 12:02:14 -0700

    only IE8+ is officially supported

--- vendor/assets/javascripts/foundation/app.js
+++ vendor/assets/javascripts/foundation/app.js
@@ -10,12 +10,11 @@
     $(document).tooltips();
     $('input, textarea').placeholder();
     
-    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids
+    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
     // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
     // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
     // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
     // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
-    
   });
   
 })(jQuery);
