5862588aa805df0b92f6f31774418521ea17afaa
Mark Hayes <mark@zurb.com>
2012-07-20 15:37:42 -0700

    updated compass template layout

--- lib/zurb-foundation.rb
+++ lib/zurb-foundation.rb
@@ -1,9 +1,10 @@
-require "foundation/version"
-require "compass"
-extension_path = File.expand_path(File.dirname(File.dirname(__FILE__)))
-config = {:path => extension_path}
-Compass::Frameworks.register("foundation", config)
+require 'foundation/version'
+require 'compass'
+extension_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+Compass::Frameworks.register('foundation', :path => extension_path)
 
 module Foundation
   require "foundation/engine" if defined?(Rails)
 end
+
+
