eed7f3fbfeece91696c6e172cf54af08f8dff803
Mark Hayes <mark@zurb.com>
2012-07-23 10:38:25 -0700

    initialize all plugins in app.js file

--- vendor/assets/javascripts/foundation/app.js
+++ vendor/assets/javascripts/foundation/app.js
@@ -1,34 +1,21 @@
-(function($){
-  
+(function($){  
 
-  /* SPLIT BUTTONS/DROPDOWNS */
-  $('.button.dropdown > ul').addClass('no-hover');
-
-  $(document).on('click.fndtn', '.button.dropdown', function (e) {
-    e.stopPropagation();
+  $(function(){
+    $(document).foundationAlerts();
+    $(document).foundationButtons();
+    $(document).foundationAccordion();
+    $(document).foundationNavigation();
+    $(document).foundationCustomForms();
+    $(document).foundationTabs({callback:$.foundation.customForms.appendCustomMarkup});
+    
+    $(document).tooltips();
+    $('input, textarea').placeholder();
+    
+    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
+    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
+    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
+    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
   });
-  $(document).on('click.fndtn', '.button.dropdown.split span', function (e) {
-    e.preventDefault();
-    $('.button.dropdown').not($(this).parent()).children('ul').removeClass('show-dropdown');
-    $(this).siblings('ul').toggleClass('show-dropdown');
-  });
-  $('.button.dropdown').not('.split').on('click.fndtn', function (e) {
-    $('.button.dropdown').not(this).children('ul').removeClass('show-dropdown');
-    $(this).children('ul').toggleClass('show-dropdown');
-  });
-  $('body, html').on('click.fndtn', function () {
-    $('.button.dropdown ul').removeClass('show-dropdown');
-  });
-
-  // Positioning the Flyout List
-  var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny)').outerHeight() - 1,
-      largeButtonHeight   = $('.button.large.dropdown').outerHeight() - 1,
-      smallButtonHeight   = $('.button.small.dropdown').outerHeight() - 1,
-      tinyButtonHeight    = $('.button.tiny.dropdown').outerHeight() - 1;
-
-  $('.button.dropdown:not(.large):not(.small):not(.tiny) > ul').css('top', normalButtonHeight);
-  $('.button.dropdown.large > ul').css('top', largeButtonHeight);
-  $('.button.dropdown.small > ul').css('top', smallButtonHeight);
-  $('.button.dropdown.tiny > ul').css('top', tinyButtonHeight);
   
 })(jQuery);
