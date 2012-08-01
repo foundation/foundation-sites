d0fcf817008fea6d673d4ffb564af920e6b6626f
Mark Hayes <mark@zurb.com>
2012-08-01 12:40:48 -0700

    setup test/ directory for local testing of the gem

---
+++ test/config.rb
@@ -0,0 +1,12 @@
+require File.join(File.dirname(__FILE__),"../lib/zurb-foundation")
+# require "foundation"
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "../vendor/assets/images"
+# images_path = "../vendor/assets/images"
+line_comments = false
+preferred_syntax = :scss
+output_style = :compact
+relative_assets = true
