696ef2a4daa97cd7a672618ae56480a377eae062
Mark Hayes <mark@zurb.com>
2012-08-01 12:38:29 -0700

    update lib

--- lib/zurb-foundation.rb
+++ lib/zurb-foundation.rb
@@ -1,11 +1,18 @@
-require 'foundation/version'
+root = File.join(File.dirname(__FILE__), "..")
+require "foundation/version"
+require "compass"
+# require "modular-scale"
+
+Compass::Frameworks.register("foundation",
+  :stylesheets_directory => File.join(root,"scss"),
+  :templates_directory => File.join(root,"templates"),
+  :images_dir => File.join(root,"vendor","assets","images","foundation")
+)
+
 if defined?(Rails)
   require "foundation/generators/layout_generator"
   require "foundation/generators/install_generator"
 end
-require 'compass'
-extension_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-Compass::Frameworks.register('foundation', :path => extension_path)
 
 module Foundation
   require "foundation/engine" if defined?(Rails)
