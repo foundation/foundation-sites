bbc1b90b4ad0b3cca4b5f8ba7410223d8686344b
Mark Hayes <mark@zurb.com>
2012-08-07 15:16:52 -0700

    compile stylesheets into stylesheets directory

--- test/config.rb
+++ test/config.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__),"../lib/zurb-foundation")
 project_type = :stand_alone
 http_path = "/"
 sass_dir = "scss"
-css_dir = "css"
+css_dir = "stylesheets"
 images_dir = "../vendor/assets/images"
 # images_path = "../vendor/assets/images"
 line_comments = false
