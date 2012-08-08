95bf2373cbcb0d4c39610076d6b0053ffab80da9
Nic Haynes <nic@nicinabox.com>
2012-08-08 15:43:56 -0500

    Fix event delegation on accordion

--- vendor/assets/javascripts/foundation/jquery.foundation.accordion.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.accordion.js
@@ -1,15 +1,15 @@
 (function ($){
-  
+
   $.fn.foundationAccordion = function (options) {
-    
-    $('.accordion li', this).on('click.fndtn', function () {
+
+    $(document).on('click.fndtn', '.accordion li', function () {
       var flyout = $(this).children('.content').first();
       $('.accordion .content').not(flyout).hide().parent('li').removeClass('active');
       flyout.show(0, function () {
         flyout.parent('li').addClass('active');
       });
     });
-        
+
   };
 
 })( jQuery );
