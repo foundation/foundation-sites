7c13a85cc3a1fbd36019d089d14c4ae56b910b7a
Nic Haynes <nic@nicinabox.com>
2012-08-08 16:05:06 -0500

    Fix event delegation on buttons

--- vendor/assets/javascripts/foundation/jquery.foundation.buttons.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.buttons.js
@@ -1,33 +1,33 @@
 (function ($) {
-  
-  $.fn.foundationButtons = function(options) {    
+
+  $.fn.foundationButtons = function(options) {
     // Prevent event propagation on disabled buttons
-    $('.button.disabled', this).on('click.fndtn', function (event) {
-      event.preventDefault();
+    $(document).on('click.fndtn', '.button.disabled', function (e) {
+      e.preventDefault();
     });
-    
+
     $('.button.dropdown > ul', this).addClass('no-hover');
 
-    $('.button.dropdown', this).on('click.fndtn', function (e) {
+    $(document).on('click.fndtn', '.button.dropdown', function (e) {
       // Stops further propagation of the event up the DOM tree when clicked on the button.
       // Events fired by its descendants are not being blocked.
       if (e.target === this) {
         e.stopPropagation();
       }
     });
-    $('.button.dropdown.split span', this).on('click.fndtn', function (e) {
+    $(document).on('click.fndtn', '.button.dropdown.split span', function (e) {
       e.preventDefault();
       $('.button.dropdown', this).not($(this).parent()).children('ul').removeClass('show-dropdown');
       $(this).siblings('ul').toggleClass('show-dropdown');
     });
-    $('.button.dropdown', this).not('.split').on('click.fndtn', function (e) {
+    $(document).on('click.fndtn', '.button.dropdown:not(.split)', function (e) {
       $('.button.dropdown', this).not(this).children('ul').removeClass('show-dropdown');
       $(this).children('ul').toggleClass('show-dropdown');
     });
     $('body, html').on('click.fndtn', function () {
       $('.button.dropdown ul').removeClass('show-dropdown');
     });
-    
+
     // Positioning the Flyout List
     var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny)', this).outerHeight() - 1,
         largeButtonHeight   = $('.button.large.dropdown', this).outerHeight() - 1,
@@ -43,7 +43,7 @@
     $('.button.dropdown.up.large > ul', this).css('top', 'auto').css('bottom', largeButtonHeight - 2);
     $('.button.dropdown.up.small > ul', this).css('top', 'auto').css('bottom', smallButtonHeight - 2);
     $('.button.dropdown.up.tiny > ul', this).css('top', 'auto').css('bottom', tinyButtonHeight - 2);
-    
+
   };
 
 })( jQuery );
