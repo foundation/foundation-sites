6559ccc3aebb0e7d905c183c6f35494eb4910871
Mark Hayes <mark@zurb.com>
2012-08-10 10:31:40 -0700

    automatically discover and link javascripts in template file

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -5,7 +5,6 @@ stylesheet 'scss/_settings.scss',                         :to => '_settings.scss
 stylesheet 'scss/app.scss',                               :to => 'app.scss', :media => "screen, projector, print"
 
 # Make sure you list all the project template files here in the manifest.
-html 'index.html', :erb => true # use Foundation::VERSION
 file 'humans.txt'
 file 'robots.txt'
 file 'MIT-LICENSE.txt'
@@ -15,7 +14,8 @@ file 'MIT-LICENSE.txt'
 # outside the compass template
 def copy_images_from(relative_path, prefix_path)
   absolute_path = File.join(File.dirname(__FILE__), relative_path, prefix_path)
-  Dir.glob("#{absolute_path}/*.*") do |img|
+  img_files = Dir.glob("#{absolute_path}/*.*")
+  img_files.each do |img|
     image "#{relative_path}/#{prefix_path}/#{File.basename(img)}", 
       :to => "#{prefix_path}/#{File.basename(img)}"
   end
@@ -29,10 +29,22 @@ def copy_js_from(relative_path, prefix_path, excludes=[])
     javascript "#{relative_path}/#{prefix_path}/#{File.basename(js)}", 
       :to => "#{prefix_path}/#{File.basename(js)}"
   end
+  return js_files.map {|f| "#{prefix_path}/#{File.basename(f)}"}
 end
 
 copy_images_from("../../vendor/assets/images", "foundation/orbit")
-copy_js_from("../../vendor/assets/javascripts", "foundation", ["index.js"])
+javascripts = copy_js_from("../../vendor/assets/javascripts", "foundation", ["index.js"])
+
+javascripts.reject! do |f|   
+  [
+    "jquery.js",
+    "modernizr.foundation.js",
+    "app.js",
+    "jquery.offcanvas.js"
+  ].include?(File.basename(f))
+end
+
+html 'index.html', :erb => true, :javascripts => javascripts
 
 help %Q{
 
