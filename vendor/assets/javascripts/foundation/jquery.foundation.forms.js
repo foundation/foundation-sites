73ad10ab779f0a354c69638102755e3f382e8171
Mark Hayes <mark@zurb.com>
2012-08-16 14:50:57 -0700

    custom checkboxes should trigger change event

--- vendor/assets/javascripts/foundation/jquery.foundation.forms.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.forms.js
@@ -369,6 +369,8 @@
     if (false === $input.is(':disabled')) {
         input.checked = ((input.checked) ? false : true);
         $element.toggleClass('checked');
+        
+        $input.trigger('change');
     }
   };
 
