e025efe5034bea85e468e000a57df1920201b878
zurbchris <chris@zurb.com>
2012-08-29 16:10:59 -0700

    update

--- vendor/assets/javascripts/foundation/jquery.foundation.topbar.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.topbar.js
@@ -3,27 +3,12 @@
     var currentIndex = 0,
         breakPoint = 800;
 
-    // var currentIndex = 0;
-
-    // define on() and off() for older jQuery
-    if (!$.isFunction($.fn.on)) {
-      $.fn.on = function(types, sel, fn) {
-        return this.delegate(sel, types, fn); 
-      };
-      
-      $.fn.off = function(types, sel, fn) {
-        return this.undelegate(sel, types, fn);
-      };
-    }
-
+    // Define Breakpoint for small layout
     function atBreakpoint() {
       return $(window).width() < breakPoint;
     }
 
-    // function onMobile() {
-    //   return $(window).width() < 768;
-    // }
-    
+    // Function to inject new markup for small layout
     function initializeMarkup($topbar) {
       var $section = $topbar.children('section');
       
@@ -33,39 +18,19 @@
       $section.find('.has-dropdown>a').each(function () {
         var $link = $(this),
             $dropdown = $link.siblings('.dropdown'),
-            $titleLi = $('<li class="title show-on-phones js-generated"><h5></h5></li>');
+            $titleLi = $('<li class="title js-generated"><h5></h5></li>');
         
         // Copy link to subnav
         $titleLi.find('h5').html($link.html());
         $dropdown.prepend($titleLi);
-        $dropdown.prepend('<li class="back show-on-phones js-generated"><a href="">&larr; Back</a></li>');
+        $dropdown.prepend('<li class="back js-generated"><a href="">&larr; Back</a></li>');
       });
       
       // Put element back in the DOM
       $section.appendTo($topbar);
     }
 
-    // function initializeMarkup($topbar) {
-    //   var $attached = $topbar.find('.attached');
-      
-    //   // Pull element out of the DOM for manipulation
-    //   $attached.detach();
-      
-    //   $attached.find('li.has-dropdown>a').each(function () {
-    //     var $link = $(this),
-    //         $dropdown = $link.siblings('ul.dropdown'),
-    //         $titleLi = $('<li class="title show-on-phones js-generated"><h5></h5></li>');
-        
-    //     // Copy link to subnav
-    //     $titleLi.find('h5').html($link.html());
-    //     $dropdown.prepend($titleLi);
-    //     $dropdown.prepend('<li class="back show-on-phones js-generated"><a href="">&larr; Back</a></li>');
-    //   });
-      
-    //   // Put element back in the DOM
-    //   $attached.appendTo($topbar);
-    // }
-    
+    // Expand the menu on click
     $('.top-bar .toggle-nav').on('click', function(e) {
       var $this = $(this);
 
@@ -80,23 +45,8 @@
         $this.closest('.top-bar').toggleClass('expanded');
       }
     });
-
-    // $('.top-bar .name').live('click', function (event) {
-    //   var $this = $(this);
-      
-    //   if (onMobile()) {
-    //     event.preventDefault();
-
-    //     if (!$this.hasClass('top-bar-initialized')) {
-    //       initializeMarkup($this.closest('.top-bar'));
-    //       $this.addClass('top-bar-initialized');
-    //     }
-
-    //     $this.closest('.top-bar').toggleClass('expanded');
-    //   }
-    // });
     
-    
+    // Show the Dropdown Levels on Click
     $('.top-bar .has-dropdown>a').on('click', function(e) {
       if (atBreakpoint()) {
         var $this = $(this),
@@ -126,60 +76,40 @@
         }
       }
     });
-
-    // $('.top-bar .has-dropdown>a').live('click', function (event) {
-    //   if (onMobile()) {
-    //     var $this = $(this),
-    //         $selectedLi = $this.closest('li'),
-    //         $nextLevelUl = $selectedLi.find('>ul'),
-    //         $titleLi = $('<li class="title show-on-phones js-generated"><h5></h5></li>'),
-    //         $attached = $this.closest('.attached'),
-    //         $topbar = $this.closest('.top-bar'),
-    //         $largestUl;
-
-    //     event.preventDefault();
-        
-    //     currentIndex += 1;
-        
-    //     $selectedLi.addClass('active');
-    //     $attached.css({'left': '-' + 100 * currentIndex + '%'});
-    //     $attached.find('>.name').css({'left': 100 * currentIndex + '%'});
-        
-    //     if (currentIndex === 1) {
-    //       $largestUl = $nextLevelUl;
-    //       $nextLevelUl.find('ul.dropdown').each(function () {
-    //         if ($(this).height() > $largestUl.height()) {
-    //           $largestUl = $(this);
-    //         }
-    //       });
-    //       $attached.css({'height': $largestUl.height() + 45 + 'px'});
-    //     }
-    //   }
-    // });
     
+    // Go up a level on Click
+    $('.top-bar .has-dropdown').on('click', '.back', function(e) {
+      var $this = $(this),
+          $activeLi = $this.closest('li.active'),
+          $section = $this.closest('section'),
+          $topbar = $this.closest('.top-bar'),
+          $previousLevelUl = $activeLi.closest('ul');
+      
+      e.preventDefault();
+      
+      currentIndex -= 1;
+      
+      $section.css({'left': '-' + 100 * currentIndex + '%'});
+      $section.find('>.name').css({'left': 100 * currentIndex + '%'});
+      
+      if (currentIndex === 0) {
+        $section.css({'height': ''});
+      }
+      
+      setTimeout(function () {
+        $activeLi.removeClass('active');
+      }, 400);
+    });
 
-
-    // $('.top-bar .has-dropdown .back').live('click', function (event) {
-    //   var $this = $(this),
-    //       $activeLi = $this.closest('li.active'),
-    //       $attached = $this.closest('.attached'),
-    //       $topbar = $this.closest('.top-bar'),
-    //       $previousLevelUl = $activeLi.closest('ul');
+    // define on() and off() for older jQuery
+    if (!$.isFunction($.fn.on)) {
+      $.fn.on = function(types, sel, fn) {
+        return this.delegate(sel, types, fn); 
+      };
       
-    //   event.preventDefault();
-      
-    //   currentIndex -= 1;
-      
-    //   $attached.css({'left': '-' + 100 * currentIndex + '%'});
-    //   $attached.find('>.name').css({'left': 100 * currentIndex + '%'});
-      
-    //   if (currentIndex === 0) {
-    //     $attached.css({'height': ''});
-    //   }
-      
-    //   setTimeout(function () {
-    //     $activeLi.removeClass('active');
-    //   }, 200);
-    // });
+      $.fn.off = function(types, sel, fn) {
+        return this.undelegate(sel, types, fn);
+      };
+    }
 
 }(jQuery));
