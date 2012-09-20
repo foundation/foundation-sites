7bde8371b4fc4b7739c89078fa9ca2bbed89c91c
Jordan Humphreys <jordan@mailyard.net>
2012-09-20 08:49:33 -0700

    Add hover state support for desktop accordions.

--- vendor/assets/javascripts/foundation/jquery.foundation.accordion.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.accordion.js
@@ -2,15 +2,32 @@
   'use strict';
 
   $.fn.foundationAccordion = function (options) {
+    var $accordion = $('.accordion');
 
-    $('.accordion li', this).on('click.fndtn', function () {
-    var p = $(this).parent(); //changed this
-      var flyout = $(this).children('.content').first();
-      $('.content', p).not(flyout).hide().parent('li').removeClass('active'); //changed this
-      flyout.show(0, function () {
-        flyout.parent('li').addClass('active');
+    if ($accordion.hasClass('hover') && !Modernizr.touch) {
+      $('.accordion li', this).on({
+        mouseenter : function () {
+          console.log('due');
+          var p = $(this).parent(),
+            flyout = $(this).children('.content').first();
+
+          $('.content', p).not(flyout).hide().parent('li').removeClass('active'); //changed this
+          flyout.show(0, function () {
+            flyout.parent('li').addClass('active');
+          });
+        }
       });
-    });
+    } else {
+      $('.accordion li', this).on('click.fndtn', function () {
+        var p = $(this).parent(),
+            flyout = $(this).children('.content').first();
+
+        $('.content', p).not(flyout).hide().parent('li').removeClass('active'); //changed this
+        flyout.show(0, function () {
+          flyout.parent('li').addClass('active');
+        });
+      });
+    }
 
   };
 
