79d950f3ccf7be90d902ada17c04880bd160eee2
nezuvian <nezuvian@gmail.com>
2012-10-02 21:25:39 +0200

    js

--- vendor/assets/javascripts/foundation/app.js
+++ vendor/assets/javascripts/foundation/app.js
@@ -1,134 +1,36 @@
-jQuery(document).ready(function ($) {
+;(function ($, window, undefined) {
+  'use strict';
 
-  /* Use this js doc for all application specific JS */
+  var $doc = $(document),
+      Modernizr = window.Modernizr;
 
-  /* TABS --------------------------------- */
-  /* Remove if you don't need :) */
+  $(document).ready(function() {
+    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
+    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
+    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
+    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
+    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
+    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
+    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
+    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
+    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
 
-  function activateTab($tab) {
-    var $activeTab = $tab.closest('dl').find('dd.active'),
-        contentLocation = $tab.children('a').attr("href") + 'Tab';
-
-    // Strip off the current url that IE adds
-    contentLocation = contentLocation.replace(/^.+#/, '#');
-
-    //Make Tab Active
-    $activeTab.removeClass('active');
-    $tab.addClass('active');
-
-    //Show Tab Content
-    $(contentLocation).closest('.tabs-content').children('li').removeClass('active').hide();
-    $(contentLocation).css('display', 'block').addClass('active');
-  }
-
-  $('dl.tabs dd a').on('click.fndtn', function (event) {
-    activateTab($(this).parent('dd'));
+    $('input, textarea').placeholder();
   });
 
-  if (window.location.hash) {
-    activateTab($('a[href="' + window.location.hash + '"]').parent('dd'));
-    $.foundation.customForms.appendCustomMarkup();
-  }
+  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
+  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
+  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
+  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
-  /* ALERT BOXES ------------ */
-  $(".alert-box").delegate("a.close", "click", function(event) {
-    event.preventDefault();
-    $(this).closest(".alert-box").fadeOut(function(event){
-      $(this).remove();
-    });
-  });
-
-  /* PLACEHOLDER FOR FORMS ------------- */
-  /* Remove this and jquery.placeholder.min.js if you don't need :) */
-  $('input, textarea').placeholder();
-
-  /* TOOLTIPS ------------ */
-  $(this).tooltips();
-
-  /* UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids */
-  //  $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
-  //  $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
-  //  $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
-  //  $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
-
-
-  /* DROPDOWN NAV ------------- */
-
-  var lockNavBar = false;
-  /* Windows Phone, sadly, does not register touch events :( */
-  if (Modernizr.touch || navigator.userAgent.match(/Windows Phone/i)) {
-    $('.nav-bar a.flyout-toggle').on('click.fndtn touchstart.fndtn', function(e) {
-      e.preventDefault();
-      var flyout = $(this).siblings('.flyout').first();
-      if (lockNavBar === false) {
-        $('.nav-bar .flyout').not(flyout).slideUp(500);
-        flyout.slideToggle(500, function(){
-          lockNavBar = false;
-        });
-      }
-      lockNavBar = true;
-    });
-    $('.nav-bar>li.has-flyout').addClass('is-touch');
-  } else {
-    $('.nav-bar>li.has-flyout').hover(function() {
-      $(this).children('.flyout').show();
-    }, function() {
-      $(this).children('.flyout').hide();
+  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
+  if (Modernizr.touch && !window.location.hash) {
+    $(window).load(function () {
+      setTimeout(function () {
+        window.scrollTo(0, 1);
+      }, 0);
     });
   }
 
-  // Accordion
-  $('.accordion li').on('click.fndtn', function() {
-    var flyout = $(this).children('.content').first();
-    $('.accordion .content').not(flyout).hide().parent('li').removeClass('active');
-    flyout.show(0, function() {
-      flyout.parent('li').addClass('active');
-    });
-  });
-
-  /* DISABLED BUTTONS ------------- */
-  /* Gives elements with a class of 'disabled' a return: false; */
-  $('.button.disabled').on('click.fndtn', function (event) {
-    event.preventDefault();
-  });
-  
-
-  /* SPLIT BUTTONS/DROPDOWNS */
-  $('.button.dropdown > ul').addClass('no-hover');
-
-  $('.button.dropdown').on('click.fndtn', function (e) {
-    e.stopPropagation();
-  });
-  $('.button.dropdown.split span').on('click.fndtn', function (e) {
-    e.preventDefault();
-    $('.button.dropdown').not($(this).parent()).children('ul').removeClass('show-dropdown');
-    $(this).siblings('ul').toggleClass('show-dropdown');
-  });
-  $('.button.dropdown').not('.split').on('click.fndtn', function (e) {
-    $('.button.dropdown').not(this).children('ul').removeClass('show-dropdown');
-    $(this).children('ul').toggleClass('show-dropdown');
-  });
-  $('body, html').on('click.fndtn', function () {
-    $('.button.dropdown ul').removeClass('show-dropdown');
-  });
-
-  // Positioning the Flyout List
-  var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny)').outerHeight() - 1,
-      largeButtonHeight   = $('.button.large.dropdown').outerHeight() - 1,
-      smallButtonHeight   = $('.button.small.dropdown').outerHeight() - 1,
-      tinyButtonHeight    = $('.button.tiny.dropdown').outerHeight() - 1;
-
-  $('.button.dropdown:not(.large):not(.small):not(.tiny) > ul').css('top', normalButtonHeight);
-  $('.button.dropdown.large > ul').css('top', largeButtonHeight);
-  $('.button.dropdown.small > ul').css('top', smallButtonHeight);
-  $('.button.dropdown.tiny > ul').css('top', tinyButtonHeight);
-  
-  $('.button.dropdown.up:not(.large):not(.small):not(.tiny) > ul').css('top', 'auto').css('bottom', normalButtonHeight - 2);
-  $('.button.dropdown.up.large > ul').css('top', 'auto').css('bottom', largeButtonHeight - 2);
-  $('.button.dropdown.up.small > ul').css('top', 'auto').css('bottom', smallButtonHeight - 2);
-  $('.button.dropdown.up.tiny > ul').css('top', 'auto').css('bottom', tinyButtonHeight - 2);
-
-  /* CUSTOM FORMS */
-  $.foundation.customForms.appendCustomMarkup();
-
-});
+})(jQuery, this);
--- vendor/assets/javascripts/foundation/index.js
+++ vendor/assets/javascripts/foundation/index.js
@@ -1,8 +1,16 @@
 /*
 =require foundation/modernizr.foundation
-=require foundation/jquery.customforms
-=require foundation/jquery.orbit-1.4.0
-=require foundation/jquery.placeholder.min
-=require foundation/jquery.reveal
-=require foundation/jquery.tooltips
+=require foundation/jquery.placeholder
+=require foundation/jquery.foundation.alerts
+=require foundation/jquery.foundation.accordion
+=require foundation/jquery.foundation.buttons
+=require foundation/jquery.foundation.tooltips
+=require foundation/jquery.foundation.forms
+=require foundation/jquery.foundation.tabs
+=require foundation/jquery.foundation.navigation
+=require foundation/jquery.foundation.topbar
+=require foundation/jquery.foundation.reveal
+=require foundation/jquery.foundation.orbit
+=require foundation/jquery.foundation.mediaQueryToggle
+=require foundation/app
 */
