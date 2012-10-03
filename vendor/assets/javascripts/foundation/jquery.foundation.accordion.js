1bdff48063548b6dbfc9dde73dee64ff3102a39d
nezuvian <nezuvian@gmail.com>
2012-10-04 00:51:39 +0200

    adding collapsible behaviour

--- vendor/assets/javascripts/foundation/jquery.foundation.accordion.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.accordion.js
@@ -2,32 +2,52 @@
   'use strict';
 
   $.fn.foundationAccordion = function (options) {
-    var $accordion = $('.accordion');
+    var $accordions = $('.accordion');
 
-    if ($accordion.hasClass('hover') && !Modernizr.touch) {
-      $('.accordion li', this).on({
-        mouseenter : function () {
-          console.log('due');
+    $accordions.each(function(i, accordion) {
+      if ($(accordion).hasClass('hover') && !Modernizr.touch) {
+        $(accordion).children('li').on({
+          mouseenter : function () {
+            var p = $(this).parent(),
+                flyout = $(this).children('.content').first();
+
+            $('.content', p).not(flyout).hide().parent('li').removeClass('active'); //changed this
+            flyout.show(0, function () {
+              flyout.parent('li').addClass('active');
+            });
+          }
+        });
+      }
+
+      if ($(accordion).hasClass('collapsible')) {
+        $(accordion).children('li').children('.title').on({
+          click : function () {
+            var flyout = $(this).next('.content');
+
+            if($(this).parent('li').hasClass('active')) {
+              flyout.hide().parent('li').removeClass('active');
+            } else {
+              $(accordion).children('li').children('.content').not(flyout).hide().parent('li').removeClass('active');
+              flyout.show(0, function() {
+                  flyout.parent('li').addClass('active');
+                });
+            }
+          }
+        });
+      }
+
+      if ( $(accordion).hasClass('hover') === $(accordion).hasClass('collapsible') === false || Modernizr.touch) {
+        $('.accordion li', this).on('click.fndtn', function () {
           var p = $(this).parent(),
-            flyout = $(this).children('.content').first();
+              flyout = $(this).children('.content').first();
 
           $('.content', p).not(flyout).hide().parent('li').removeClass('active'); //changed this
           flyout.show(0, function () {
             flyout.parent('li').addClass('active');
           });
-        }
-      });
-    } else {
-      $('.accordion li', this).on('click.fndtn', function () {
-        var p = $(this).parent(),
-            flyout = $(this).children('.content').first();
-
-        $('.content', p).not(flyout).hide().parent('li').removeClass('active'); //changed this
-        flyout.show(0, function () {
-          flyout.parent('li').addClass('active');
         });
-      });
-    }
+      }
+    });
 
   };
 
