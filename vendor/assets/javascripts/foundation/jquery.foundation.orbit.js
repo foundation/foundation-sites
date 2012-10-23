8f0ba77942e3f17dbc6a1f85be80d82c04d445b9
Mark Hayes <mark@zurb.com>
2012-10-23 09:52:11 -0700

    cleaned up orbit comments

--- vendor/assets/javascripts/foundation/jquery.foundation.orbit.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.orbit.js
@@ -21,27 +21,27 @@
 
     defaults: {
       animation: 'horizontal-push',     // fade, horizontal-slide, vertical-slide, horizontal-push, vertical-push
-      animationSpeed: 600,        // how fast animtions are
-      timer: true,            // true or false to have the timer
-      advanceSpeed: 4000,         // if timer is enabled, time between transitions
-      pauseOnHover: false,        // if you hover pauses the slider
-      startClockOnMouseOut: false,    // if clock should start on MouseOut
+      animationSpeed: 600,              // how fast animations are
+      timer: true,                      // display timer?
+      advanceSpeed: 4000,               // if timer is enabled, time between transitions
+      pauseOnHover: false,              // if you hover pauses the slider
+      startClockOnMouseOut: false,      // if clock should start on MouseOut
       startClockOnMouseOutAfter: 1000,  // how long after MouseOut should the timer start again
-      directionalNav: true,         // manual advancing directional navs
+      directionalNav: true,             // manual advancing directional navs
       directionalNavRightText: 'Right', // text of right directional element for accessibility
-      directionalNavLeftText: 'Left', // text of left directional element for accessibility
-      captions: true,           // do you want captions?
-      captionAnimation: 'fade',       // fade, slideOpen, none
-      captionAnimationSpeed: 600,     // if so how quickly should they animate in
-      resetTimerOnClick: false,      // true resets the timer instead of pausing slideshow progress on manual navigation
-      bullets: false,           // true or false to activate the bullet navigation
-      bulletThumbs: false,        // thumbnails for the bullets
-      bulletThumbLocation: '',      // location from this file where thumbs will be
-      afterSlideChange: $.noop,   // empty function
-      afterLoadComplete: $.noop, //callback to execute after everything has been loaded
+      directionalNavLeftText: 'Left',   // text of left directional element for accessibility
+      captions: true,                   // do you want captions?
+      captionAnimation: 'fade',         // fade, slideOpen, none
+      captionAnimationSpeed: 600,       // if so how quickly should they animate in
+      resetTimerOnClick: false,         // true resets the timer instead of pausing slideshow progress on manual navigation
+      bullets: false,                   // true or false to activate the bullet navigation
+      bulletThumbs: false,              // thumbnails for the bullets
+      bulletThumbLocation: '',          // relative path to thumbnails from this file
+      afterSlideChange: $.noop,         // callback to execute after slide changes
+      afterLoadComplete: $.noop,        // callback to execute after everything has been loaded
       fluid: true,
-      centerBullets: true,   // center bullet nav with js, turn this off if you want to position the bullet nav manually
-      singleCycle: false     // cycles through orbit slides only once
+      centerBullets: true,              // center bullet nav with js, turn this off if you want to position the bullet nav manually
+      singleCycle: false                // cycles through orbit slides only once
     },
 
     activeSlide: 0,
