aec980eb61897f867abc1d51c58cb79cd2f002a6
Matt Olson <matt@mattolson.com>
2012-11-07 22:21:57 -0800

    Refactor hiddenFix and use it for button dropdowns
    -- Button dropdowns rely on outerHeight to determine proper
       placement of list below button. When the button is hidden on
       the page, the calculation fails. Code to adjust for this
       already exists in foundation.forms.
    -- Refactor "hiddenFix" from a private function inside foundation.forms
       to an object in a new module, foundation.utils.
    -- Include foundation.utils in index.js so it's available to
       the rest of the codebase.
    -- Use hiddenFix to properly calculate size of button dropdowns
    -- Revert previous change of putting the caculation inside document
       ready.

--- vendor/assets/javascripts/foundation/jquery.foundation.buttons.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.buttons.js
@@ -57,23 +57,27 @@
       }
     });
 
-    // Positioning the Flyout List
-    $doc.ready(function() {
-      var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny)', this).outerHeight() - 1,
-          largeButtonHeight   = $('.button.large.dropdown', this).outerHeight() - 1,
-          smallButtonHeight   = $('.button.small.dropdown', this).outerHeight() - 1,
-          tinyButtonHeight    = $('.button.tiny.dropdown', this).outerHeight() - 1;
+    // Make sure we calculate size of hidden elements properly.
+    var outerHeightHelper = $.foundation.utils.hiddenFix();
+    outerHeightHelper.adjust($('.button.dropdown'));
+    
+    var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny)', this).outerHeight() - 1,
+        largeButtonHeight   = $('.button.large.dropdown', this).outerHeight() - 1,
+        smallButtonHeight   = $('.button.small.dropdown', this).outerHeight() - 1,
+        tinyButtonHeight    = $('.button.tiny.dropdown', this).outerHeight() - 1;
 
-      $('.button.dropdown:not(.large):not(.small):not(.tiny) > ul', this).css('top', normalButtonHeight);
-      $('.button.dropdown.large > ul', this).css('top', largeButtonHeight);
-      $('.button.dropdown.small > ul', this).css('top', smallButtonHeight);
-      $('.button.dropdown.tiny > ul', this).css('top', tinyButtonHeight);
+    outerHeightHelper.reset();
+    
+    // Position flyout lists appropriately
+    $('.button.dropdown:not(.large):not(.small):not(.tiny) > ul', this).css('top', normalButtonHeight);
+    $('.button.dropdown.large > ul', this).css('top', largeButtonHeight);
+    $('.button.dropdown.small > ul', this).css('top', smallButtonHeight);
+    $('.button.dropdown.tiny > ul', this).css('top', tinyButtonHeight);
 
-      $('.button.dropdown.up:not(.large):not(.small):not(.tiny) > ul', this).css('top', 'auto').css('bottom', normalButtonHeight - 2);
-      $('.button.dropdown.up.large > ul', this).css('top', 'auto').css('bottom', largeButtonHeight - 2);
-      $('.button.dropdown.up.small > ul', this).css('top', 'auto').css('bottom', smallButtonHeight - 2);
-      $('.button.dropdown.up.tiny > ul', this).css('top', 'auto').css('bottom', tinyButtonHeight - 2);
-    });
+    $('.button.dropdown.up:not(.large):not(.small):not(.tiny) > ul', this).css('top', 'auto').css('bottom', normalButtonHeight - 2);
+    $('.button.dropdown.up.large > ul', this).css('top', 'auto').css('bottom', largeButtonHeight - 2);
+    $('.button.dropdown.up.small > ul', this).css('top', 'auto').css('bottom', smallButtonHeight - 2);
+    $('.button.dropdown.up.tiny > ul', this).css('top', 'auto').css('bottom', tinyButtonHeight - 2);
   };
 
 })( jQuery, this );
