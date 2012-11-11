860c040f4db9af64712e8a67229e74dda447edc7
Mark Hayes <mark@zurb.com>
2012-11-10 18:39:09 -0800

    fixed another typo

--- vendor/assets/javascripts/foundation/jquery.foundation.tabs.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.tabs.js
@@ -50,7 +50,7 @@
     } else if (typeof method === 'object' || !method) {
       return methods.init.apply(this, arguments);
     } else {
-      $.error('Method ' +  method + ' does not exist on jQuery.foundationTooltips');
+      $.error('Method ' +  method + ' does not exist on jQuery.foundationTabs');
     }
   };
 }(jQuery, this, this.document));
