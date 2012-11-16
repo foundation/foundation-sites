f4a6617a1c6d3b900edddf153d176192f5a4ce3e
Remo Siegwart <remo.siegwart@gmail.com>
2012-11-16 20:46:10 +0100

    fixed orbit single cycle feature

--- vendor/assets/javascripts/foundation/jquery.foundation.orbit.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.orbit.js
@@ -642,7 +642,7 @@
         this.setCaption();
       }
 
-      if (this.$slides.last() && this.options.singleCycle) {
+      if (this.currentSlide().is(":last-child") && this.options.singleCycle) {
         this.stopClock();
       }
     }
