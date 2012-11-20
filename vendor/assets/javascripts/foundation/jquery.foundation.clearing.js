25b73c47a07d06ad1e84e8968f313b74a8a7b7e5
Jordan Humphreys <jordan@mailyard.net>
2012-11-20 10:03:53 -0800

    Make background click optional, remove deprecated variables.

--- vendor/assets/javascripts/foundation/jquery.foundation.clearing.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.clearing.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Foundation Clearing 1.0
+ * jQuery Foundation Clearing 1.1
  * http://foundation.zurb.com
  * Copyright 2012, ZURB
  * Free to use under the MIT license.
@@ -18,13 +18,22 @@
             '<p class="clearing-caption"></p><a href="#" class="clearing-main-left"></a>' +
             '<a href="#" class="clearing-main-right"></a></div>'
         },
+
+        // comma delimited list of selectors that, on click, will close clearing, 
+        // add 'div.clearing-blackout, div.visible-img' to close on background click
+        close_selectors : 'a.clearing-close',
+
+        // event initializers and locks
         initialized : false,
+        swipe_events : false,
         locked : false
       },
 
+      // original clearing methods will be stored here if
+      // initialized with custom methods
       superMethods = {},
 
-      methods = {
+      cl = {
         init : function (options, extendMethods) {
           return this.find('ul[data-clearing]').each(function () {
             var doc = $(document),
@@ -39,123 +48,134 @@
               $el.data('fndtn.clearing.settings', $.extend({}, defaults, options));
 
               // developer goodness experiment
-              methods.extend(methods, extendMethods);
+              cl.extend(cl, extendMethods);
 
               // if the gallery hasn't been built yet...build it
-              methods.assemble($el.find('li'));
+              cl.assemble($el.find('li'));
 
-              if (!defaults.initialized) methods.events();
+              if (!defaults.initialized) cl.events($el);
+              cl.swipe_events($el);
+
 
             }
           });
         },
 
-        events : function () {
-          var doc = $(document);
+        events : function (el) {
+          var settings = el.data('fndtn.clearing.settings');
 
-          doc.on('click.fndtn.clearing', 'ul[data-clearing] li', function (e, current, target) {
-            var current = current || $(this),
-                target = target || current,
-                settings = current.parent().data('fndtn.clearing.settings');
+          $(document)
+            .on('click.fndtn.clearing', 'ul[data-clearing] li', function (e, current, target) {
+              var current = current || $(this),
+                  target = target || current,
+                  settings = current.parent().data('fndtn.clearing.settings');
 
-            e.preventDefault();
+              e.preventDefault();
 
-            if (!settings) {
-              current.parent().foundationClearing();
-            }
+              if (!settings) {
+                current.parent().foundationClearing();
+              }
 
-            // set current and target to the clicked li if not otherwise defined.
-            methods.open($(e.target), current, target);
-            methods.update_paddles(target);
-          });
+              // set current and target to the clicked li if not otherwise defined.
+              cl.open($(e.target), current, target);
+              cl.update_paddles(target);
+            })
+
+            .on('click.fndtn.clearing', '.clearing-main-right', function (e) {
+              var clearing = $('.clearing-blackout').find('ul[data-clearing]');
+
+              e.preventDefault();
+              cl.go(clearing, 'next');
+            })
+
+            .on('click.fndtn.clearing', '.clearing-main-left', function (e) {
+              var clearing = $('.clearing-blackout').find('ul[data-clearing]');
+
+              e.preventDefault();
+              cl.go(clearing, 'prev');
+            })
+
+            .on('click.fndtn.clearing', '.clearing-main-left', function (e) {
+              var clearing = $('.clearing-blackout').find('ul[data-clearing]');
+
+              e.preventDefault();
+              cl.go(clearing, 'prev');
+            })
+
+            .on('click.fndtn.clearing', settings.close_selectors, function (e) {
+              e.preventDefault();
+
+              var root = (function (target) {
+                    if (/blackout/.test(target.selector)) {
+                      return target;
+                    } else {
+                      return target.closest('.clearing-blackout');
+                    }
+                  }($(this))), container, visible_image;
+
+              if (this === e.target && root) {
+                container = root.find('div:first'),
+                visible_image = container.find('.visible-img');
+
+                defaults.prev_index = 0;
+
+                root.find('ul[data-clearing]').attr('style', '')
+                root.removeClass('clearing-blackout');
+                container.removeClass('clearing-container');
+                visible_image.hide();
+              }
+
+              return false;
+            })
+
+            .on('keydown.fndtn.clearing', function (e) {
+              var clearing = $('.clearing-blackout').find('ul[data-clearing]');
+
+              // right
+              if (e.which === 39) {
+                cl.go(clearing, 'next');
+              }
+
+              // left
+              if (e.which === 37) {
+                cl.go(clearing, 'prev');
+              }
+
+              if (e.which === 27) {
+                $('a.clearing-close').trigger('click');
+              }
+            });
 
           $(window).on('resize.fndtn.clearing', function () {
             var image = $('.clearing-blackout .visible-img').find('img');
 
             if (image.length > 0) {
-              methods.center(image);
+              cl.center(image);
             }
-          });
-
-          doc.on('click.fndtn.clearing', '.clearing-main-right', function (e) {
-            var clearing = $('.clearing-blackout').find('ul[data-clearing]');
-
-            e.preventDefault();
-            methods.go(clearing, 'next');
-          });
-
-          doc.on('click.fndtn.clearing', '.clearing-main-left', function (e) {
-            var clearing = $('.clearing-blackout').find('ul[data-clearing]');
-
-            e.preventDefault();
-            methods.go(clearing, 'prev');
-          });
-
-          doc.on('click.fndtn.clearing', 'a.clearing-close, div.clearing-blackout, div.visible-img', function (e) {
-            var root = (function (target) {
-              if (/blackout/.test(target.selector)) {
-                return target;
-              } else {
-                return target.closest('.clearing-blackout');
-              }
-            }($(this))), container, visible_image;
-
-            if (this === e.target && root) {
-              container = root.find('div:first'),
-              visible_image = container.find('.visible-img');
-
-              defaults.prev_index = 0;
-
-              root.find('ul[data-clearing]').attr('style', '')
-              root.removeClass('clearing-blackout');
-              container.removeClass('clearing-container');
-              visible_image.hide();
-            }
-
-            return false;
-          });
-
-          // should specify a target selector
-          doc.on('keydown.fndtn.clearing', function (e) {
-            var clearing = $('.clearing-blackout').find('ul[data-clearing]');
-
-            // right
-            if (e.which === 39) {
-              methods.go(clearing, 'next');
-            }
-
-            // left
-            if (e.which === 37) {
-              methods.go(clearing, 'prev');
-            }
-
-            if (e.which === 27) {
-              $('a.clearing-close').trigger('click');
-            }
-          });
-
-          doc.on('movestart', 'ul[data-clearing] li', function (e) {
-
-            // If the movestart is heading off in an upwards or downwards
-            // direction, prevent it so that the browser scrolls normally.
-
-            if ((e.distX > e.distY && e.distX < -e.distY) ||
-                (e.distX < e.distY && e.distX > -e.distY)) {
-              e.preventDefault();
-            }
-          });
-
-          doc.bind('swipeleft', 'ul[data-clearing] li', function () {
-            var clearing = $('.clearing-blackout').find('ul[data-clearing]');
-            methods.go(clearing, 'next');
-          });
-
-          doc.bind('swiperight', 'ul[data-clearing] li', function () {
-            var clearing = $('.clearing-blackout').find('ul[data-clearing]');
-            methods.go(clearing, 'prev');
           });
 
           defaults.initialized = true;
+        },
+
+        swipe_events : function (el) {
+          el.find('.visible-img')
+            .on('swipeleft', function (e) {
+              var clearing = $('.clearing-blackout').find('ul[data-clearing]');
+              cl.go(clearing, 'next');
+            })
+
+            .on('swiperight', function (e) {
+              var clearing = $('.clearing-blackout').find('ul[data-clearing]');
+              cl.go(clearing, 'prev');
+            })
+
+            .on('movestart', function (e) {
+              if ((e.distX > e.distY && e.distX < -e.distY) ||
+                  (e.distX < e.distY && e.distX > -e.distY)) {
+                e.preventDefault();
+                return;
+              }
+            });
         },
 
         assemble : function ($li) {
@@ -177,7 +197,7 @@
               visible_image = container.find('.visible-img'),
               image = visible_image.find('img').not($image);
 
-          if (!methods.locked()) {
+          if (!cl.locked()) {
 
             // set the image to the selected thumbnail
             image.attr('src', this.load($image));
@@ -186,33 +206,34 @@
               // toggle the gallery if not visible
               root.addClass('clearing-blackout');
               container.addClass('clearing-container');
-              methods.caption(visible_image.find('.clearing-caption'), $image);
+              this.caption(visible_image.find('.clearing-caption'), $image);
               visible_image.show();
-              methods.fix_height(target);
+              this.fix_height(target);
 
-              methods.center(image);
+              this.center(image);
 
               // shift the thumbnails if necessary
-              methods.shift(current, target, function () {
+              this.shift(current, target, function () {
                 target.siblings().removeClass('visible');
                 target.addClass('visible');
               });
-            });
+            }.bind(this));
           }
         },
 
         fix_height : function (target) {
           var lis = target.siblings();
 
-          lis.each(function () {
-            var li = $(this),
-                image = li.find('img');
+          target.siblings()
+            .each(function () {
+              var li = $(this),
+                  image = li.find('img');
 
-            if (li.height() > image.outerHeight()) {
-              li.addClass('fix-height');
-            }
-          });
-          lis.closest('ul').width(lis.length * 100 + '%');
+              if (li.height() > image.outerHeight()) {
+                li.addClass('fix-height');
+              }
+            })
+            .closest('ul').width(lis.length * 100 + '%');
         },
 
         update_paddles : function (target) {
@@ -234,7 +255,6 @@
         load : function ($image) {
           var href = $image.parent().attr('href');
 
-          // preload next and previous
           this.preload($image);
 
           if (href) {
@@ -292,9 +312,6 @@
 
         shift : function (current, target, callback) {
           var clearing = target.parent(),
-              container = clearing.closest('.clearing-container'),
-              target_offset = target.position().left,
-              thumbs_offset = clearing.position().left,
               old_index = defaults.prev_index,
               direction = this.direction(clearing, current, target),
               left = parseInt(clearing.css('left'), 10),
@@ -306,23 +323,23 @@
 
           if (target.index() !== old_index && !/skip/.test(direction)){
             if (/left/.test(direction)) {
-              methods.lock();
-              clearing.animate({left : left + width}, 300, methods.unlock);
+              this.lock();
+              clearing.animate({left : left + width}, 300, this.unlock);
             } else if (/right/.test(direction)) {
-              methods.lock();
-              clearing.animate({left : left - width}, 300, methods.unlock);
+              this.lock();
+              clearing.animate({left : left - width}, 300, this.unlock);
             }
           } else if (/skip/.test(direction)) {
 
             // the target image is not adjacent to the current image, so
             // do we scroll right or not
             skip_shift = target.index() - defaults.up_count;
-            methods.lock();
+            this.lock();
 
             if (skip_shift > 0) {
-              clearing.animate({left : -(skip_shift * width)}, 300, methods.unlock);
+              clearing.animate({left : -(skip_shift * width)}, 300, this.unlock);
             } else {
-              clearing.animate({left : 0}, 300, methods.unlock);
+              clearing.animate({left : 0}, 300, this.unlock);
             }
           }
 
@@ -344,11 +361,8 @@
         direction : function ($el, current, target) {
           var lis = $el.find('li'),
               li_width = lis.outerWidth() + (lis.outerWidth() / 4),
-              container = $('.clearing-container'),
-              up_count = Math.floor(container.outerWidth() / li_width) - 1,
-              shift_count = lis.length - up_count,
+              up_count = Math.floor($('.clearing-container').outerWidth() / li_width) - 1,
               target_index = lis.index(target),
-              current_index = lis.index(current),
               response;
 
           defaults.up_count = up_count;
@@ -412,7 +426,7 @@
             }
           });
 
-          $.extend(methods, extendMethods);
+          $.extend(cl, extendMethods);
         },
 
         // you can call this._super('methodName', [args]) to call
@@ -424,10 +438,10 @@
       };
 
   $.fn.foundationClearing = function (method) {
-    if (methods[method]) {
-      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    if (cl[method]) {
+      return cl[method].apply(this, Array.prototype.slice.call(arguments, 1));
     } else if (typeof method === 'object' || !method) {
-      return methods.init.apply(this, arguments);
+      return cl.init.apply(this, arguments);
     } else {
       $.error('Method ' +  method + ' does not exist on jQuery.foundationClearing');
     }
