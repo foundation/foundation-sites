e41af550babe7dfd93d83f3b3015d59531181cc2
Mark McConachie <mark@markmcconachie.com>
2013-01-23 02:00:28 +0100

    switched die for off

--- vendor/assets/javascripts/foundation/jquery.foundation.topbar.js
+++ vendor/assets/javascripts/foundation/jquery.foundation.topbar.js
@@ -42,7 +42,7 @@
             $('body').css('padding-top',settings.$topbar.outerHeight())
           }
 
-          $('.top-bar .toggle-topbar').die('click.fndtn').live('click.fndtn', function (e) {
+          $('.top-bar .toggle-topbar').off('click.fndtn').live('click.fndtn', function (e) {
             e.preventDefault();
 
             if (methods.breakpoint()) {
@@ -59,7 +59,7 @@
           });
 
           // Show the Dropdown Levels on Click
-          $('.top-bar .has-dropdown>a').die('click.fndtn').live('click.fndtn', function (e) {
+          $('.top-bar .has-dropdown>a').off('click.fndtn').live('click.fndtn', function (e) {
             if (Modernizr.touch || methods.breakpoint())
               e.preventDefault();
 
@@ -84,7 +84,7 @@
           });
 
           // Go up a level on Click
-          $('.top-bar .has-dropdown .back').die('click.fndtn').live('click.fndtn', function (e) {
+          $('.top-bar .has-dropdown .back').off('click.fndtn').live('click.fndtn', function (e) {
             e.preventDefault();
 
             var $this = $(this),
