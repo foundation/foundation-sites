936544ee7acae0cc5fe85df1a0e88029025282b7
Jordan Humphreys <jordan@mailyard.net>
2013-02-25 15:19:51 -0800

    Enable custom forms.

--- js/foundation/index.js
+++ js/foundation/index.js
@@ -2,6 +2,7 @@
 =require foundation/foundation
 =require foundation/foundation.orbit
 =require foundation/foundation.alerts
+=require foundation/foundation.forms
 =require foundation/foundation.clearing
 =require foundation/foundation.reveal
 */
