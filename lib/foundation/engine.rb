71df52c93980e70ac1b8d112622959e49ef3062e
Mark Hayes <mark@zurb.com>
2013-02-27 23:34:50 -0800

    update rails generators used for installation

--- lib/foundation/engine.rb
+++ lib/foundation/engine.rb
@@ -4,6 +4,9 @@ module Foundation
     initializer 'foundation.update_asset_paths' do |app|
       app.config.assets.paths << File.expand_path("../../../scss", __FILE__)
       app.config.assets.paths << File.expand_path("../../../js", __FILE__)
+
+      # Ensure Zepto and Modernizr are precompiled in production
+      app.config.assets.precompile += %w(vendor/zepto.js vendor/custom.modernizr.js)
     end
   end
 end
--- lib/zurb-foundation.rb
+++ lib/zurb-foundation.rb
@@ -2,7 +2,6 @@ root = File.join(File.dirname(__FILE__), "..")
 require "foundation/version"
 
 if defined?(Rails)
-  require "foundation/generators/layout_generator"
   require "foundation/generators/install_generator"
 end
 
