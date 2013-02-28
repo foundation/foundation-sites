e1e0c424ee1443771601d4abf20c03b9933682e3
Mark Hayes <mark@zurb.com>
2013-02-28 09:23:32 -0800

    setup compilation process for stasis

--- docs/controller.rb
+++ docs/controller.rb
@@ -4,10 +4,17 @@ layout 'layout.html.erb'
 ignore /css\//
 ignore /js\//
 ignore /.+.md/
+ignore /Gemfile.*/
+ignore /Procfile/
+ignore /compile.rb/
 
 helpers do
   def asset_path
-    "http://#{Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.getnameinfo[0]}:4001/assets"
+    if Socket.gethostname == "foundation"
+      "http://foundation.zurb.com/docs"
+    else
+      "http://#{Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.getnameinfo[0]}:4001/assets"
+    end
   end
 
   def code_example(code, lang=:ruby)
