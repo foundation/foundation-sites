a5c981099de3610359a31cbcf0b8712741ec6152
Jordan Humphreys <jordan@mailyard.net>
2013-03-01 10:42:04 -0800

    Fix syntax error in section.js

--- js/foundation/foundation.section.js
+++ js/foundation/foundation.section.js
@@ -17,7 +17,7 @@
     init : function (scope, method, options) {
       this.scope = scope || this.scope;
       Foundation.inherit(this, 'throttle data_options');
-
+      console.log(this)
       if (typeof method === 'object') {
         $.extend(true, this.settings, method);
       }
@@ -130,7 +130,7 @@
 
     set_active_from_hash : function () {
       var hash = window.location.hash.substring(1),
-          sections = $('[data-section]')
+          sections = $('[data-section]'),
           self = this;
 
       sections.each(function () {
