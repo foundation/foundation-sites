eaa571f376debe10899ace063114b46231aec2ad
zurbchris <chris@zurb.com>
2013-03-01 09:46:35 -0800

    updated controller.rb to not point to foundation4.zurb.com

--- docs/controller.rb
+++ docs/controller.rb
@@ -11,7 +11,7 @@ ignore /compile.rb/
 helpers do
   def asset_path
     if Socket.gethostname == "foundation"
-      "http://foundation4.zurb.com/docs/assets"
+      "http://foundation.zurb.com/docs/assets"
     else
       "http://#{Socket.ip_address_list.detect{|intf| intf.ipv4_private?}.getnameinfo[0]}:4001/assets"
     end
@@ -20,23 +20,23 @@ helpers do
   def code_example(code, lang=:ruby)
     "<div class='#{lang}'>" + CodeRay.scan(code, lang).div(:css => :class) + "</div>"
   end
-  
+
   def foundation_home_path
     '/'
   end
-  
+
   def features_path
     '/grid.php'
   end
-  
+
   def add_ons_path
     '/templates.php'
   end
-  
+
   def case_studies_path
     '/case-jacquelinewest.php'
   end
-  
+
   def docs_path
     '/docs/'
   end
