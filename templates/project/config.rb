7b6f97bf3c1b14a8d56596bc63a26b5802b4d4e1
Johan van der Geest <johan.van.der.geest@bwoffshore.com>
2013-03-04 15:46:04 +0100

    Change the _dir configuration properties to the default values of Compass. According to http://stackoverflow.com/questions/11922712/sass-scss-compass-how-to-set-default-project-file-structure, the default structure of Compass cannot be changed.

--- templates/project/config.rb
+++ templates/project/config.rb
@@ -4,10 +4,10 @@ require 'zurb-foundation'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "img"
-javascripts_dir = "js"
+css_dir = "stylesheets"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
