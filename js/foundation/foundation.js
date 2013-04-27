b1d05707ae7f76fd13a5a72b85758c27550ac15b
Jordan Humphreys <jordan@mailyard.net>
2013-04-26 18:04:53 -0700

    Add IE8 support for indexOf.

--- js/foundation/foundation.js
+++ js/foundation/foundation.js
@@ -83,6 +83,39 @@ if (typeof jQuery === "undefined" &&
     };
   }
 
+  if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
+      "use strict";
+      if (this == null) {
+        throw new TypeError();
+      }
+      var t = Object(this);
+      var len = t.length >>> 0;
+      if (len === 0) {
+        return -1;
+      }
+      var n = 0;
+      if (arguments.length > 1) {
+        n = Number(arguments[1]);
+        if (n != n) { // shortcut for verifying if it's NaN
+          n = 0;
+        } else if (n != 0 && n != Infinity && n != -Infinity) {
+          n = (n > 0 || -1) * Math.floor(Math.abs(n));
+        }
+      }
+      if (n >= len) {
+          return -1;
+      }
+      var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+      for (; k < len; k++) {
+        if (k in t && t[k] === searchElement) {
+          return k;
+        }
+      }
+      return -1;
+    }
+  }
+
   // fake stop() for zepto.
   $.fn.stop = $.fn.stop || function() {
     return this;
@@ -91,7 +124,7 @@ if (typeof jQuery === "undefined" &&
   window.Foundation = {
     name : 'Foundation',
 
-    version : '4.1.3',
+    version : '4.1.5',
 
     // global Foundation cache object
     cache : {},
