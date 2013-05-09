fda1693837970e1121e300ab8ccfcb69bc4276da
Jordan Humphreys <jordan@mailyard.net>
2013-05-08 19:24:01 -0700

    Disable mousedown on custom select.

--- js/foundation/foundation.forms.js
+++ js/foundation/foundation.forms.js
@@ -87,6 +87,9 @@
             }
           }
         })
+        .on('mousedown.fndtn.forms', 'form.custom div.custom.dropdown', function () {
+          return false;
+        })
         .on('click.fndtn.forms', 'form.custom div.custom.dropdown a.current, form.custom div.custom.dropdown a.selector', function (e) {
           var $this = $(this),
               $dropdown = $this.closest('div.custom.dropdown'),
