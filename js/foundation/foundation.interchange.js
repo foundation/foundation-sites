29e3d6035b0c142ea7da77d7ce0c8f273a5e5768
Jordan Humphreys <jordan@mailyard.net>
2013-05-10 20:36:37 -0700

    Complete first pass of interchange.

--- js/foundation/foundation.interchange.js
+++ js/foundation/foundation.interchange.js
@@ -1,5 +1,7 @@
 /*jslint unparam: true, browser: true, indent: 2 */
 
+// requires Modernizr
+
 ;(function ($, window, document, undefined) {
   'use strict';
 
@@ -11,15 +13,39 @@
     cache : {},
 
     settings : {
-      load_attr : 'foundation-load'
+      load_attr : 'foundation-load',
+      named_rules : {
+        small : 'max-width: 767px',
+        medium : '',
+        large : ''
+      },
+      directives : {
+        replace : function (el, path, revert) {
+          if (/IMG/.test(el[0].nodeName)) {
+            var path_parts = path.split('/'),
+                path_file = path_parts[path_parts.length - 1];
+
+            if (new RegExp(path_file, 'i').test(el[0].src)) return;
+
+            el[0].src = path;
+
+            return el.trigger('replace', el);
+          }
+        },
+        before : function (el, path, revert) {},
+        after : function (el, path, revert) {}
+      }
     },
 
     init : function (scope, method, options) {
+      Foundation.inherit(this, 'throttle');
+
       if (typeof method === 'object') {
         $.extend(true, this.settings, method);
       }
 
-      // this.set_preferred();
+      // load images;
+      this.images();
 
       if (typeof method != 'string') {
         return this.settings.init;
@@ -28,9 +54,56 @@
       }
     },
 
-    // retina : function () {
-    //   return window.devicePixelRatio > 1;
-    // },
+    events : function () {
+      var self = this;
+
+      $(window).on('resize.fndtn.interchange', self.throttle(function () {
+        self.resize.call(self);
+      }, 50)).trigger('resize');
+    },
+
+    resize : function () {
+      var cache = this.cache;
+
+      for (var uuid in cache) {
+        if (cache.hasOwnProperty(uuid)) {
+          var passed = this.results(uuid, cache[uuid]);
+          
+          if (passed) {
+            return this.settings.directives[passed
+              .scenario[1]](passed.el, passed.scenario[0]);
+          }
+
+          // need to be able to revert the change 
+          // if the window is scaled back to original
+
+          // return this.settings.directives[passed
+          //     .scenarios[1](passed.el, pass.scenarios[0], true)];
+        }
+      }
+    },
+
+    results : function (uuid, scenarios) {
+      var count = scenarios.length,
+          el = $('[data-uuid="' + uuid + '"]'),
+          results_arr = [];
+
+      if (scenarios.length > 0) {
+        for (var i = count - 1; i >= 0; i--) {
+          var rule = scenarios[i][2];
+          if (this.settings.named_rules.hasOwnProperty(rule)) {
+            var mq = Modernizr.mq('(' + this.settings.named_rules[rule] + ')')
+          } else {
+            var mq = Modernizr.mq('(' + scenarios[i][2] + ')');
+          }
+          if (mq) {
+            return {el: el, scenario: scenarios[i]};
+          }
+        }
+      }
+
+      return results_arr;
+    },
 
     loadables : function (reset) {
       if (reset) {
@@ -70,17 +143,25 @@
 
     update_images : function () {
       var images = document.getElementsByTagName('img'),
-          count = images.length;
+          count = images.length,
+          data_attr = 'data-' + this.settings.load_attr;
 
       this.cached_images = [];
 
       for (var i = count - 1; i >= 0; i--) {
         this.valid(images[i], (i === 0), function (image, last) {
-          if (image && image.hasAttribute('data-' + this.settings.load_attr)) {
-            this.cached_images.push(image);
+          if (image) {
+            var str = image.getAttribute(data_attr) || '';
+
+            if (str.length > 0) {
+              this.cached_images.push(image);
+            }
           }
 
           if (last) {
+            var loaded = setTimeout(function () {
+
+            }.bind(this), 10);
             // all images have been updated, we
             // can now begin parsing queries
             this.enhance();
@@ -94,39 +175,110 @@
     valid : function (image, last, callback) {
       var img = new Image();
 
-      img.onerror = function () {
-        callback(false, last);
-      };
-
-      img.onload = function () {
-        callback(image, last);
-      };
-
       img.src = image.src;
+
+      this.loaded($(image), last, callback);
 
       return img;
     },
 
-    // temporary callback
-    enhance : function () {
-      console.log(this.images())
+    loaded : function (image, last, callback) {
+      // based on jquery.imageready.js
+      // @weblinc, @jsantell, (c) 2012
+
+      function loaded () {
+        callback(image[0], last);
+      }
+
+      function bindLoad () {
+        this.one('load', loaded);
+
+        if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {
+          var src = this.attr( 'src' ),
+              param = src.match( /\?/ ) ? '&' : '?';
+
+          param += 'random=' + (new Date()).getTime();
+          this.attr('src', src + param);
+        }
+      }
+
+      if (!image.attr('src')) {
+        loaded();
+        return;
+      }
+
+      if (image[0].complete || image[0].readyState === 4) {
+        loaded();
+      } else {
+        bindLoad.call(image);
+      }
     },
 
-    build_config_object : function(el) {
+    // temporary callback
+    enhance : function () {
+      this._object($(this.images()[0]));
+      return this.events();
+    },
+
+    parse_params : function (path, directive, mq) {
+      return [this.convert_path(path), this.convert_directive(directive), this.convert_mq(mq)];
+    },
+
+    convert_mq : function (mq) {
+      return $.trim(mq);
+    },
+
+    convert_path : function (path) {
+      return $.trim(path);
+    },
+
+    convert_directive : function (directive) {
+      var trimmed = $.trim(directive);
+      if (trimmed.length > 0) {
+        return trimmed;
+      }
+
+      return 'replace';
+    },
+
+    _object : function(el) {
       var raw_settings_arr = this.parse_data_attr(el),
           scenarios = [], count = raw_settings_arr.length;
 
       if (count > 0) {
         for (var i = count - 1; i >= 0; i--) {
-          var split = raw_settings_arr[i].split(',');
+          var split = raw_settings_arr[i].split(/\((.*?)\)/);
 
           if (split.length > 1) {
-            if (/(/.split[1])
-            scenarios.push({path: split[0], action: split})
+            var mq = split[1],
+                path = split[0].split(',')[0],
+                directive = split[0].split(',')[1],
+                params = this.parse_params(path, directive, mq);
+
+            scenarios.push(params)
           }
         }
       }
 
+      return this.store(el, scenarios);
+    },
+
+    uuid : function (separator) {
+      var delim = separator || "-";
+
+      function S4() {
+          return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+      }
+
+      return (S4() + S4() + delim + S4() + delim + S4() + delim + S4() + delim + S4() + S4() + S4());
+    },
+
+    store : function (el, scenarios) {
+      var uuid = this.uuid();
+
+      el.attr('data-uuid', uuid);
+
+      return this.cache[uuid] = scenarios;
     },
 
     trim : function(str) {
@@ -153,4 +305,5 @@
     }
     
   };
+
 }(Foundation.zj, this, this.document));
