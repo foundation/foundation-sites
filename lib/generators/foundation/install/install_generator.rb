51970b4493f9407764cd1f0cb9fc951b8afced59
L8D <tenorbiel@gmail.com>
2013-07-12 00:25:46 -0500

    Generator file locations updated for Rails 4.0

--- lib/generators/foundation/install/install_generator.rb
+++ lib/generators/foundation/install/install_generator.rb
@@ -14,7 +14,7 @@ module Foundation
         # gsub_file "app/assets/javascripts/application#{detect_js_format[0]}", /\/\/= require jquery\n/, ""
         insert_into_file "app/assets/javascripts/application#{detect_js_format[0]}", "#{detect_js_format[1]} require foundation\n", :after => "jquery_ujs\n"
         append_to_file "app/assets/javascripts/application#{detect_js_format[0]}", "\n$(function(){ $(document).foundation(); });\n"
-        settings_file = File.join(File.dirname(__FILE__),"..","..","..","scss","foundation","_variables.scss")
+        settings_file = File.join(File.dirname(__FILE__),"..","..","..","..","scss","foundation","_variables.scss")
         create_file "app/assets/stylesheets/foundation_and_overrides.scss", File.read(settings_file)
         append_to_file "app/assets/stylesheets/foundation_and_overrides.scss", "\n@import 'foundation';\n"
         insert_into_file "app/assets/stylesheets/application#{detect_css_format[0]}", "#{detect_css_format[1]} require foundation_and_overrides\n", :after => "require_self\n"
