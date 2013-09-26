428d00760d3f9df8ebe6483df76c1b3f07d5a754
Chris Chua <chris.sirhc@gmail.com>
2013-09-26 01:59:14 -0700

    fix(section): deep_linking option

--- js/foundation/foundation.section.js
+++ js/foundation/foundation.section.js
@@ -350,7 +350,7 @@
       
       if (selectedSection != null) {
         sections.each(function() {
-          if (selectedSection == $(this)) {
+          if (selectedSection[0] == this) {
             var section = $(this),
                 settings = $.extend({}, self.settings, self.data_options(section)),
                 regions = section.children(self.settings.region_selector),
