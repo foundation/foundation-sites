6ab34c8eb0d703707b0ed28d52da6b8528de8273
James Dullaghan <james@fldlvl.com>
2013-10-21 10:46:18 -0700

    Add content to index docs

---
+++ doc/helpers/default.js
@@ -0,0 +1,14 @@
+module.exports.navLink = function () {
+
+  context = {
+    navLink: [
+      "Features",
+      "Add-ons",
+      "Case Studies",
+      "Docs",
+      "Training"
+    ]
+  };
+
+  return context;
+};
