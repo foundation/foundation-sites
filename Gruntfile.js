9164cdd79c29d5c616fa17cd0e86b404c257e986
Mark Hayes <mark@zurb.com>
2013-10-29 09:37:54 -0700

    doc specific js should go into docs.js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt) {
       docs: {
         files: {
           'dist/docs/assets/js/custom.modernizr.js': ['js/vendor/custom.modernizr.js'],
-          'dist/docs/assets/js/all.js': ['js/vendor/jquery.js', 'js/vendor/fastclick.js', 'js/foundation/foundation.js', 'js/foundation/foundation.*.js']
+          'dist/docs/assets/js/all.js': ['js/vendor/jquery.js', 'js/vendor/fastclick.js', 'js/foundation/foundation.js', 'js/foundation/foundation.*.js', 'doc/assets/js/docs.js']
         }
       }
     },
