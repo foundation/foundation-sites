8d4ee430548b04de37fbb303861fdaef658ee455
James Dullaghan <james@fldlvl.com>
2013-11-11 10:44:38 -0800

    Add more autocomplete values

--- doc/assets/js/docs.js
+++ doc/assets/js/docs.js
@@ -1,44 +1,99 @@
 var components = [
   { value: 'CSS', data: 'css.html' },
+  { value: 'Styles', data: 'css.html' },
+
   { value: 'Sass', data: 'sass.html' },
+  { value: 'SCSS', data: 'sass.html' },
+
   { value: 'Rails', data: '#rails' },
+  { value: 'Gem', data: '#rails' },
+
   { value: 'Global Styles', data: 'global.html' },
+  { value: 'Global Mixins', data: 'global.html' },
+  { value: 'Global Variables', data: 'global.html' },
+  { value: 'Useful HTML Classes', data: 'global.html' },
+
   { value: 'Upgrading', data: '#upgrading' },
+  { value: 'Migration', data: '#upgrading' },
+
   { value: 'Media Queries', data: '#mediaQueries' },
   { value: 'Grid', data: 'grid.html' },
   { value: 'Block Grid', data: 'block_grid.html' },
   { value: 'Thumbnails', data: 'thumbnails.html' },
+  { value: 'Images', data: 'thumbnails.html'},
   { value: 'Utility Classes', data: '#utility classes' },
   { value: 'Right-to-Left Support', data: '#right-to-left support' },
   { value: 'Offcanvas', data: 'offcanvas.html' },
+
   { value: 'Top Bar', data: 'topbar.html' },
+  { value: 'Nav Bar', data: 'topbar.html' },
+  { value: 'Navigation', data: 'topbar.html' },
+  { value: 'Sticky', data: 'topbar.html' },
+
   { value: 'Side Nav', data: 'sidenav.html' },
+
   { value: 'magellan', data: 'magellan.html' },
+  { value: 'scrollspy', data: 'magellan.html' },
+
   { value: 'Sub Nav', data: 'subnav.html' },
+
   { value: 'Breadcrumbs', data: 'breadcrumbs.html' },
-  { value: 'Pagination', data: 'pagination.html' },
+  { value: 'Navigation Path', data: 'breadcrumbs.html' },
+  { value: 'Cookie crumb', data: 'breadcrumbs.html' },
+
   { value: 'Orbit Slider', data: 'orbit.html' },
+  { value: 'Carousel', data: 'orbit.html' },
+  { value: 'Slider', data: 'orbit.html' },
+  { value: 'Slideshow', data: 'orbit.html' },
+
   { value: 'Clearing Lightbox', data: 'clearing.html' },
+  { value: 'lightbox', data: 'clearing.html' },
+
   { value: 'Interchange Responsive Images', data: 'interchange.html' },
+
   { value: 'Flex Video', data: 'flex_video.html' },
+  { value: 'Responsive Video', data: 'flex_video.html' },
+
   { value: 'Forms', data: 'forms.html' },
+
   { value: 'Abide Validation', data: 'abide.html' },
+
   { value: 'Buttons', data: 'buttons.html' },
   { value: 'Button Groups', data: 'button_groups.html' },
   { value: 'Split Buttons', data: 'split_buttons.html' },
   { value: 'Dropdown Buttons', data: 'dropdown_buttons.html' },
+
   { value: 'Type', data: 'typography.html' },
+  { value: 'Typography', data: 'typography.html' },
+  { value: 'Type-setting', data: 'typography.html' },
+  { value: 'Composition', data: 'typography.html' },
+
   { value: 'Lists', data: 'inline_lists.html' },
   { value: 'Labels', data: 'labels.html' },
   { value: 'Keystrokes', data: 'keystrokes.html' },
+
   { value: 'Reveal Modal', data: 'reveal.html' },
+
   { value: 'Alerts', data: 'alert_boxes.html' },
+  { value: 'Error', data: 'alert_boxes.html' },
+  { value: 'Success', data: 'alert_boxes.html' },
+  { value: 'Warning', data: 'alert_boxes.html' },
+
   { value: 'Panels', data: 'panels.html' },
+  { value: 'Wells', data: 'panels.html' },
+
   { value: 'Tooltips', data: 'tooltips.html' },
+
   { value: 'Joyride', data: 'joyride.html' },
+  { value: 'Tooltip Tour', data: 'joyride.html' },
+  { value: 'Guider', data: 'joyride.html' },
+  { value: 'Tooltip Walk-through', data: 'joyride.html' },
+
   { value: 'Dropdowns', data: 'dropdown.html' },
   { value: 'Pricing Tables', data: 'pricing_tables.html' },
+
   { value: 'Progress Bars', data: 'progress_bars.html' },
+
   { value: 'Accordian', data: '#accordian' },
   { value: 'Tabs', data: '#tabs' },
   { value: 'Changelog', data: '#changelog' },
