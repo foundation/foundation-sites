760bd16309401910564091454f014938f5c97545
Jordan Humphreys <jordan@mailyard.net>
2013-11-12 10:31:31 -0800

    Remove console.log statements>

--- js/foundation/foundation.offcanvas.js
+++ js/foundation/foundation.offcanvas.js
@@ -15,19 +15,15 @@
     events : function () {
       $(this.scope).off('.offcanvas')
         .on('click.fndtn.offcanvas', '.off-canvas-left-toggle', function (e) {
-          console.log($(this).closest('.off-canvas-wrap'))
           $(this).closest('.off-canvas-wrap').toggleClass('move-right');
         })
         .on('click.fndtn.offcanvas', '.exit-off-canvas', function () {
-          console.log('exit left')
           $(this).closest(".off-canvas-wrap").removeClass("move-right");
         })
         .on('click.fndtn.offcanvas', '.off-canvas-right-toggle', function (e) {
-          console.log('toggle right')
           $(this).closest(".off-canvas-wrap").toggleClass("move-left");
         })
         .on('click.fndtn.offcanvas', '.exit-off-canvas', function () {
-          console.log('exit right')
           $(this).closest(".off-canvas-wrap").removeClass("move-left");
         });
     },
