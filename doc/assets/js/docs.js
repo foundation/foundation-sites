197ac9ed9b20f17781f2f789b2275e9d64c6f7ca
James Dullaghan <james@fldlvl.com>
2013-11-21 13:58:27 -0800

    Add download link to autocomplete

--- doc/assets/js/docs.js
+++ doc/assets/js/docs.js
@@ -127,7 +127,7 @@ var components = [
   { value: 'Foundation 3', data: 'http://foundation3.zurb.com/old-docs/f3/' },
   { value: 'Foundation 2', data: '/docs/v/2.2.1' },
   // { value: 'What Comes With Foundation', data: '/docs/components/#what comes with foundation' },
-  { value: 'Download', data: '/docs/components/#download' }
+  { value: 'Download', data: '/develop/download.html' }
 ];
 
 $('#autocomplete').autocomplete({
