b0239f2c1a0506c4a471154bb3e5600c4ce11570
Muja <cydrop@gmail.com>
2013-11-22 15:19:20 +0100

    Sticky topbar reference fix
    in line 38.

--- js/foundation/foundation.topbar.js
+++ js/foundation/foundation.topbar.js
@@ -35,7 +35,7 @@
         var topbarContainer = topbar.parent();
         if(topbarContainer.hasClass('fixed') || topbarContainer.hasClass(settings.sticky_class)) {
           self.settings.sticky_class = settings.sticky_class;
-          self.settings.stick_topbar = topbar;
+          self.settings.sticky_topbar = topbar;
           topbar.data('height', topbarContainer.outerHeight());
           topbar.data('stickyoffset', topbarContainer.offset().top);
         } else {
