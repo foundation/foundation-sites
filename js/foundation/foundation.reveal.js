9190fb9b589c146034f75b482509fa4c6f7b2ef3
Jordan Humphreys <jordan@mailyard.net>
2013-11-25 10:05:43 -0800

    Remove log statement.

--- js/foundation/foundation.reveal.js
+++ js/foundation/foundation.reveal.js
@@ -275,7 +275,6 @@
     hide : function (el, css) {
       // is modal
       if (css) {
-        console.log(el)
         var settings = el.data('reveal-init');
         if (/pop/i.test(settings.animation)) {
           var end_css = {
