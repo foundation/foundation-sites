226e5870e81996d64ab9322492799fefb42c325a
Mark Hayes <mark@zurb.com>
2013-12-17 08:46:47 -0800

    build search from sidebar urls so autocomplete works in development

--- doc/assets/js/docs.js
+++ doc/assets/js/docs.js
@@ -1,145 +1,29 @@
-var components = [
-  { value: 'Getting Started', data: '/docs/index.html' },
-
-  { value: 'CSS', data: '/docs/css.html' },
-  { value: 'Styles', data: '/docs/css.html' },
-
-  { value: 'Sass', data: '/docs/sass.html' },
-  { value: 'SCSS', data: '/docs/sass.html' },
-
-  { value: 'Applications', data: '/docs/applications.html' },
-  { value: 'Rails', data: '/docs/applications.html' },
-  { value: 'Gem', data: '/docs/applications.html' },
-  { value: 'JavaScript', data: '/docs/javascript.html' },
-
-  { value: 'Global Styles', data: '/docs/components/global.html' },
-  { value: 'Global Mixins', data: '/docs/components/global.html' },
-  { value: 'Global Variables', data: '/docs/components/global.html' },
-  { value: 'Useful HTML Classes', data: '/docs/components/global.html' },
-
-  { value: 'Upgrading', data: '/docs/upgrading.html' },
-  { value: 'Migration', data: '/docs/upgrading.html' },
-
-  { value: 'Media Queries', data: '/docs/media-queries.html' },
-  { value: 'Breakpoints', data: '/docs/media-queries.html' },
-
-  { value: 'Grid', data: '/docs/components/grid.html' },
-  { value: 'Block Grid', data: '/docs/components/block_grid.html' },
-  { value: 'Thumbnails', data: '/docs/components/thumbnails.html' },
-  { value: 'Images', data: '/docs/components/thumbnails.html'},
-  { value: 'Utility Classes', data: '/docs/utility-classes.html' },
-  { value: 'Right-to-Left Support', data: '/docs/components/rtl.html' },
-  { value: 'Offcanvas', data: '/docs/components/offcanvas.html' },
-
-  { value: 'Top Bar', data: '/docs/components/topbar.html' },
-  { value: 'Nav Bar', data: '/docs/components/topbar.html' },
-  { value: 'Navigation', data: '/docs/components/topbar.html' },
-  { value: 'Sticky', data: '/docs/components/topbar.html' },
-
-  { value: 'Side Nav', data: '/docs/components/sidenav.html' },
-
-  { value: 'Magellan', data: '/docs/components/magellan.html' },
-  { value: 'scrollspy', data: '/docs/components/magellan.html' },
-
-  { value: 'Sub Nav', data: '/docs/components/subnav.html' },
-
-  { value: 'Breadcrumbs', data: '/docs/components/breadcrumbs.html' },
-  { value: 'Navigation Path', data: '/docs/components/breadcrumbs.html' },
-  { value: 'Cookie crumb', data: '/docs/components/breadcrumbs.html' },
-
-  { value: 'Pagination', data: '/docs/components/pagination.html' },
-
-  { value: 'Orbit Slider', data: '/docs/components/orbit.html' },
-  { value: 'Carousel', data: '/docs/components/orbit.html' },
-  { value: 'Slider', data: '/docs/components/orbit.html' },
-  { value: 'Slideshow', data: '/docs/components/orbit.html' },
-
-  { value: 'Clearing Lightbox', data: '/docs/components/clearing.html' },
-  { value: 'Responsive Lightbox', data: '/docs/components/clearing.html' },
-  { value: 'Lightbox', data: '/docs/components/clearing.html' },
-
-  { value: 'Interchange Responsive Content', data: '/docs/components/interchange.html' },
-
-  { value: 'Flex Video', data: '/docs/components/flex_video.html' },
-  { value: 'Responsive Video', data: '/docs/components/flex_video.html' },
-
-  { value: 'Forms', data: '/docs/components/forms.html' },
-
-  { value: 'Abide Validation', data: '/docs/components/abide.html' },
-  { value: 'Form Validation', data: '/docs/components/abide.html' },
-  { value: 'Field Validation', data: '/docs/components/abide.html' },
-
-  { value: 'Buttons', data: '/docs/components/buttons.html' },
-  { value: 'Button Groups', data: '/docs/components/button_groups.html' },
-  { value: 'Button Bar', data: '/docs/components/button_groups.html' },
-  { value: 'Split Buttons', data: '/docs/components/split_buttons.html' },
-  { value: 'Dropdown Buttons', data: '/docs/components/dropdown_buttons.html' },
-
-  { value: 'Type', data: '/docs/components/typography.html' },
-  { value: 'Typography', data: '/docs/components/typography.html' },
-  { value: 'Type-setting', data: '/docs/components/typography.html' },
-  { value: 'Composition', data: '/docs/components/typography.html' },
-
-  { value: 'Inline Lists', data: '/docs/components/inline_lists.html' },
-  { value: 'Lists', data: '/docs/components/inline_lists.html' },
-
-  { value: 'Labels', data: '/docs/components/labels.html' },
-  { value: 'Tags', data: '/docs/components/labels.html' },
-
-  { value: 'Keystrokes', data: '/docs/components/keystrokes.html' },
-
-  { value: 'Reveal Modal', data: '/docs/components/reveal.html' },
-  { value: 'Modal', data: '/docs/components/reveal.html' },
-
-  { value: 'Alerts', data: '/docs/components/alert_boxes.html' },
-  { value: 'Error', data: '/docs/components/alert_boxes.html' },
-  { value: 'Success', data: '/docs/components/alert_boxes.html' },
-  { value: 'Warning', data: '/docs/components/alert_boxes.html' },
-
-  { value: 'Panels', data: '/docs/components/panels.html' },
-  { value: 'Wells', data: '/docs/components/panels.html' },
-
-  { value: 'Tooltips', data: '/docs/components/tooltips.html' },
-  { value: 'Popover', data: '/docs/components/tooltips.html' },
-
-  { value: 'Joyride', data: '/docs/components/joyride.html' },
-  { value: 'Tooltip Tour', data: '/docs/components/joyride.html' },
-  { value: 'Guider', data: '/docs/components/joyride.html' },
-  { value: 'Tooltip Walk-through', data: '/docs/components/joyride.html' },
-
-  { value: 'Dropdowns', data: '/docs/components/dropdown.html' },
-  { value: 'Pricing Tables', data: '/docs/components/pricing_tables.html' },
-
-  { value: 'Progress Bars', data: '/docs/components/progress_bars.html' },
-
-  { value: 'Tables', data: '/docs/components/tables.html' },
-
-  { value: 'Accordion', data: '/docs/components/accordion.html' },
-  { value: 'Collapse', data: '/docs/components/accordion.html' },
-
-  { value: 'Visibility', data: '/docs/components/visibility.html' },
-
-  { value: 'Tabs', data: '/docs/components/tabs.html' },
-  { value: 'Changelog', data: '/docs/changelog.html' },
-  { value: 'Compatibility', data: '/docs/compatibility.html' },
-  { value: 'FAQ', data: '/docs/components/faq.html' },
-  { value: 'Foundation 4', data: '/docs/v/4.3.2/' },
-  { value: 'Foundation 3', data: 'http://foundation3.zurb.com/old-docs/f3/' },
-  { value: 'Foundation 2', data: '/docs/v/2.2.1' },
-  // { value: 'What Comes With Foundation', data: '/docs/components/#what comes with foundation' },
-  { value: 'Download', data: '/develop/download.html' }
-];
+var components = [];
+$("[data-search]")
+  .each(function() {
+    var self = $(this),
+        searchTerm = self.text().trim(),
+        otherSearchTerms = self.data("search").trim(),
+        url = self.attr("href");
+    components.push({
+      value: searchTerm,
+      data: self.attr("href")
+    });
+    if (otherSearchTerms !== "") {
+      $.each(otherSearchTerms.split(","), function(idx, el) {
+        components.push({
+          value: el,
+          data: self.attr("href")
+        });
+      });
+    }
+  });
 
 $('#autocomplete').autocomplete({
   lookup: components,
   autoSelectFirst: true,
   onSelect: function (suggestion) {
-    // if (/index.html/i.test(window.location.href)) {
-    //   return window.location = 'components/' + suggestion.data;
-    // }
-
-    // return window.location = '../components/' + suggestion.data;
-    window.location = "http://foundation5.zurb.com" + suggestion.data;
+    window.location = suggestion.data;
   }
 });
 
