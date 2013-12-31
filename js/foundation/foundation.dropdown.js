dec06b26cc4c3f927d328c89b37cccc933a6c67c
nesl247 <harrison@theheck.me>
2013-12-31 21:07:59 +0000

    Respect data-options for elements added after foundation is initialized

--- js/foundation/foundation.dropdown.js
+++ js/foundation/foundation.dropdown.js
@@ -25,7 +25,7 @@
       $(this.scope)
         .off('.dropdown')
         .on('click.fndtn.dropdown', '[data-dropdown]', function (e) {
-          var settings = $(this).data('dropdown-init') || self.settings;
+          var settings = $.extend({}, self.settings, self.data_options($(this)));
           e.preventDefault();
 
           if (!settings.is_hover || Modernizr.touch) self.toggle($(this));
@@ -42,18 +42,18 @@
                 target = $("[data-dropdown='" + dropdown.attr('id') + "']");
           }
 
-          var settings = target.data('dropdown-init') || self.settings;
+          var settings = $.extend({}, self.settings, self.data_options(target));
           if (settings.is_hover) self.open.apply(self, [dropdown, target]);
         })
         .on('mouseleave.fndtn.dropdown', '[data-dropdown], [data-dropdown-content]', function (e) {
           var $this = $(this);
           self.timeout = setTimeout(function () {
             if ($this.data('dropdown')) {
-              var settings = $this.data('dropdown-init') || self.settings;
+              var settings = $.extend({}, self.settings, self.data_options($this));
               if (settings.is_hover) self.close.call(self, $('#' + $this.data('dropdown')));
             } else {
               var target = $('[data-dropdown="' + $(this).attr('id') + '"]'),
-                  settings = target.data('dropdown-init') || self.settings;
+                  settings = $.extend({}, self.settings, self.data_options(target));
               if (settings.is_hover) self.close.call(self, $this);
             }
           }.bind(this), 150);
