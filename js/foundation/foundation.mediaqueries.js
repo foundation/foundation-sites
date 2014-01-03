bd60b12779fa826f8781b597d1c3ed7626d17f95
Patrick Cason <me@patrickcason.com>
2014-01-03 15:47:22 -0600

    Added media query support for Javascript while letting the processing primarily be done in SASS, not JS

---
+++ js/foundation/foundation.mediaqueries.js
@@ -0,0 +1,42 @@
+;(function ($, window, document, undefined) {
+  'use strict';
+
+  Foundation.libs.mediaqueries = {
+    name : 'mediaqueries',
+
+    version : '5.0.0',
+
+    settings : {},
+
+    init : function (scope, method, options) {
+      this.events();
+    },
+
+    events : function () {
+		var $stateIndicator = $('body').append('<div>').addClass('state-indicator');
+		var level = parseInt($stateIndicator.css('z-index'), 10);
+
+		if(level > 0 && level <= 2)
+			$('body').attr('data-mediaquery', 'small');
+		else if(level > 2 && level <= 4)
+			$('body').attr('data-mediaquery', 'medium');
+		else if(level > 4 && level <= 6)
+			$('body').attr('data-mediaquery', 'large');
+		else if(level > 6 && level <= 8)
+			$('body').attr('data-mediaquery', 'xlarge');
+		else
+			$('body').attr('data-mediaquery', 'xxlarge');
+    },
+
+    reflow : function () {}
+  };
+}(jQuery, this, this.document));
+
+/*
+	$(window).resize(function() {
+		if($('body').attr('data-mediaquery') == "small")
+			// Small layout
+		else
+			//this
+	});
+*/
