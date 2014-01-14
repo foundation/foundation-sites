f9c49ada51032345a2f7d6d78bd98f1de1ae392e
Jordan Humphreys <jordan@zurb.com>
2014-01-14 14:57:31 -0800

    Bump version to 5.0.3

--- js/foundation/foundation.joyride.js
+++ js/foundation/foundation.joyride.js
@@ -6,7 +6,7 @@
   Foundation.libs.joyride = {
     name : 'joyride',
 
-    version : '5.0.0',
+    version : '5.0.3',
 
     defaults : {
       expose               : false,      // turn on or off the expose feature
