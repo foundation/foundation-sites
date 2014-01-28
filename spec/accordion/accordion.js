189ab7c148c605a6e2f3106804b6b30a927a3a18
Sean Timm <sean@seantimm.com>
2014-01-27 21:51:51 -0700

    Added basic accordion tests

--- spec/accordion/accordion.js
+++ spec/accordion/accordion.js
@@ -11,4 +11,32 @@ describe('accordion:', function() {
       return result;
     });
   });
+
+  describe('basic accordion with tab child', function() {
+    beforeEach(function() {
+      document.body.innerHTML = __html__['spec/accordion/basic.html'];
+    });
+
+    it('should default to the active tab', function() {
+      $(document).foundation();
+
+      expect($('#panel1')).toBeVisible();
+      expect($('#panel2-1')).toBeVisible();
+      expect($('#panel2-2')).toBeHidden();
+      expect($('#panel2')).toBeHidden();
+      expect($('#panel3')).toBeHidden();
+    });
+
+    it('should switch to the clicked section', function() {
+      $(document).foundation();
+
+      $('#panel2').prev().click();
+
+      expect($('#panel1')).toBeHidden();
+      expect($('#panel2-1')).toBeHidden();
+      expect($('#panel2-2')).toBeHidden();
+      expect($('#panel2')).toBeVisible();
+      expect($('#panel3')).toBeHidden();
+    });
+  });
 });
