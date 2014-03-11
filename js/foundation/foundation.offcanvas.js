d3855afe5ee811d3efb63ff28df67d5c3113f35a
Lonewolf <nikhilkalige@gmail.com>
2014-03-11 21:58:02 +0530

    Replace event content with functions

--- js/foundation/foundation.offcanvas.js
+++ js/foundation/foundation.offcanvas.js
@@ -13,42 +13,27 @@
     },
 
     events : function () {
-      var S = this.S;
+      var self = this,
+          S = self.S;
 
       S(this.scope).off('.offcanvas')
         .on('click.fndtn.offcanvas', '.left-off-canvas-toggle', function (e) {
-          e.preventDefault();
-          S(this).closest('.off-canvas-wrap').toggleClass('move-right');
+          self.click_toggle_class(e, 'move-right');
         })
         .on('click.fndtn.offcanvas', '.exit-off-canvas', function (e) {
-          e.preventDefault();
-          S(".off-canvas-wrap").removeClass("move-right");
+          self.click_remove_class(e, 'move-right');
         })
         .on('click.fndtn.offcanvas', '.left-off-canvas-menu a', function (e) {
-          e.preventDefault();
-          var href = $(this).attr('href');
-          S('.off-canvas-wrap').on('transitionend webkitTransitionEnd oTransitionEnd', function(e) {
-              window.location = href
-              S('.off-canvas-wrap').off('transitionend webkitTransitionEnd oTransitionEnd');
-          });
-          S(".off-canvas-wrap").removeClass("move-right");
+          self.click_add_animation_handler(e, 'move-right');
         })
         .on('click.fndtn.offcanvas', '.right-off-canvas-toggle', function (e) {
-          e.preventDefault();
-          S(this).closest(".off-canvas-wrap").toggleClass("move-left");
+          self.click_toggle_class(e, 'move-left');
         })
         .on('click.fndtn.offcanvas', '.exit-off-canvas', function (e) {
-          e.preventDefault();
-          S(".off-canvas-wrap").removeClass("move-left");
+          self.click_remove_class(e, 'move-left');
         })
         .on('click.fndtn.offcanvas', '.right-off-canvas-menu a', function (e) {
-          e.preventDefault();
-          var href = $(this).attr('href');
-          S('.off-canvas-wrap').on('transitionend webkitTransitionEnd oTransitionEnd', function(e) {
-              window.location = href
-              S('.off-canvas-wrap').off('transitionend webkitTransitionEnd oTransitionEnd');
-          });
-          S(".off-canvas-wrap").removeClass("move-left");
+          self.click_add_animation_handler(e, 'move-left');
         });
     },
 
