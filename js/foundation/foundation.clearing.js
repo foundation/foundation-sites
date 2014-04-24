821d8779113fa28373bc088a3da57c4c278f0026
Adam Sommer <asommer70@gmail.com>
2014-04-23 20:28:17 -0400

    Removed debugging statements.

--- js/foundation/foundation.clearing.js
+++ js/foundation/foundation.clearing.js
@@ -55,7 +55,6 @@
         .off('.clearing')
         .on('click.fndtn.clearing', 'ul[' + this.attr_name() + '] li ' + this.settings.open_selectors,
           function (e, current, target) {
-            console.log('opened...');
             var current = current || S(this),
                 target = target || current,
                 next = current.next('li'),
@@ -66,7 +65,6 @@
 
             if (!settings) {
               self.init();
-      console.log(this.attr_name());
               settings = current.closest('[' + self.attr_name() + ']').data(self.attr_name(true) + '-init');
             }
 
