7a799ba140dc9c630a4fa550092d85ccb2cb99b9
David Govea <govea.d@gmail.com>
2014-05-10 22:27:45 -0700

    Consolidate offcanvas event binding
    Conflicts:
    	js/foundation/foundation.offcanvas.js

--- js/foundation/foundation.offcanvas.js
+++ js/foundation/foundation.offcanvas.js
@@ -17,54 +17,40 @@
 
     events : function () {
       var self = this,
-          S = self.S;
-          
+          S = self.S,
+          move_class = '',
+          right_postfix = '',
+          left_postfix = '';
+
       if (this.settings.open_method === 'move'){
-        S(this.scope).off('.offcanvas')
-          .on('click.fndtn.offcanvas', '.left-off-canvas-toggle', function (e) {
-            self.click_toggle_class(e, 'move-right');
-          })
-          .on('click.fndtn.offcanvas', '.left-off-canvas-menu a', function (e) {
-            var settings = self.get_settings(e)
-            if (settings.close_on_click)
-              S(".off-canvas-wrap").removeClass("move-right");
-          })
-          .on('click.fndtn.offcanvas', '.right-off-canvas-toggle', function (e) {
-            self.click_toggle_class(e, 'move-left');
-          })
-          .on('click.fndtn.offcanvas', '.right-off-canvas-menu a', function (e) {
-            var settings = self.get_settings(e)
-            if (settings.close_on_click)
-              S(".off-canvas-wrap").removeClass("move-left");
-          })
-          .on('click.fndtn.offcanvas', '.exit-off-canvas', function (e) {
-            self.click_remove_class(e, 'move-left');
-            self.click_remove_class(e, 'move-right');
-          })
+        move_class = 'move-';
+        right_postfix = 'right';
+        left_postfix = 'left';
       } else if (this.settings.open_method === 'overlap') {
-        S(this.scope).off('.offcanvas')
-          .on('click.fndtn.offcanvas', '.left-off-canvas-toggle', function (e) {
-            self.click_toggle_class(e, 'offcanvas-overlap');
-          })
-          .on('click.fndtn.offcanvas', '.left-off-canvas-menu a', function (e) {
-            var settings = self.get_settings(e)
-            if (settings.close_on_click)
-              S(".off-canvas-wrap").removeClass("offcanvas-overlap");
-          })
-          .on('click.fndtn.offcanvas', '.right-off-canvas-toggle', function (e) {
-            self.click_toggle_class(e, 'offcanvas-overlap');
-          })
-          .on('click.fndtn.offcanvas', '.right-off-canvas-menu a', function (e) {
-            var settings = self.get_settings(e)
-            if (settings.close_on_click)
-              S(".off-canvas-wrap").removeClass("offcanvas-overlap");
-          })
-          .on('click.fndtn.offcanvas', '.exit-off-canvas', function (e) {
-            self.click_remove_class(e, 'offcanvas-overlap');
-          })
-      } else {
-        return;
+        move_class = 'offcanvas-overlap';
       }
+
+      S(this.scope).off('.offcanvas')
+        .on('click.fndtn.offcanvas', '.left-off-canvas-toggle', function (e) {
+        self.click_toggle_class(e, move_class + right_postfix);
+        })
+        .on('click.fndtn.offcanvas', '.left-off-canvas-menu a', function (e) {
+          var settings = self.get_settings(e)
+          if (settings.close_on_click)
+          S(".off-canvas-wrap").removeClass(move_class + right_postfix);
+        })
+        .on('click.fndtn.offcanvas', '.right-off-canvas-toggle', function (e) {
+        self.click_toggle_class(e, move_class + left_postfix);
+        })
+        .on('click.fndtn.offcanvas', '.right-off-canvas-menu a', function (e) {
+          var settings = self.get_settings(e)
+          if (settings.close_on_click)
+          S(".off-canvas-wrap").removeClass(move_class + left_postfix);
+        })
+        .on('click.fndtn.offcanvas', '.exit-off-canvas', function (e) {
+        self.click_remove_class(e, move_class + left_postfix);
+        if (right_postfix) self.click_remove_class(e, move_class + right_postfix);
+      });
     },
 
     click_toggle_class: function(e, class_name) {
