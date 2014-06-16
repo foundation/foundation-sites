9a6d2fc3ef34388a05c611634457f5fe18fc2fd4
Eric Morris <eric@zurb.com>
2014-06-16 14:08:56 -0700

    Set default value for reveal root_element

--- js/foundation/foundation.reveal.js
+++ js/foundation/foundation.reveal.js
@@ -15,6 +15,7 @@
       close_on_esc: true,
       dismiss_modal_class: 'close-reveal-modal',
       bg_class: 'reveal-modal-bg',
+      root_element: 'body',
       open: function(){},
       opened: function(){},
       close: function(){},
@@ -249,7 +250,7 @@
       // is modal
       if (css) {
         var settings = el.data(this.attr_name(true) + '-init') || this.settings,
-            root_element = settings.root_element || 'body';
+            root_element = settings.root_element;
 
         if (el.parent(root_element).length === 0) {
           var placeholder = el.wrap('<div style="display: none;" />').parent();
