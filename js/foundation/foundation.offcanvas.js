57a1b07a4052436754cc07c46a5c07cbd752a913
Eduardo Cataño <socketz@gmail.com>
2014-06-20 20:42:16 +0200

    Changed \t to 2 space chars

--- js/foundation/foundation.offcanvas.js
+++ js/foundation/foundation.offcanvas.js
@@ -7,7 +7,7 @@
     version : '5.2.1',
 
     settings : {
-    	close_on_click: true
+      close_on_click: true
     },
 
     init : function (scope, method, options) {
@@ -24,32 +24,32 @@
             S(".left-off-canvas-submenu").removeClass('move-right');
         })
         .on('click.fndtn.offcanvas', '.left-off-canvas-menu a', function (e) {
-        	if(self.settings.close_on_click && !S(this).parent().hasClass("has-submenu") && !S(this).parent().hasClass("back")){
-        		S(".off-canvas-wrap").removeClass("move-right");
-        		S(this).parent().parent().removeClass('move-right');
-        	}else if(S(this).parent().hasClass("has-submenu")){
-        		e.preventDefault();
-        		S(this).siblings(".left-off-canvas-submenu").toggleClass('move-right');
-        	}else if(S(this).parent().hasClass("back")){
-        		e.preventDefault();
-        		S(this).parent().parent().removeClass('move-right');
-        	}
+          if(self.settings.close_on_click && !S(this).parent().hasClass("has-submenu") && !S(this).parent().hasClass("back")){
+            S(".off-canvas-wrap").removeClass("move-right");
+            S(this).parent().parent().removeClass('move-right');
+          }else if(S(this).parent().hasClass("has-submenu")){
+            e.preventDefault();
+            S(this).siblings(".left-off-canvas-submenu").toggleClass('move-right');
+          }else if(S(this).parent().hasClass("back")){
+            e.preventDefault();
+            S(this).parent().parent().removeClass('move-right');
+          }
         })
         .on('click.fndtn.offcanvas', '.right-off-canvas-toggle', function (e) {
           self.click_toggle_class(e, 'move-left');
           S(".right-off-canvas-submenu").removeClass('move-left');
         })
         .on('click.fndtn.offcanvas', '.right-off-canvas-menu a', function (e) {
-        	if(self.settings.close_on_click && !S(this).parent().hasClass("has-submenu") && !S(this).parent().hasClass("back")){
-        		S(".off-canvas-wrap").removeClass("move-left");
-        		S(this).parent().parent().removeClass('move-left');
-        	}else if(S(this).parent().hasClass("has-submenu")){
-        		e.preventDefault();
-        		S(this).siblings(".right-off-canvas-submenu").toggleClass('move-left');
-        	}else if(S(this).parent().hasClass("back")){
-        		e.preventDefault();
-        		S(this).parent().parent().removeClass('move-left');
-        	}
+          if(self.settings.close_on_click && !S(this).parent().hasClass("has-submenu") && !S(this).parent().hasClass("back")){
+            S(".off-canvas-wrap").removeClass("move-left");
+            S(this).parent().parent().removeClass('move-left');
+          }else if(S(this).parent().hasClass("has-submenu")){
+            e.preventDefault();
+            S(this).siblings(".right-off-canvas-submenu").toggleClass('move-left');
+          }else if(S(this).parent().hasClass("back")){
+            e.preventDefault();
+            S(this).parent().parent().removeClass('move-left');
+          }
         })
         .on('click.fndtn.offcanvas', '.exit-off-canvas', function (e) {
           self.click_remove_class(e, 'move-left');
