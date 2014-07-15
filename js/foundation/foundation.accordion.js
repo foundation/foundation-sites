16dc19b4f5e09d20bd1c04d341c7f8c8a91ed22a
Joe Theuerkauf <joe@thook.me>
2014-07-15 02:07:16 -0400

    Fixes #5445 - Limit Accordion element selection to the current Accordion.

--- js/foundation/foundation.accordion.js
+++ js/foundation/foundation.accordion.js
@@ -26,10 +26,10 @@
         var accordion = S(this).closest('[' + self.attr_name() + ']'),
             target = S('#' + this.href.split('#')[1]),
             siblings = S('dd > .content', accordion),
-            aunts = $('>dd', accordion),
+            aunts = $('> dd', accordion),
             groupSelector = self.attr_name() + '=' + accordion.attr(self.attr_name()),
             settings = accordion.data(self.attr_name(true) + '-init'),
-            active_content = S('dd > .content.' + settings.active_class, accordion);
+            active_content = $('> dd > .content.' + settings.active_class, accordion);
         e.preventDefault();
 
         if (accordion.attr(self.attr_name())) {
