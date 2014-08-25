e9a2cd57ba10647fc046117a7d020b28e539a0c4
Kevin Lozandier <lozandier@gmail.com>
2014-08-25 14:57:23 -0700

    Edit dropdown.js files to make components depending on this file more accessible by automatically changing aria-expanded, aria-hidden values when dropdown is pressed and unpressed. Markup changes now necessary to the dropdown documentation to make it clear what is being changed and what the dropdown components should be markedup initially to be a more accessible component

--- js/foundation/foundation.dropdown.js
+++ js/foundation/foundation.dropdown.js
@@ -102,10 +102,16 @@
 
     close: function (dropdown) {
       var self = this;
+      console.log(dropdown);
+      console.log("slf.attr_name", self.attr_name());
       dropdown.each(function () {
+        var original_target = $('[' + self.attr_name() + '=' + dropdown[0].id + ']') || $('aria-controls=' + dropdown[0].id+ ']');
+        original_target.attr('aria-expanded', "false");
+        console.log(original_target);
         if (self.S(this).hasClass(self.settings.active_class)) {
           self.S(this)
             .css(Foundation.rtl ? 'right':'left', '-99999px')
+            .attr('aria-hidden', "true")
             .removeClass(self.settings.active_class)
             .prev('[' + self.attr_name() + ']')
             .removeClass(self.settings.active_class)
@@ -129,6 +135,9 @@
             .addClass(this.settings.active_class), target);
         dropdown.prev('[' + this.attr_name() + ']').addClass(this.settings.active_class);
         dropdown.data('target', target.get(0)).trigger('opened').trigger('opened.fndtn.dropdown', [dropdown, target]);
+        dropdown.attr('aria-hidden', 'false');
+        target.attr('aria-expanded', 'true')
+        dropdown.focus();
     },
 
     data_attr: function () {
