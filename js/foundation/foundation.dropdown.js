foundation/foundation-sites

commit d9a5a7ca71fcf262d53b05a3af1e90a2dc142995

Add drop directions.

js/foundation/foundation.dropdown.js
@@ -4,10 +4,11 @@
   Foundation.libs.dropdown = {
     name : 'dropdown',
 
-    version : '5.1.1',
+    version : '5.2.0',
 
     settings : {
       active_class: 'open',
+      align: 'bottom',
       is_hover: false,
       opened: function(){},
       closed: function(){}
@@ -153,41 +154,97 @@
     },
 
     css : function (dropdown, target) {
-      var offset_parent = dropdown.offsetParent(),
-          position = target.offset();
-
-      position.top -= offset_parent.offset().top;
-      position.left -= offset_parent.offset().left;
-
       if (this.small()) {
+        var p = this.dirs._base.call(dropdown, target);
+
         dropdown.css({
           position : 'absolute',
           width: '95%',
           'max-width': 'none',
-          top: position.top + target.outerHeight()
+          top: p.top + target.outerHeight()
         });
+
         dropdown.css(Foundation.rtl ? 'right':'left', '2.5%');
       } else {
-        if (!Foundation.rtl && this.S(window).width() > dropdown.outerWidth() + target.offset().left) {
-          var left = position.left;
-          if (dropdown.hasClass('right')) {
-            dropdown.removeClass('right');
-          }
-        } else {
-          if (!dropdown.hasClass('right')) {
-            dropdown.addClass('right');
-          }
-          var left = position.left - (dropdown.outerWidth() - target.outerWidth());
-        }
+        var settings = target.data(this.attr_name(true) + '-init') || this.settings;
+        this.style(dropdown, target, settings);
+      //   if (!Foundation.rtl && this.S(window).width() > dropdown.outerWidth() + target.offset().left) {
+      //     var left = position.left;
+      //     if (dropdown.hasClass('right')) {
+      //       dropdown.removeClass('right');
+      //     }
+      //   } else {
+      //     if (!dropdown.hasClass('right')) {
+      //       dropdown.addClass('right');
+      //     }
+      //     var left = position.left - (dropdown.outerWidth() - target.outerWidth());
+      //   }
 
-        dropdown.attr('style', '').css({
-          position : 'absolute',
-          top: position.top + target.outerHeight(),
-          left: left
-        });
+      //   dropdown.attr('style', '').css({
+      //     position : 'absolute',
+      //     top: position.top + target.outerHeight(),
+      //     left: left
+      //   });
       }
 
       return dropdown;
+    },
+
+    style : function (dropdown, target, settings) {
+      var css = $.extend({position: 'absolute'}, this.dirs[settings.align].call(dropdown, target, settings));
+
+      dropdown.attr('style', '').css(css);
+    },
+
+    // return CSS property object
+    // `this` is the dropdown
+    dirs : {
+      _base : function (t) {
+        var o_p = this.offsetParent(),
+            o = o_p.offset(),
+            p = t.offset();
+
+        p.top -= o.top;
+        p.left -= o.left;
+
+        return p;
+      },
+      top: function (t, s) {
+        var p = Foundation.libs.dropdown.dirs._base.call(this, t);
+
+        this.addClass('drop-top');
+
+        if (Foundation.rtl) {
+          this.addClass('right');
+          return {left: p.left - this.outerWidth() + t.outerWidth(), top: p.top - this.outerHeight()};
+        }
+
+        return {left: p.left, top: p.top - this.outerHeight()};
+      },
+      bottom: function (t, s) {
+        var p = Foundation.libs.dropdown.dirs._base.call(this, t);
+
+        if (Foundation.rtl) {
+          this.addClass('right');
+          return {left: p.left - this.outerWidth() + t.outerWidth(), top: p.top + t.outerHeight()};
+        }
+
+        return {left: p.left, top: p.top + t.outerHeight()};
+      },
+      left: function (t, s) {
+        var p = Foundation.libs.dropdown.dirs._base.call(this, t);
+
+        this.addClass('drop-left');
+
+        return {left: p.left - this.outerWidth(), top: p.top};
+      },
+      right: function (t, s) {
+        var p = Foundation.libs.dropdown.dirs._base.call(this, t);
+
+        this.addClass('drop-right');
+
+        return {left: p.left + t.outerWidth(), top: p.top};
+      }
     },
 
     small : function () {
@@ -200,7 +257,6 @@
       this.S('html, body').off('.fndtn.dropdown');
       this.S(window).off('.fndtn.dropdown');
       this.S('[data-dropdown-content]').off('.fndtn.dropdown');
-      this.settings.init = false;
     },
 
     reflow : function () {}

doc/layouts/default.html
@@ -1,6 +1,6 @@
 <!doctype html>
 <!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
-<html class="no-js" lang="en">
+<html class="no-js" lang="en" dir="rtl">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

doc/pages/components/dropdown.html
@@ -50,8 +50,8 @@ Additional classes can be added to your dropdown to change its appearance.
   </div>
   <div class="large-6 columns">
     <h4>Rendered HTML</h4>
-    <a href="#" class="button" data-dropdown="tinyDrop">Link Dropdown &raquo;</a>
-    <ul id="tinyDrop" class="medium f-dropdown" data-dropdown-content>
+    <a href="#" class="button" data-dropdown="tinyDrop2" data-options="align: top">Link Dropdown &raquo;</a>
+    <ul id="tinyDrop2" class="medium f-dropdown" data-dropdown-content>
       <li><a href="#">This is a link</a></li>
       <li><a href="#">This is another</a></li>
       <li><a href="#">Yet another</a></li>

js/foundation/foundation.js
@@ -281,7 +281,7 @@
   window.Foundation = {
     name : 'Foundation',
 
-    version : '5.1.1',
+    version : '5.2.0',
 
     media_queries : {
       small : S('.foundation-mq-small').css('font-family').replace(/^[\/\\'"]+|(;\s?})+|[\/\\'"]+$/g, ''),

scss/foundation/_settings.scss
@@ -387,6 +387,8 @@
 // $clearing-carousel-thumb-width: 120px;
 // $clearing-carousel-thumb-active-border: 1px solid rgb(255,255,255);
 
+$text-direction: rtl;
+
 // Dropdown
 
 // $include-html-dropdown-classes: $include-html-classes;

commit 93240c3ce6e660ac7cf3b4e1ead20b61e0a7de2f

Brought back auto-aligning dropdown if it stretches outside of the viewport and no align is given, broken in d9a5a7c, fixes #4619

js/foundation/foundation.dropdown.js
@@ -248,7 +248,8 @@
           self.adjust_pip(this,t,s,p);
         }
 
-        if (self.rtl) {
+        if (self.rtl || !($(window).width() > this.outerWidth() + t.offset().left)) {
+          self.adjust_pip(this, t, s, p, true);
           return {left: p.left - this.outerWidth() + t.outerWidth(), top: p.top + t.outerHeight()};
         }
 
@@ -271,7 +272,7 @@
     },
 
     // Insert rule to style psuedo elements
-    adjust_pip : function (dropdown,target,settings,position) {
+    adjust_pip : function (dropdown,target,settings,position,switch_sides) {
       var sheet = Foundation.stylesheet,
           pip_offset_base = 8;
 
@@ -280,6 +281,10 @@
       }
       else if (this.small()) {
         pip_offset_base += position.left - 8;
+      }
+      if(switch_sides) {
+        console.log('a');
+        pip_offset_base = dropdown.innerWidth() - 16;
       }
 
       this.rule_idx = sheet.cssRules.length;