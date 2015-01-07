0a5c9f811d3835f650cf1efb011115ef67420875
Rafi <rafibomb@users.noreply.github.com>
2015-01-07 15:34:27 -0800

    Revert "[Magellan] Check for arrivals after setting expedition position"

--- js/foundation/foundation.magellan.js
+++ js/foundation/foundation.magellan.js
@@ -93,7 +93,6 @@
         expedition.data(self.data_attr('magellan-top-offset'), top_offset);
         expedition.attr('style', styles);
       });
-      self.check_for_arrivals();
     },
 
     update_expedition_positions : function() {
