b05b50390cc8ec4c72c3ca9c7a2f881d8c4c2a74
Geoff Kimball <geoff@zurb.com>
2015-02-23 16:05:38 -0800

    Remove unneeded code for now, setup blank HTML file to test components on

--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,64 @@
-var gulp  = require('gulp');
-var Super = require('supercollider');
-var $     = require('gulp-load-plugins')();
-$.uncomment = require('./lib/uncomment-css');
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
 
 var files = {
-  sassSrc: 'docs_old/assets/scss/docs.scss',
+  sassSrc: 'scss/foundation.scss',
   sassPaths: ['scss'],
   javascript: ['js/foundation/foundation.*.js', 'js/foundation/foundation.js']
 }
 
-gulp.task('dist', function() {
-  gulp.src('scss/foundation.scss')
-    .pipe($.sass({
-      includePaths: files.sassPaths
-    }))
-    .pipe($.uncomment())
-    .pipe(gulp.dest('dist/css'))
-    .pipe($.minifyCss({
-      advanced: false,
-      keepSpecialComments: 0
-    }))
-    .pipe($.rename('foundation.min.css'))
-    .pipe(gulp.dest('dist/css'));
-});
+// gulp.task('dist', function() {
+//   gulp.src('scss/foundation.scss')
+//     .pipe($.sass({
+//       includePaths: files.sassPaths
+//     }))
+//     .pipe($.uncomment())
+//     .pipe(gulp.dest('dist/css'))
+//     .pipe($.minifyCss({
+//       advanced: false,
+//       keepSpecialComments: 0
+//     }))
+//     .pipe($.rename('foundation.min.css'))
+//     .pipe(gulp.dest('dist/css'));
+// });
 
-gulp.task('html', function() {
-  gulp.src('docs/pages/**/*.html')
-    .pipe($.assemble({
-      flatten: false,
-      assets: 'build/assets',
-      data: ['docs/data/*.json'],
-      partials: ['docs/includes/**/*.{html,scss}'],
-      layoutdir: 'docs/layouts',
-      layout: 'default.html'
-    }))
-    .pipe(gulp.dest('build/'))
-});
+// gulp.task('html', function() {
+//   gulp.src('docs/pages/**/*.html')
+//     .pipe($.assemble({
+//       flatten: false,
+//       assets: 'build/assets',
+//       data: ['docs/data/*.json'],
+//       partials: ['docs/includes/**/*.{html,scss}'],
+//       layoutdir: 'docs/layouts',
+//       layout: 'default.html'
+//     }))
+//     .pipe(gulp.dest('build/'))
+// });
 
-gulp.task('docs', function() {
-  Super({
-    html: './scss',
-    sass: './scss',
-    js: './js/**/*.js',
-    dest: './build'
-  });
-});
+// gulp.task('docs', function() {
+//   Super({
+//     html: './scss',
+//     sass: './scss',
+//     js: './js/**/*.js',
+//     dest: './build'
+//   });
+// });
 
-gulp.task('css', function() {
+gulp.task('sass', function() {
   gulp.src(files.sassSrc)
     .pipe($.sass({
       includePaths: files.sassPaths
     }))
-    .pipe(gulp.dest('build/assets/scss'));
+    .pipe(gulp.dest('dist/assets/css'));
 });
 
 gulp.task('javascript', function() {
   gulp.src(files.javascript)
     .pipe($.concat('foundation.js'))
-    .pipe(gulp.dest('build/assets/js'));
+    .pipe(gulp.dest('dist/assets/js'));
 });
 
-gulp.task('default', ['html', 'css', 'javascript'], function() {
-
+gulp.task('default', ['sass', 'javascript'], function() {
+  gulp.watch('scss/**/*', ['css']);
+  gulp.watch('js/**/*', ['javascript']);
 });
