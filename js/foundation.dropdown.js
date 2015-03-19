3dc6f3f2b822616114f557575aa3f159b0fa69e4
Geoff Kimball <geoff@zurb.com>
2015-03-19 13:51:47 -0700

    Add basic dropdown JS component

---
+++ js/foundation.dropdown.js
@@ -0,0 +1,67 @@
+!function(Foundation, $) {
+  'use strict';
+
+  /**
+   * Creates a new instance of Dropdown.
+   * @class
+   * @fires Dropdown#init
+   * @param {jQuery} element - jQuery object to make into a dropdown menu.
+   * @param {Object} options - Overrides to the default plugin settings.
+   */
+  function Dropdown(element, options) {
+    this.$element = element;
+    this.options = $.extend(this.defaults, options || {});
+
+    this.$openMenu = $();
+
+    this._init();
+
+    /**
+     * Fires when the plugin has been successfuly initialized.
+     * @event Drilldown#init
+     */
+    this.$element.trigger('init.zf.dropdown');
+  }
+
+  Dropdown.prototype.defaults = {
+    toggleOn: 'both'
+  }
+
+  Dropdown.prototype._init = function() {
+    this._prepareMenu(this.$element);
+  }
+
+  Dropdown.prototype._prepareMenu = function($elem) {
+    var _this = this;
+
+    $elem.children('li').each(function() {
+      var $submenu = $(this).children('[data-submenu]');
+
+      if ($submenu.length) {
+        $submenu.addClass('js-dropdown-nohover');
+
+        $(this).children('a').click(function(event) {
+          event.stopPropagation();
+
+          _this.toggleMenu($submenu);
+
+          return false;
+        }).on('mouseenter', function(event) {
+          console.log("Hi");
+          event.stopPropagation();
+          event.preventDefault();
+        });
+
+        _this._prepareMenu($submenu);
+      }
+    });
+  }
+
+  Dropdown.prototype.toggleMenu = function($target) {
+    this.$openMenu.removeClass('js-dropdown-active');
+    $target.toggleClass('js-dropdown-active');
+    this.$openMenu = $target;
+  }
+
+  Foundation.plugin('dropdown', Dropdown)
+}(Foundation, jQuery)
