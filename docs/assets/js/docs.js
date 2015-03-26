5b83ad1ecd1681089dabb0b74da6e8562c56f170
Geoff Kimball <geoff@zurb.com>
2015-03-26 13:49:34 -0700

    Add copy button for code samples

---
+++ docs/assets/js/docs.js
@@ -0,0 +1,30 @@
+$(document).foundation();
+
+$(function() {
+  if (!ZeroClipboard.isFlashUnusable()) {
+    var $buttonTemplate = $('<button class="docs-code-copy">Copy</button>');
+
+    // Look for code samples and set up a copy button on each
+    $('[data-docs-code]').each(function() {
+      var $button = $buttonTemplate.clone();
+      var text = $(this).find('code').text()
+        .replace('&lt;', '<')
+        .replace('&gt;', '>');
+
+      $(this).prepend($button);
+
+      var clipboard = new ZeroClipboard($button);
+      clipboard.on('copy', function(event) {
+        clipboard.setData('text/plain', text);
+      });
+
+      // Change the text of the copy button when it's clicked on
+      $button.click(function() {
+        $(this).text('Copied!');
+        window.setTimeout(function() {
+          $button.text('Copy');
+        }, 3000);
+      });
+    });
+  }
+});
