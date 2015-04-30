c95a6cf4094b3e7c2db774da9613f0db3aae5315
Chris Sherman <chrisjsherm@users.noreply.github.com>
2015-04-30 16:35:09 -0400

    Update foundation.reveal.js
    Ensure open event has a target before accessing the 'data-reveal-id' attribute. I ran into target === undefined after calling $('#myModal').foundation('reveal', 'open');

--- js/foundation/foundation.reveal.js
+++ js/foundation/foundation.reveal.js
@@ -155,7 +155,7 @@
       settings = settings || this.settings;
 
 
-      if (modal.hasClass('open') && target.attr('data-reveal-id') == modal.attr('id')) {
+      if (modal.hasClass('open') && target !== undefined && target.attr('data-reveal-id') == modal.attr('id')) {
         return self.close(modal);
       }
 
