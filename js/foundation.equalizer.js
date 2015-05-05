7795549d4e89cbd8750db7527ca045bfde7257ee
Jeanie Chung <jeanie.chung@gmail.com>
2015-05-05 10:57:37 -0700

    Changes to prototype language so that Equalizer is included on the Foundation object.

--- js/foundation.equalizer.js
+++ js/foundation.equalizer.js
@@ -28,102 +28,100 @@
   /**
    * Default settings for plugin
    */
-  Equalizer.defaults = {
+  Equalizer.prototype.defaults = {
     equalizeOnStack: true
   };
 
-  Equalizer.prototype = {
-    /**
-     * Initializes the Equalizer plugin and calls functions to get equalizer functioning on load.
-     * @private
-     */
-    _init: function() {
-      this._reflow();
-    },
+  /**
+   * Initializes the Equalizer plugin and calls functions to get equalizer functioning on load.
+   * @private
+   */
+  Equalizer.prototype._init = function() {
+    this._reflow();
+  };
 
-    /**
-     * Initializes events for Equalizer.
-     * @private
-     */
-    _events: function() {
-      var self = this;
+  /**
+   * Initializes events for Equalizer.
+   * @private
+   */
+  Equalizer.prototype._events = function() {
+    var self = this;
 
-      this.$window
-        .off('.equalizer')
-        .on('resize.fndtn.equalizer', Foundation.throttle(function () {
-          self._reflow();
-        }.bind(this), 50));
-    },
-    /**
-     * Calls necessary functions to update Equalizer upon DOM change
-     * @private
-     */
-    _reflow: function() {
-      var self = this;
+    this.$window
+      .off('.equalizer')
+      .on('resize.fndtn.equalizer', Foundation.throttle(function () {
+        self._reflow();
+      }.bind(this), 50));
+  };
+  /**
+   * Calls necessary functions to update Equalizer upon DOM change
+   * @private
+   */
+  Equalizer.prototype._reflow = function() {
+    var self = this;
 
-      $('[' + this.attr + ']').each(function() {
-        var $eqParent       = $(this),
-            adjustedHeights;
+    $('[' + this.attr + ']').each(function() {
+      var $eqParent       = $(this),
+          adjustedHeights;
 
-        if ($eqParent.find('img').length) {
-          Foundation.imagesLoaded($eqParent.find('img'), function() {
-            console.log('image callback');
-            adjustedHeights = self.getHeights($eqParent);
-          });
-        }
-        else {
+      if ($eqParent.find('img').length) {
+        Foundation.imagesLoaded($eqParent.find('img'), function() {
+          console.log('image callback');
           adjustedHeights = self.getHeights($eqParent);
-        }
-
-        self.applyHeight($eqParent, adjustedHeights);   
-      });
-    },
-    /**
-     * Finds the outer heights of children contained within an Equalizer parent and returns them in an array
-     * @param {Object} $eqParent - A jQuery instance of an Equalizer container
-     * @return {array} heights - An array of heights of children within Equalizer container
-     */
-    getHeights: function($eqParent) {
-
-      var eqGroupName = $eqParent.data('equalizer'),
-          eqGroup     = eqGroupName ? $eqParent.find('[' + this.attr + '-watch="' + eqGroupName + '"]:visible') : $eqParent.find('[' + this.attr + '-watch]:visible'),
-          heights;
-      
-      eqGroup.height('inherit');
-
-      heights = eqGroup.map(function () { return $(this).outerHeight(false) }).get();
-      
-      return heights;
-    },
-    /**
-     * Changes the CSS height property of each child in an Equalizer parent to match the tallest
-     * @param {Object} $eqParent - A jQuery instance of an Equalizer container
-     * @param {array} heights - An array of heights of children within Equalizer container
-     * @fires Equalizer#preEqualized
-     * @fires Equalizer#postEqualized
-     */
-    applyHeight: function($eqParent, heights) {
-      var eqGroupName = $eqParent.data('equalizer'),
-          eqGroup     = eqGroupName ? $eqParent.find('['+this.attr+'-watch="'+eqGroupName+'"]:visible') : $eqParent.find('['+this.attr+'-watch]:visible'),
-          max         = Math.max.apply(null, heights);
-
-      /**
-       * Fires before the heights are applied
-       * @event Equalizer#preEqualized
-       */
-      $eqParent.trigger('preEqualized.fndtn.equalizer');
-
-      // for now, apply the max height found in the array
-      for (var i = 0; i < eqGroup.length; i++) {
-        $(eqGroup[i]).css('height', max);
+        });
+      }
+      else {
+        adjustedHeights = self.getHeights($eqParent);
       }
 
-      /**
-       * Fires when the heights have been applied
-       * @event Equalizer#postEqualized
-       */
-      $eqParent.trigger('postEqualized.fndtn.equalizer');
+      self.applyHeight($eqParent, adjustedHeights);   
+    });
+  };
+  /**
+   * Finds the outer heights of children contained within an Equalizer parent and returns them in an array
+   * @param {Object} $eqParent A jQuery instance of an Equalizer container
+   * @returns {Array} heights An array of heights of children within Equalizer container
+   */
+  Equalizer.prototype.getHeights = function($eqParent) {
+
+    var eqGroupName = $eqParent.data('equalizer'),
+        eqGroup     = eqGroupName ? $eqParent.find('[' + this.attr + '-watch="' + eqGroupName + '"]:visible') : $eqParent.find('[' + this.attr + '-watch]:visible'),
+        heights;
+    
+    eqGroup.height('inherit');
+
+    heights = eqGroup.map(function () { return $(this).outerHeight(false) }).get();
+    
+    return heights;
+  };
+  /**
+   * Changes the CSS height property of each child in an Equalizer parent to match the tallest
+   * @param {Object} $eqParent - A jQuery instance of an Equalizer container
+   * @param {array} heights - An array of heights of children within Equalizer container
+   * @fires Equalizer#preEqualized
+   * @fires Equalizer#postEqualized
+   */
+  Equalizer.prototype.applyHeight = function($eqParent, heights) {
+    var eqGroupName = $eqParent.data('equalizer'),
+        eqGroup     = eqGroupName ? $eqParent.find('['+this.attr+'-watch="'+eqGroupName+'"]:visible') : $eqParent.find('['+this.attr+'-watch]:visible'),
+        max         = Math.max.apply(null, heights);
+
+    /**
+     * Fires before the heights are applied
+     * @event Equalizer#preEqualized
+     */
+    $eqParent.trigger('preEqualized.fndtn.equalizer');
+
+    // for now, apply the max height found in the array
+    for (var i = 0; i < eqGroup.length; i++) {
+      $(eqGroup[i]).css('height', max);
     }
+
+    /**
+     * Fires when the heights have been applied
+     * @event Equalizer#postEqualized
+     */
+    $eqParent.trigger('postEqualized.fndtn.equalizer');
   };
 
   Foundation.plugin('equalizer', Equalizer);
