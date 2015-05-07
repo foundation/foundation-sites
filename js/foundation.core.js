dc2dcd5a113243ddfb4115f6539deef31d7f6f01
Jeanie Chung <jeanie.chung@gmail.com>
2015-05-07 11:05:48 -0700

    Added px units to breakpoints so media query API works. Added a current method so that we can easily grab the breakpoint.

--- js/foundation.core.js
+++ js/foundation.core.js
@@ -128,18 +128,85 @@
           $(this).data('zf-plugin', new plugin($(this), options));
         });
       });
+      
+      // popping this in here for now, will have to get a init method later
+      _this.MediaQuery.extractStyle();
     },
 
     MediaQuery: {
-      current: "small",
+      queries: {},
+      current: function() {
+        var mqObj = this.queries,
+            arr   = [];
+        // prepare to iterate through object in reverse
+        // so we can get the first "true" MQ we hit
+        for (var key in mqObj) {
+          arr.push(key);
+        }
+        for (var i = arr.length - 1; i >= 0; i--) {
+          var mqName = arr[i];
+          if (mqObj.hasOwnProperty(mqName) && window.matchMedia(mqObj[mqName]).matches) {
+            return mqName;
+          }
+        }
+      },
       is: function(mq) {
-        console.log(mq);
+        return window.matchMedia(mqObject[mq]).matches;
         //check if current mq against argument mq
         // return boolean
       },
       atLeast: function(mq) {
         console.log("i am at least" + mq);
         // return boolean
+      },
+      extractStyle: function() {
+        // set default mqs as well
+        var extractedStyles = $('.foundation-mq').css('font-family');
+        
+        // https://github.com/sindresorhus/query-string
+        function parseStyleToObject(str) {
+          var styleObject = {};
+
+          if (typeof str !== 'string') {
+            return styleObject;
+          }
+
+          str = str.trim().slice(1, -1); // browsers re-quote string style values
+
+          if (!str) {
+            return styleObject;
+          }
+
+          styleObject = str.split('&').reduce(function(ret, param) {
+            var parts = param.replace(/\+/g, ' ').split('=');
+            var key = parts[0];
+            var val = parts[1];
+            key = decodeURIComponent(key);
+
+            // missing `=` should be `null`:
+            // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+            val = val === undefined ? null : decodeURIComponent(val);
+
+            if (!ret.hasOwnProperty(key)) {
+              ret[key] = val;
+            } else if (Array.isArray(ret[key])) {
+              ret[key].push(val);
+            } else {
+              ret[key] = [ret[key], val];
+            }
+            return ret;
+          }, {});
+
+          return styleObject;
+        }
+        var namedQueries = parseStyleToObject(extractedStyles);
+        for (var key in namedQueries) {
+          namedQueries[key] = 'only screen and (min-width: ' + namedQueries[key].replace('rem', 'em') + ')';
+        }
+        // extend default queries
+
+        // assign queries object to parsed styles
+        this.queries = namedQueries;
       }
     },
 
@@ -1099,3 +1166,51 @@ function makeArray( obj ) {
   return ImagesLoaded;
 
 });
+
+
+/*! matchMedia() polyfill - Test a CSS media type/query in JS. Authors & copyright (c) 2012: Scott Jehl, Paul Irish, Nicholas Zakas, David Knight. Dual MIT/BSD license */
+
+window.matchMedia || (window.matchMedia = function() {
+    "use strict";
+
+    // For browsers that support matchMedium api such as IE 9 and webkit
+    var styleMedia = (window.styleMedia || window.media);
+
+    // For those that don't support matchMedium
+    if (!styleMedia) {
+        var style       = document.createElement('style'),
+            script      = document.getElementsByTagName('script')[0],
+            info        = null;
+
+        style.type  = 'text/css';
+        style.id    = 'matchmediajs-test';
+
+        script.parentNode.insertBefore(style, script);
+
+        // 'style.currentStyle' is used by IE <= 8 and 'window.getComputedStyle' for all other browsers
+        info = ('getComputedStyle' in window) && window.getComputedStyle(style, null) || style.currentStyle;
+
+        styleMedia = {
+            matchMedium: function(media) {
+                var text = '@media ' + media + '{ #matchmediajs-test { width: 1px; } }';
+
+                // 'style.styleSheet' is used by IE <= 8 and 'style.textContent' for all other browsers
+                if (style.styleSheet) {
+                    style.styleSheet.cssText = text;
+                } else {
+                    style.textContent = text;
+                }
+
+                // Test if media query is true or false
+                return info.width === '1px';
+            }
+        };
+    }
+
+    return function(media) {
+        return {
+            matches: styleMedia.matchMedium(media || 'all'),
+            media: media || 'all'
+        };
+    };
+}());
