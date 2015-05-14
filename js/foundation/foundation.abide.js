0a2526ecc947f458ac520c91045bc26af3142717
caycek <me@caycekoehler.com>
2015-05-14 12:58:06 -0400

    Hot fix
    Fix a check to make sure that the element is not empty. The only reason
    this check should be run is if it already has a value inside, otherwise
    it is not necessary.

--- js/foundation/foundation.abide.js
+++ js/foundation/foundation.abide.js
@@ -95,15 +95,15 @@
           .off('.abide')
           .on('blur.fndtn.abide change.fndtn.abide', function (e) {
               var id = this.getAttribute('id'),
-                  eqTo = form.find('[data-equalto="'+ id +'"]').get(0);
+                  eqTo = form.find('[data-equalto="'+ id +'"]');
             // old settings fallback
             // will be deprecated with F6 release
             if (settings.validate_on_blur && settings.validate_on_blur === true) {
               validate(this, e);
             }
             // checks if there is an equalTo equivilant related by id
-            if(typeof eqTo !== "undefined"){
-              validate(eqTo,e);
+            if(typeof eqTo.get(0) !== "undefined" && eqTo.val().length){
+              validate(eqTo.get(0),e);
             }
             // new settings combining validate options into one setting
             if (settings.validate_on === 'change') {
@@ -119,8 +119,8 @@
               validate(this, e);
             }
             // checks if there is an equalTo equivilant related by id
-            if(typeof eqTo !== "undefined"){
-                validate(eqTo,e);
+            if(typeof eqTo.get(0) !== "undefined" && eqTo.val().length){
+              validate(eqTo.get(0),e);
             }
             // new settings combining validate options into one setting
             if (settings.validate_on === 'tab' && e.which === 9) {
