89b3f1c1b68c25f1eaf0525604dce2c80eb623ab
Geoff Kimball <geoff@zurb.com>
2015-07-29 14:51:34 -0700

    Add JSDoc comments to off-canvas

--- docs/pages/off-canvas.md
+++ docs/pages/off-canvas.md
@@ -2,6 +2,8 @@
 title: Off-canvas
 description: Off-canvas menus are positioned outside of the viewport and slide in when activated. Setting up an off-canvas layout in Foundation is super easy.
 sass: ./scss/components/_off-canvas.scss
+js: ./js/foundation.offcanvas.js
 ---
 
 <a class="button" data-toggle="body">Toggle Off-canvas</a>
+ 
--- js/foundation.offcanvas.js
+++ js/foundation.offcanvas.js
@@ -2,6 +2,13 @@
 
 'use strict';
 
+/**
+ * Creates a new instance of an off-canvas wrapper.
+ * @class
+ * @fires OffCanvas#init
+ * @param {Object} element - jQuery object to initialize.
+ * @param {Object} options - Overrides to the default plugin settings.
+ */
 function OffCanvas(element, options) {
   this.$element = element;
   this.options = $.extend(this.defaults, options);
@@ -9,14 +16,34 @@ function OffCanvas(element, options) {
   this._init();
   this._events();
 
+  /**
+   * Fires when the plugin has been successfully initialized.
+   * @event OffCanvas#init
+   */
   this.$element.trigger('init.zf.offcanvas');
 }
 
 OffCanvas.prototype.defaults = {
+  /**
+   * Allow the user to click outside of the menu to close it.
+   * @option
+   * @example true
+   */
   closeOnClick: true,
+
+  /**
+   * CSS class to use when the off-canvas menu is open.
+   * @option
+   * @example 'is-off-canvas-open'
+   */
   activeClass: 'is-off-canvas-open'
 }
 
+/**
+ * Initializes the off-canvas wrapper by adding the exit overlay (if needed).
+ * @function
+ * @private
+ */
 OffCanvas.prototype._init = function() {
   if (this.options.closeOnClick) {
     var exiter = document.createElement('div');
@@ -27,6 +54,11 @@ OffCanvas.prototype._init = function() {
   }
 }
 
+/**
+ * Adds event handlers to the off-canvas wrapper and the exit overlay.
+ * @function
+ * @private
+ */
 OffCanvas.prototype._events = function() {
   this.$element.on({
     'open.zf.trigger': this.open.bind(this),
@@ -39,18 +71,40 @@ OffCanvas.prototype._events = function() {
   }
 }
 
+/**
+ * Opens the off-canvas menu.
+ * @function
+ * @fires OffCanvas#opened
+ */
 OffCanvas.prototype.open = function() {
+  /**
+   * Fires when the off-canvas menu opens.
+   * @event OffCanvas#opened
+   */
   this.$element
     .addClass(this.options.activeClass)
     .trigger('opened.zf.offcanvas');
 }
 
+/**
+ * Closes the off-canvas menu.
+ * @function
+ * @fires OffCanvas#closed
+ */
 OffCanvas.prototype.close = function() {
+  /**
+   * Fires when the off-canvas menu opens.
+   * @event OffCanvas#closed
+   */
   this.$element
     .removeClass(this.options.activeClass)
     .trigger('closed.zf.offcanvas');
 }
 
+/**
+ * Toggles the off-canvas menu open or closed.
+ * @function
+ */
 OffCanvas.prototype.toggle = function() {
   if (this.$element.hasClass(this.options.activeClass)) {
     this.close();
