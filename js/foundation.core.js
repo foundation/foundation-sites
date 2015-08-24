a1b509eea3471ae1146228206bf6ba7353f635d1
Geoff Kimball <geoff@zurb.com>
2015-08-24 12:05:38 -0700

    Remove unused functions from the core JavaScript file

--- js/foundation.core.js
+++ js/foundation.core.js
@@ -37,27 +37,6 @@ var Foundation = {
   },
 
   /**
-   * Sets individual configuration for plug in instances.
-   * @param {String} plugin - Formal name of the component.
-   * @param {Object} config - Object that contains configuration settings for each instance of plug ins
-   */
-  setPluginConfig: function(plugin, config) {
-    var uuid = this.generateUuid();
-    // initialize config object
-    this._plugins[plugin].config = {};
-    // set key value pairing of element uuid and specified config
-    this._plugins[plugin].config[uuid] = config;
-  },
-
-  /**
-   * Return the instance of a plugin initialized on an element, or `null` if the element has no Foundation plugins.
-   * @param {Object} element - CSS selector, DOM element, or jQuery object to check.
-   */
-  getPlugin: function(element) {
-    return $(element).eq(0).data('zf-plugin') || null;
-  },
-
-  /**
    * Initialize plugins on any elements within `elem` (and `elem` itself) that aren't already initialized.
    * @param {Object} elem - jQuery object containing the element to check inside. Also checks the element itself, unless it's the `document` object.
    * @param {String|Array} plugins - A list of plugins to initialize. Leave this out to initialize everything.
@@ -123,23 +102,6 @@ var Foundation = {
         }, delay);
       }
     };
-  },
-
-  /**
-   * Generate a unique 16-character id to assign to an element to account for multiple instances of a plugin
-   */
-  generateUuid: function() {
-    var uuid = '';
-
-    do {
-      uuid += 'zf-uuid-';
-      for (var i=0; i<16; i++) {
-        uuid += Math.floor(Math.random()*16).toString(16);
-      }
-    } while(!this._uuids.indexOf(uuid));
-
-    this._uuids.push(uuid);
-    return uuid;
   },
 
   /**
