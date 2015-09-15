befc3461a58e784a8944a99d24c5ff34d543e568
Kristofer Krause <kris.krause@gmail.com>
2015-09-15 06:35:24 -0400

    tab test

--- spec/tab/tab.js
+++ spec/tab/tab.js
@@ -11,4 +11,18 @@ describe('tab:', function() {
       return result;
     });
   });
+  
+  describe('basic clearing', function() {
+    beforeEach(function() {
+      document.body.innerHTML = __html__['spec/tab/basic.html'];
+    });
+
+    it('default tab active on initialization', function() {
+      $(document).foundation();
+
+      expect($('#panel11').hasClass('active')).toBe(false);
+      expect($('#panel21').hasClass('active')).toBe(true);
+      expect($('#panel31').hasClass('active')).toBe(false);
+    }); 
+  });
 });
