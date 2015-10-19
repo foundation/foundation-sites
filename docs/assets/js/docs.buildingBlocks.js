760a203f4fd2fa8119ab1301e7b5a7d4d0562637
Geoff Kimball <geoff@zurb.com>
2015-10-19 16:37:18 -0700

    Restyle building blocks section to give each item more prominence, and room for a text description

--- docs/assets/js/docs.buildingBlocks.js
+++ docs/assets/js/docs.buildingBlocks.js
@@ -12,14 +12,14 @@ function addBuildingBlocks(data) {
   var html = '';
 
   $.each(data, function() {
-    html += '<div class="column"><a href="http://zurb.com/building-blocks/'+this.slug+'"><p>'+this.name+'</p><img src="'+this.image_url+'"/></a></div>';
+    html += '<div class="docs-bb"><div class="docs-bb-thumb"><a href="http://zurb.com/building-blocks/'+this.slug+'"><img src="'+this.image_url+'"/></a></div><div class="docs-bb-main"><h3><a href="http://zurb.com/building-blocks/'+this.slug+'">'+this.name+'</a></h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, amet incidunt sint esse nihil eum repellendus necessitatibus officiis iusto reprehenderit, alias laudantium veniam ad. Soluta repellat nemo quaerat quis laboriosam.</p></div></div>';
   });
 
   $('[data-building-blocks]').each(function() {
     $(this).html(html);
   });
 
-  if ($('[data-building-blocks] .column').length === 0) {
+  if ($('[data-building-blocks] .docs-bb').length === 0) {
     $('.docs-building-blocks').hide(0);
   }
 }
