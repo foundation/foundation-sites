f58cf46f26673926db091031ab8658b5f10f4eef
Geoff Kimball <geoff@zurb.com>
2015-10-20 15:41:55 -0700

    Build out Motion UI docs page

--- docs/assets/js/docs.examples.js
+++ docs/assets/js/docs.examples.js
@@ -6,4 +6,18 @@ $('[data-docs-example-ofc]').click(function() {
   $('#offCanvasLeft').toggleClass('reveal-for-large');
 });
 
+$('[data-docs-example-series]').click(function() {
+  $('#series-example').addClass('is-animating');
+});
+
+var $transitionDemo = $('.docs-transition-demo');
+$('.docs-transitions').change(function() {
+  var value = $(this).val();
+  var method = value.match('In') ? 'animateIn' : 'animateOut';
+
+  Foundation.Motion[method]($transitionDemo, value, function() {
+    $transitionDemo.show();
+  });
+});
+
 }();
