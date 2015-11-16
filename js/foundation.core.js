0f9e9f4ceca2cd9d61d7760c6a4a00f16346b241
zurbchris <chris@zurb.com>
2015-11-16 11:21:10 -0800

    adds check for vendor prefixed transitionend event to foundation.core

--- js/foundation.core.js
+++ js/foundation.core.js
@@ -159,8 +159,23 @@ var Foundation = {
       });
     });
   },
-  getFnName: functionName
-}
+  getFnName: functionName,
+  transitionend: (function() {
+    var transitions = {
+      'transition': 'transitionend',
+      'WebkitTransition': 'webkitTransitionEnd',
+      'MozTransition': 'transitionend',
+      'OTransition': 'otransitionend'
+    };
+    var elem = document.createElement('div');
+
+    for (var t in transitions){
+      if (typeof elem.style[t] !== 'undefined'){
+        return transitions[t];
+      }
+    }
+  })()
+};
 
 Foundation.util = {
   throttle: function (func, delay) {
