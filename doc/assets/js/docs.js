f18072bdb5860adae078483bc85b127db38cd11b
arthurzurb <arthur@zurb.com>
2015-11-17 10:00:57 -0800

    Adding counter

--- doc/assets/js/docs.js
+++ doc/assets/js/docs.js
@@ -115,3 +115,45 @@ if ($('[data-building-blocks]').length > 0) {
     success: cb
   });
 }
+
+
+// COUNTDOWN TIMER
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) );
+  var seconds = Math.floor( (t/1000) % 60 );
+
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock(){
+    var t = getTimeRemaining(endtime);
+
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock,1000);
+}
+
+var deadline = 'November 19 2015 10:15:00 PDT-0800';
+initializeClock('clockdiv', deadline);
