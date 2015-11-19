dff6f33bb888ef9a9d4b8ec66f76f2a448a5b332
zurbchris <chris@zurb.com>
2015-11-18 22:56:39 -0800

    adds fn.protoype.bind polyfill

--- js/foundation.core.js
+++ js/foundation.core.js
@@ -300,14 +300,34 @@ $.fn.foundation = foundation;
       now: function(){ return Date.now() - this.start; }
     };
   }
-  // window.performance = (window.performance || {
-  //   start: Date.now(),
-  //   now: function(){
-  //       return Date.now() - this.start;
-  //   }
-  // });
 })();
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
 
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(this instanceof fNOP
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    if (this.prototype) {
+      // native functions don't have a prototype
+      fNOP.prototype = this.prototype;
+    }
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
 // Polyfill to get the name of a function in IE9
 function functionName(fn) {
   if (Function.prototype.name === undefined) {
