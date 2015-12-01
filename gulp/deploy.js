cbc4093cd7925f12159a93189446549273268a32
Geoff Kimball <geoff@zurb.com>
2015-12-01 08:57:43 -0800

    Start implementing new faster deploy command (NYI)

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -4,10 +4,42 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var confirm = require('gulp-prompt').confirm;
+var prompt = require('gulp-prompt').prompt;
 var rsync = require('gulp-rsync');
+var replace = require('gulp-replace');
+var git = require('gitty')('../');
+var octophant = require('octophant');
 
-gulp.task('deploy', ['deploy:dist']);
+var VERSIONED_FILES = [
+  'bower.json',
+  'composer.json',
+  'docs/pages/*.md',
+  'js/foundation.core.js',
+  'meteor-README.md',
+  'package.js',
+  'package.json'
+];
 
+var CURRENT_VERSION = require('../package.json').version;
+var NEXT_VERSION;
+
+gulp.task('deploy', function(cb) {
+  sequence('deploy:version', 'deploy:dist', 'deploy:settings', 'deploy:commit', 'deploy:docs', cb);
+});
+
+// Bumps the version number in any file that has one
+gulp.task('deploy:version', function() {
+  return gulp.src(VERSIONED_FILES)
+    .pipe(prompt({
+      type: 'input',
+      name: 'version',
+      message: 'What version are we moving to?'
+    }, setVersionNumber))
+    .pipe(replace(CURRENT_VERSION, NEXT_VERSION))
+    .pipe('.');
+});
+
+// Generates compiled CSS and JS files and puts them in the dist/ folder
 gulp.task('deploy:dist', ['sass:foundation', 'javascript:foundation'], function() {
   var cssFilter = filter(['*.css']);
   var jsFilter  = filter(['*.js']);
@@ -26,6 +58,49 @@ gulp.task('deploy:dist', ['sass:foundation', 'javascript:foundation'], function(
       .pipe(gulp.dest('./dist'));
 });
 
+// Generates a settings file
+gulp.task('deploy:settings', function(cb) {
+  var options = {
+    title: 'Foundation for Sites Settings',
+    output: './scss/settings/_settings.scss',
+    groups: {
+      'grid': 'The Grid',
+      'off-canvas': 'Off-canvas',
+      'typography-base': 'Base Typography'
+    },
+    sort: [
+      'global',
+      'breakpoints',
+      'grid',
+      'typography-base',
+      'typography-helpers'
+    ],
+    imports: ['util/util']
+  }
+
+  octophant('./scss', options, cb);
+});
+
+// Writes a commit with the changes to the version numbers
+gulp.task('deploy:commit', function(cb) {
+  git.addSync('.');
+  git.commitSync('Bump to version ' + NEW_VERSION);
+  git.tagSync('v' + NEW_VERSION);
+  git.push('origin', 'develop', cb);
+});
+
+// Uploads the documentation to the live server
+gulp.task('deploy:docs', ['build'], function() {
+  return gulp.src('./_build/**')
+    .pipe(confirm('Make sure everything looks right before you deploy.'))
+    .pipe(rsync({
+      root: './_build',
+      hostname: 'deployer@72.32.134.77',
+      destination: '/home/deployer/sites/foundation-sites-6-docs'
+    }));
+});
+
+// The Customizer runs this function to generate files it needs
 gulp.task('deploy:custom', ['sass:foundation', 'javascript:foundation'], function() {
   var cssFilter = filter(['*.css']);
   var jsFilter  = filter(['*.js']);
@@ -41,12 +116,7 @@ gulp.task('deploy:custom', ['sass:foundation', 'javascript:foundation'], functio
       .pipe(gulp.dest('./_build/assets/js'));
 });
 
-gulp.task('deploy:docs', ['build'], function() {
-  return gulp.src('./_build/**')
-    .pipe(confirm('Make sure everything looks right before you deploy.'))
-    .pipe(rsync({
-      root: './_build',
-      hostname: 'deployer@72.32.134.77',
-      destination: '/home/deployer/sites/foundation-sites-6-docs'
-    }));
-});
+// Callback used by Inquirer in the deploy:version task to set the input version number
+function setVersionNumber(res) {
+  NEXT_VERSION = res.version;
+}
