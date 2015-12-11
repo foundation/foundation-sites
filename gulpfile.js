68d52872cdba9d969381748c5019ede804e6d6dc
Geoff Kimball <geoff@zurb.com>
2015-12-11 11:27:13 -0800

    Replace gulp-cached with gulp-newer to prevent Gulp crashes when changing the docs layout

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var port = process.env.SERVER_PORT || 3000;
 requireDir('./gulp');
 
 // Builds the documentation and framework files
-gulp.task('build', ['clean', 'copy', 'docs', 'docs:search', 'sass', 'javascript']);
+gulp.task('build', ['clean', 'copy', 'docs:all', 'docs:search', 'sass', 'javascript']);
 
 // Starts a BrowerSync instance
 gulp.task('serve', ['build'], function(){
@@ -16,7 +16,7 @@ gulp.task('serve', ['build'], function(){
 // Runs all of the above tasks and then waits for files to change
 gulp.task('default', ['serve'], function() {
   gulp.watch('docs/**/*', ['docs', browser.reload]);
-  gulp.watch('docs/layout/*.html', ['docs:reset', browser.reload]);
+  gulp.watch(['docs/layout/*.html', 'docs/partials/*.html'], ['docs:all', browser.reload]);
   gulp.watch('scss/**/*', ['sass', browser.reload]);
   gulp.watch('docs/assets/scss/**/*', ['sass:docs', browser.reload]);
   gulp.watch('js/**/*', ['javascript:foundation', browser.reload]);
