5972d4a56552e9f8bc48e1e8567c3841d7fd9474
Geoff Kimball <geoff@zurb.com>
2016-01-12 16:31:57 -0800

    Use nyan reporter for Sass unit tests because it's important

--- gulp/test.js
+++ gulp/test.js
@@ -6,8 +6,8 @@ var mocha = require('gulp-mocha');
 gulp.task('test', ['test:sass', 'test:javascript']);
 
 gulp.task('test:sass', function() {
-  return gulp.src('./test/sass/test_sass.js', {read: false})
-    .pipe(mocha());
+  return gulp.src('./test/sass/test_sass.js', { read: false })
+    .pipe(mocha({ reporter: 'nyan' }));
 });
 
 gulp.task('test:javascript', function(cb) {
