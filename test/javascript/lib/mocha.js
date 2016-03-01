73f2dabd4645694d0897e8e2ca16ab934271faa6
Geoff Kimball <geoff@zurb.com>
2016-03-01 10:28:06 -0800

    Move JS testing libraries into their own folder

--- test/javascript/lib/mocha.js
+++ test/javascript/lib/mocha.js
@@ -12030,7 +12030,7 @@ var exec = require('child_process').exec
 function which(name) {
   var paths = process.env.PATH.split(':');
   var loc;
-  
+
   for (var i = 0, len = paths.length; i < len; ++i) {
     loc = path.join(paths[i], name);
     if (exists(loc)) return loc;
