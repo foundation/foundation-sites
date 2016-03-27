eec42569d06e7d8af3c8b8e5ad43cb6149804dd9
Colin Marshall <colin.michael.marshall@gmail.com>
2016-03-26 18:43:03 -0600

    Added ES6/babel to JS tests, exported babel reporting

--- gulp/test.js
+++ gulp/test.js
@@ -2,9 +2,17 @@ var gulp = require('gulp');
 var opener = require('opener');
 var mocha = require('gulp-mocha');
 var browser = require('browser-sync');
+var concat = require('gulp-concat');
+var babel = require('gulp-babel');
+var onBabelError = require('./babel-error.js')
+
+var JSTESTS = [
+  'test/javascript/components/**/*.js',
+  'test/javascript/util/**/*.js'
+];
 
 // Runs unit tests
-gulp.task('test', ['sass:foundation', 'javascript:foundation', 'watch'], function() {
+gulp.task('test', ['sass:foundation', 'test:transpile-js', 'watch'], function() {
   browser.init({
     server: 'test/visual',
     directory: true
@@ -15,4 +23,12 @@ gulp.task('test', ['sass:foundation', 'javascript:foundation', 'watch'], functio
 gulp.task('test:reload', function(done) {
   browser.reload();
   done();
+});
+
+gulp.task('test:transpile-js', ['javascript:foundation'], function() {
+  return gulp.src(JSTESTS)
+  	.pipe(babel()
+  		.on('error', onBabelError))
+  	.pipe(concat('js-tests.js'))
+  	.pipe(gulp.dest('test/javascript'));
 });
