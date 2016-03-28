a789e52dc57c2c9e61d41729139f27dff0302b02
Colin Marshall <colin.michael.marshall@gmail.com>
2016-03-27 20:36:15 -0600

    Added starter templates for js component unit tests

---
+++ test/javascript/components/abide.js
@@ -0,0 +1,20 @@
+describe('Abide', function() {
+  var plugin;
+  var $html;
+
+  // afterEach(function() {
+  //   plugin.destroy();
+  //   $html.remove();
+  // });
+
+  describe('constructor()', function() {
+    // it('', function() {
+    //   $html = $('').appendTo('body');
+    //   plugin = new Foundation.Abide($html, {});
+
+    //   plugin.$element.should.be.an('object');
+    //   plugin.options.should.be.an('object');
+    // });
+  });
+
+});
---
+++ test/javascript/components/dropdown.js
@@ -0,0 +1,20 @@
+describe('Dropdown', function() {
+	var plugin;
+	var $html;
+
+	// afterEach(function() {
+	// 	plugin.destroy();
+	// 	$html.remove();
+	// });
+
+	describe('constructor()', function() {
+		// it('', function() {
+		// 	$html = $('').appendTo('body');
+		// 	plugin = new Foundation.Dropdown($html, {});
+
+		// 	plugin.$element.should.be.an('object');
+		// 	plugin.options.should.be.an('object');
+		// });
+	});
+
+});
