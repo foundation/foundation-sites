0469a583c6607118b9c81ebec2885df7d033879d
arthurzurb <arthur@zurb.com>
2016-08-19 16:55:46 -0700

    Adding Wired countdown banner

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -16,3 +16,49 @@ $(function() {
 $(function() {
   $('[data-docs-version]').text('v' + Foundation.version);
 });
+
+
+// COUNTDOWN TIMER for Events banner
+function getTimeRemaining(endtime){
+ var t = Date.parse(endtime) - Date.parse(new Date());
+ var minutes = Math.floor( (t/1000/60) % 60 );
+ var days = Math.floor( (t/(1000*60*60)/24) );
+ var hours = Math.floor( (t/(1000*60*60)) % 24 );
+ var seconds = Math.floor( (t/1000) % 60 );
+
+ return {
+   'total': t,
+   'hours': hours,
+   'days': days,
+   'minutes': minutes,
+   'seconds': seconds
+ };
+}
+
+function initializeClock(id, endtime){
+ var clock = document.getElementById(id);
+ var daysSpan = clock.querySelector('.days');
+ var hoursSpan = clock.querySelector('.hours');
+ var minutesSpan = clock.querySelector('.minutes');
+ var secondsSpan = clock.querySelector('.seconds');
+
+ function updateClock(){
+   var t = getTimeRemaining(endtime);
+
+   daysSpan.innerHTML = ('0' + t.days).slice(-2);
+   hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+   minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+   secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+   if(t.total<=0){
+     clearInterval(timeinterval);
+   }
+ }
+
+ updateClock();
+ var timeinterval = setInterval(updateClock,1000);
+}
+
+var deadline = 'Thurs, 25 Aug 2016 8:00:00 PDT';
+initializeClock('clockdiv', deadline);
+// COUNTDOWN TIMER END
