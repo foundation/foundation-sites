88c234f0df76e81f9386d869a481f985ba02e28c
Daniel Schuba <daniel.schuba@getit.de>
2016-09-14 11:58:35 +0200

    Don't attach accordion elements that are not marked as accordion-item

--- js/foundation.accordion.js
+++ js/foundation.accordion.js
@@ -38,7 +38,7 @@ class Accordion {
    */
   _init() {
     this.$element.attr('role', 'tablist');
-    this.$tabs = this.$element.children('li, [data-accordion-item]');
+    this.$tabs = this.$element.children('[data-accordion-item]');
 
     this.$tabs.each(function(idx, el) {
       var $el = $(el),
