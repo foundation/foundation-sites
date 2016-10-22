c2c1c45add757629efbc161250b109a7f1cba0e5
Marius Olbertz <marius.olbertz@gmail.com>
2016-10-22 17:17:48 +0200

    Rewrite event listener to make spying possible.
    Arrow notation makes `bind(this)` statement obsolete, so it can be replaced with an anonymous function calling `_reflow()`, which allows using a `sinon.spy` during the unit tests.

--- js/foundation.interchange.js
+++ js/foundation.interchange.js
@@ -46,7 +46,9 @@ class Interchange {
    * @private
    */
   _events() {
-    $(window).on('resize.zf.interchange', Foundation.util.throttle(this._reflow.bind(this), 50));
+    $(window).on('resize.zf.interchange', Foundation.util.throttle(() => {
+      this._reflow();
+    }, 50));
   }
 
   /**
@@ -61,7 +63,6 @@ class Interchange {
     for (var i in this.rules) {
       if(this.rules.hasOwnProperty(i)) {
         var rule = this.rules[i];
-
         if (window.matchMedia(rule.query).matches) {
           match = rule;
         }
