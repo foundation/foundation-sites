dadfc1dfa2e05f335dcc72b50b8d360428e6b216
Kevin Ball <kmball11@gmail.com>
2017-01-17 16:55:05 -0800

    Change main foundation gulp build to webpack

--- gulp/tasks/javascript.js
+++ gulp/tasks/javascript.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var babel = require('gulp-babel');
 var onBabelError = require('./babel-error.js');
+var rename = require('gulp-rename');
+var webpackStream = require('webpack-stream');
+var webpack2 = require('webpack');
 
 var CONFIG = require('../config.js');
 
@@ -9,13 +12,19 @@ var CONFIG = require('../config.js');
 gulp.task('javascript', ['javascript:foundation', 'javascript:deps', 'javascript:docs']);
 
 gulp.task('javascript:foundation', function() {
-  return gulp.src(CONFIG.JS_FILES)
-    .pipe(babel()
-      .on('error', onBabelError))
-    .pipe(gulp.dest('_build/assets/js/plugins'))
-    .pipe(concat('foundation.js'))
+  return gulp.src('js/entries/all.js')
+    .pipe(webpackStream({/* options */}, webpack2))
+    .pipe(rename('foundation.js'))
     .pipe(gulp.dest('_build/assets/js'));
 });
+//gulp.task('javascript:foundation', function() {
+//  return gulp.src(CONFIG.JS_FILES)
+//    .pipe(babel()
+//      .on('error', onBabelError))
+//    .pipe(gulp.dest('_build/assets/js/plugins'))
+//    .pipe(concat('foundation.js'))
+//    .pipe(gulp.dest('_build/assets/js'));
+//});
 
 gulp.task('javascript:deps', function() {
   return gulp.src(CONFIG.JS_DEPS)
