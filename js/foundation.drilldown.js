5da374d86295bdbc31b6845c41004872c44a4efb
Kevin Ball <kmball11@gmail.com>
2017-07-21 08:28:16 -0700

    Merge pull request #10434 from zurb/brettsmason-drilldown-aria-fix
    Update aria role for drilldown submenu parent

--- js/foundation.drilldown.js
+++ js/foundation.drilldown.js
@@ -85,7 +85,7 @@ class Drilldown extends Plugin {
       var $link = $(this);
       var $sub = $link.parent();
       if(_this.options.parentLink){
-        $link.clone().prependTo($sub.children('[data-submenu]')).wrap('<li class="is-submenu-parent-item is-submenu-item is-drilldown-submenu-item" role="menu-item"></li>');
+        $link.clone().prependTo($sub.children('[data-submenu]')).wrap('<li class="is-submenu-parent-item is-submenu-item is-drilldown-submenu-item" role="menuitem"></li>');
       }
       $link.data('savedHref', $link.attr('href')).removeAttr('href').attr('tabindex', 0);
       $link.children('[data-submenu]')
