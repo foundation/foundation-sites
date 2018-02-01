e8f1a457297b90b0a13f01f8b8f4d944cb4ee7a3
Nicolas Coden <nicolas@ncoden.fr>
2018-02-01 20:05:23 +0100

    fix: fix ES6 plugins webpack build
    Export the mutated `Foundation` object and the plugin in every plugin entry

--- js/entries/plugins/foundation.responsiveToggle.js
+++ js/entries/plugins/foundation.responsiveToggle.js
@@ -3,3 +3,4 @@ import { Foundation } from './foundation.core';
 import { ResponsiveToggle } from '../../foundation.responsiveToggle';
 Foundation.plugin(ResponsiveToggle, 'ResponsiveToggle');
 
+export { Foundation, ResponsiveToggle };
