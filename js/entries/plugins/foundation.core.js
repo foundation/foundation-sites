4a8004d8d538498a31bdadfa645e66f6ccae53a5
Nicolas Coden <nicolas@ncoden.fr>
2018-02-26 07:25:27 +0100

    chore: prevent `core.utils` and `core.plugin` duplication across plugin entries
    Export all `foundation.core.*` files in the `foundation.core` entry to prevent duplication across plugin entries without adding new entries.

--- js/entries/plugins/foundation.core.js
+++ js/entries/plugins/foundation.core.js
@@ -1,23 +1,31 @@
+// --- Foundation Core API ---
+// Initialize Foundation and add some utilities to its public API for backward compatibility.
+// Please note that every utility do not have to be added to the core API.
 import $ from 'jquery';
-
 import { Foundation } from '../../foundation.core';
+import { Plugin } from '../../foundation.core.plugin';
+import { rtl, GetYoDigits, transitionend, RegExpEscape } from '../../foundation.core.utils';
+
 Foundation.addToJquery($);
+
+// Every plugin depends on plugin now, we can include that on the core for the
+// script inclusion path.
+Foundation.Plugin = Plugin;
 
 // These are now separated out, but historically were a part of this module,
 // and since this is here for backwards compatibility we include them in
 // this entry.
-import { rtl, GetYoDigits, transitionend, RegExpEscape } from '../../foundation.core.utils';
 Foundation.rtl = rtl;
 Foundation.GetYoDigits = GetYoDigits;
 Foundation.transitionend = transitionend;
 Foundation.RegExpEscape = RegExpEscape;
 
-// Every plugin depends on plugin now, we can include that on the core for the
-// script inclusion path.
-import { Plugin } from '../../foundation.core.plugin';
-Foundation.Plugin = Plugin;
-
-
 window.Foundation = Foundation;
 
+// --- Foundation Core exports ---
+// Export "Plugin" and all core utilities, since the `foundation.core` entry plays the role of
+// all core source files.
+
 export { Foundation };
+export * from '../../foundation.core.plugin';
+export * from '../../foundation.core.utils';
