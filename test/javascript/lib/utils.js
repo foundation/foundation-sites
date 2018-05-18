33d009a48f5fba91a5720544354500c51f1eff04
Nicolas Coden <nicolas@ncoden.fr>
2018-05-18 22:16:27 +0200

    docs: add doc for "tryInterval" test utility

--- test/javascript/lib/utils.js
+++ test/javascript/lib/utils.js
@@ -1,5 +1,14 @@
 (function (global, $) {
 
+  /**
+  * Try to catch the `do` function every `interval` delay, until it succedes
+  * (does not throw an error) or a given `timout` time passed.
+  * If a `then` function is passed, it is called after `do` succeded.
+  * If a `catch` function is passed, it is called after each time `do` fails.
+  *
+  * @param  {object} opts
+  * @param  {object} opts
+  */
   global.tryInterval = function (opts) {
     var _opts = $.extend({}, opts);
     var totalTime = 0;
