5fb700950756fd9a4f1fe4ad172c4a64005e680d
Nicolas Coden <nicolas@ncoden.fr>
2018-06-16 09:32:00 +0200

    Use pull request #11086 from ncoden/chore/normalize-8 for v6.5.0
    6e72435b2 chore: drop `normalize-scss` and upgrade to normalize.css v8.0.0
    
    Signed-off-by: Nicolas Coden <nicolas@ncoden.fr>

--- gulp/config.js
+++ gulp/config.js
@@ -24,7 +24,6 @@ module.exports = {
 
   // Sass
   SASS_DEPS_FILES: [
-    'node_modules/@(normalize-scss)/sass/**/*.scss',
     'node_modules/@(sassy-lists)/stylesheets/helpers/_missing-dependencies.scss',
     'node_modules/@(sassy-lists)/stylesheets/helpers/_true.scss',
     'node_modules/@(sassy-lists)/stylesheets/functions/_contain.scss',
