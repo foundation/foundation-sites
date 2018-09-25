be0ef6b35de2151779fd77a2ff339df51dd6094a
Nicolas Coden <nicolas@ncoden.fr>
2018-09-25 23:47:10 +0200

    chore: bump version for for v6.5.0-rc.4

--- js/foundation.core.js
+++ js/foundation.core.js
@@ -4,7 +4,7 @@ import $ from 'jquery';
 import { GetYoDigits } from './foundation.core.utils';
 import { MediaQuery } from './foundation.util.mediaQuery';
 
-var FOUNDATION_VERSION = '6.5.0-rc.3';
+var FOUNDATION_VERSION = '6.5.0-rc.4';
 
 // Global Foundation object
 // This is attached to the window, or used as a module for AMD/Browserify
