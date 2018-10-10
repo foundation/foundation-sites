ad06d3b91567c10d039fdd9c96858c565f8bf70b
Nicolas Coden <nicolas@ncoden.fr>
2018-10-10 19:31:51 +0200

    docs: fix various typo in Touch methods and Reveal tests (v6.5)
    This is a patch of 6200d17 (#11430) for v6.5.

--- js/foundation.util.touch.js
+++ js/foundation.util.touch.js
@@ -19,7 +19,7 @@ function onTouchEnd(e) {
   this.removeEventListener('touchmove', onTouchMove);
   this.removeEventListener('touchend', onTouchEnd);
 
-  // If the touch did not moved, consider it as a "tap"
+  // If the touch did not move, consider it as a "tap"
   if (!didMoved) {
     var tapEvent = $.Event('tap', startEvent || e);
     $(this).trigger(tapEvent);
