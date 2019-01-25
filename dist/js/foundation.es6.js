791393973c530f3888a95e84baeedcac53452cf5
Nicolas Coden <nicolas@ncoden.fr>
2019-01-25 22:08:37 +0100

    chore: update dist files for v6.5.3

--- dist/js/foundation.es6.js
+++ dist/js/foundation.es6.js
@@ -367,7 +367,7 @@ function parseStyleToObject(str) {
   return styleObject;
 }
 
-var FOUNDATION_VERSION = '6.5.2';
+var FOUNDATION_VERSION = '6.5.3';
 
 // Global Foundation object
 // This is attached to the window, or used as a module for AMD/Browserify
