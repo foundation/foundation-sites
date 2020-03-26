d6012a476ca232a5c5bd9e2c3e754b5a1d6096e7
Trisha Salas <trisha@trishasalas.com>
2020-03-26 08:33:06 -0500

    set root ul visibility:hidden and set subnav to visibility:visible when subnav is open
    This will remove it from the dom allowing the user to tab into the next component/area when the subnav is open/visible

--- js/foundation.drilldown.js
+++ js/foundation.drilldown.js
@@ -149,26 +149,26 @@ class Drilldown extends Plugin {
     var _this = this;
 
     $elem.off('click.zf.drilldown')
-    .on('click.zf.drilldown', function(e){
-      if($(e.target).parentsUntil('ul', 'li').hasClass('is-drilldown-submenu-parent')){
-        e.preventDefault();
-      }
+         .on('click.zf.drilldown', function(e){
+           if($(e.target).parentsUntil('ul', 'li').hasClass('is-drilldown-submenu-parent')){
+             e.preventDefault();
+           }
 
-      // if(e.target !== e.currentTarget.firstElementChild){
-      //   return false;
-      // }
-      _this._show($elem.parent('li'));
+           // if(e.target !== e.currentTarget.firstElementChild){
+           //   return false;
+           // }
+           _this._show($elem.parent('li'));
 
-      if(_this.options.closeOnClick){
-        var $body = $('body');
-        $body.off('.zf.drilldown').on('click.zf.drilldown', function(e){
-          if (e.target === _this.$element[0] || $.contains(_this.$element[0], e.target)) { return; }
-          e.preventDefault();
-          _this._hideAll();
-          $body.off('.zf.drilldown');
-        });
-      }
-    });
+           if(_this.options.closeOnClick){
+             var $body = $('body');
+             $body.off('.zf.drilldown').on('click.zf.drilldown', function(e){
+               if (e.target === _this.$element[0] || $.contains(_this.$element[0], e.target)) { return; }
+               e.preventDefault();
+               _this._hideAll();
+               $body.off('.zf.drilldown');
+             });
+           }
+         });
   }
 
   /**
@@ -195,9 +195,9 @@ class Drilldown extends Plugin {
         scrollPos = parseInt($scrollTopElement.offset().top+_this.options.scrollTopOffset, 10);
     $('html, body').stop(true).animate({ scrollTop: scrollPos }, _this.options.animationDuration, _this.options.animationEasing,function(){
       /**
-        * Fires after the menu has scrolled
-        * @event Drilldown#scrollme
-        */
+       * Fires after the menu has scrolled
+       * @event Drilldown#scrollme
+       */
       if(this===$('html')[0])_this.$element.trigger('scrollme.zf.drilldown');
     });
   }
@@ -319,16 +319,16 @@ class Drilldown extends Plugin {
     var _this = this;
     $elem.off('click.zf.drilldown');
     $elem.children('.js-drilldown-back')
-      .on('click.zf.drilldown', function(e){
-        // console.log('mouseup on back');
-        _this._hide($elem);
+         .on('click.zf.drilldown', function(e){
+           // console.log('mouseup on back');
+           _this._hide($elem);
 
-        // If there is a parent submenu, call show
-        let parentSubMenu = $elem.parent('li').parent('ul').parent('li');
-        if (parentSubMenu.length) {
-          _this._show(parentSubMenu);
-        }
-      });
+           // If there is a parent submenu, call show
+           let parentSubMenu = $elem.parent('li').parent('ul').parent('li');
+           if (parentSubMenu.length) {
+             _this._show(parentSubMenu);
+           }
+         });
   }
 
   /**
@@ -344,7 +344,7 @@ class Drilldown extends Plugin {
           setTimeout(function(){
             _this._hideAll();
           }, 0);
-      });
+        });
   }
 
   /**
@@ -444,7 +444,13 @@ class Drilldown extends Plugin {
     $elem.attr('aria-expanded', true);
 
     this.$currentMenu = $submenu;
-    $submenu.addClass('is-active').removeClass('invisible').attr('aria-hidden', false);
+
+    //hide drilldown parent menu when submenu is open
+    // this removes it from the dom so that the tab key will take the user to the next visible element
+    $elem.parent().closest('ul').addClass('invisible');
+
+    // add visible class to submenu to override invisible class above
+    $submenu.addClass('is-active visible').removeClass('invisible').attr('aria-hidden', false);
     if (this.options.autoHeight) {
       this.$wrapper.css({ height: $submenu.data('calcHeight') });
     }
@@ -465,11 +471,14 @@ class Drilldown extends Plugin {
   _hide($elem) {
     if(this.options.autoHeight) this.$wrapper.css({height:$elem.parent().closest('ul').data('calcHeight')});
     var _this = this;
+
+    $elem.parent().closest('ul').removeClass('invisible');
+
     $elem.parent('li').attr('aria-expanded', false);
     $elem.attr('aria-hidden', true);
     $elem.addClass('is-closing')
          .one(transitionend($elem), function(){
-           $elem.removeClass('is-active is-closing');
+           $elem.removeClass('is-active is-closing visible');
            $elem.blur().addClass('invisible');
          });
     /**
@@ -517,12 +526,12 @@ class Drilldown extends Plugin {
   _destroy() {
     if(this.options.scrollTop) this.$element.off('.zf.drilldown',this._bindHandler);
     this._hideAll();
-	  this.$element.off('mutateme.zf.trigger');
+    this.$element.off('mutateme.zf.trigger');
     Nest.Burn(this.$element, 'drilldown');
     this.$element.unwrap()
-                 .find('.js-drilldown-back, .is-submenu-parent-item').remove()
-                 .end().find('.is-active, .is-closing, .is-drilldown-submenu').removeClass('is-active is-closing is-drilldown-submenu')
-                 .end().find('[data-submenu]').removeAttr('aria-hidden tabindex role');
+        .find('.js-drilldown-back, .is-submenu-parent-item').remove()
+        .end().find('.is-active, .is-closing, .is-drilldown-submenu').removeClass('is-active is-closing is-drilldown-submenu')
+        .end().find('[data-submenu]').removeAttr('aria-hidden tabindex role');
     this.$submenuAnchors.each(function() {
       $(this).off('.zf.drilldown');
     });
